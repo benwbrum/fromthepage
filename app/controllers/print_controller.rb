37a80b9ff431113b545af0f9abc0b7b6fb9e2cf3
benwbrum <benwbrum@3675f2b2-6144-0410-ac64-b8c47cbf175e>
2008-10-27 01:13:57 +0000

    Debuggable RTex

--- app/controllers/print_controller.rb
+++ app/controllers/print_controller.rb
@@ -1,6 +1,14 @@
 class PrintController < ApplicationController
 
   def make_pdf
+    string = render_without_rtex
+#    string = render_to_string
+    logger.debug(string)
+    erase_render_results
+    @printed_before = nil
+    forget_variables_added_to_assigns
+    reset_variables_added_to_assigns
+    render_with_rtex
   end
 
   def work
--- app/helpers/print_helper.rb
+++ app/helpers/print_helper.rb
@@ -1,5 +1,6 @@
 module PrintHelper
-  
+  MINIMUM_TITLE_TO_EXPAND = 8
+  TITLE_TO_TEXT_THRESHOLD = 4
   
   def xml_to_latex(page)
     latex = ""
@@ -15,20 +16,46 @@ module PrintHelper
       title = link.attributes['target_title']
       id = link.attributes['target_id']
       latex_link = link.children.to_s
+      if print_footnote?(id, title, latex_link) 
+        latex_link += "\\footnote{#{title}}"
+      end
       link.replace_with(REXML::Text.new(latex_link))
     end
     
     page.elements.each("//p") do |para|
-      latex << '\n\n' 
+      latex << "\n\n" 
       para.each do |e|
         #p e
         latex << e.to_s
       end
     end
+    # clear the footnote array in case render is called twice for debugging
     return latex
     
   end
   
   
   private
+  def print_footnote?(id, title, text)
+    @printed_before ||= {}
+    # have we printed the footnote before?
+    logger.debug "DEBUG: @printed_before[#{id}] == #{@printed_before[id].to_s} (#{title})"
+    if @printed_before[id] == true
+      logger.debug "DEBUG: bailing because we've printed this link before"
+      return false
+    end
+    logger.debug "DEBUG: exited bailout"
+    # is it long enough to expand?
+    logger.debug "DEBUG: is #{title.length} > #{MINIMUM_TITLE_TO_EXPAND} ? "
+    if title.length > MINIMUM_TITLE_TO_EXPAND
+      if title.length - text.length > TITLE_TO_TEXT_THRESHOLD
+        logger.debug "DEBUG: printing the link"
+        @printed_before[id] = true
+        logger.debug "DEBG: set @printed_before[#{id}] = true (#{@printed_before[id]})"
+        return true
+      end
+    end
+    # does it have article text
+    false
+  end
 end
