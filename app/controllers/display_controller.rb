4574137285c6b2d00e6180a6ad33ad0cbddd1878
benwbrum <benwbrum@3675f2b2-6144-0410-ac64-b8c47cbf175e>
2009-03-19 02:22:44 +0000

    Show pages that mention a subject across works.

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -9,29 +9,28 @@ class DisplayController < ApplicationController
                                         :per_page => 5,
                                         :joins => 'INNER JOIN page_article_links pal ON pages.id = pal.page_id',
                                         :conditions => [ 'pal.article_id = ?', @article.id ]
+      @pages.uniq!
     else
       @pages = Page.paginate_by_work_id @work.id, :page => params[:page],  
                                         :order => 'position',
                                         :per_page => 5
     end                                      
-                                      
-
-#    articles = []
-#    @pages.each { |page| articles += page.articles }
-#    articles.uniq!
-#    @categories = []
-#    logger.debug("DEBUG #{articles.inspect}")
-#    articles.each { |article| @categories += article.categories }
-#    logger.debug(@categories.length)
-#    @categories.uniq!
-#    # TODO -- figure out how to display parent categories for children without expanding them!
-#    # note: to make this work, we need to walk through each article,
-#    # walk through its's categories and add each parent category to the list
-#    # then pass that list to the treeview control as a "don't show anything but these"
-#    # contstraint.
-#    # 
-#    # I will not, however do that since it seems more important for the
-#    # Category treeview to be consistent between views than to restrict it.
-#    logger.debug(@categories.length)
   end
+
+  def read_all_works
+    if @article
+      # restrict to pages that include that subject
+      @pages = Page.paginate :all, :page => params[:page],  
+                                        :order => 'work_id, position',
+                                        :per_page => 5,
+                                        :joins => 'INNER JOIN page_article_links pal ON pages.id = pal.page_id',
+                                        :conditions => [ 'pal.article_id = ?', @article.id ]
+      @pages.uniq!
+    else
+      @pages = Page.paginate :all, :page => params[:page],  
+                                        :order => 'work_id, position',
+                                        :per_page => 5
+    end                                      
+  end
+
 end
