121353ea7bb2aa39459c71bb28fa6311c6ea0c75
Ben Brumfield <benwbrum@anitaborg.(none)>
2011-11-05 13:21:13 -0500

    Added special route for ZenasMatthews

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,8 @@ ActionController::Routing::Routes.draw do |map|
   
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
-
+  map.connect 'ZenasMatthews', :controller => 'collection', :action => 'show', :collection_id => 7
+  map.connect 'JuliaBrumfield', :controller => 'collection', :action => 'show', :collection_id => 1
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
