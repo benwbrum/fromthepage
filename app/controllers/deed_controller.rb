714c311f6063af8c5e837c5b9102bb7507ddc62a
Ben Brumfield <benwbrum@anitaborg.(none)>
2012-01-22 11:13:37 -0600

    Issue #8: "more" link is not collection-specific.

--- app/controllers/deed_controller.rb
+++ app/controllers/deed_controller.rb
@@ -3,7 +3,18 @@ class DeedController < ApplicationController
   def list
     limit = params[:limit] || 50
     @offset = params[:offset] || 0
-    @deeds = Deed.find(:all, :limit => limit, :offset => @offset, :order => 'created_at DESC')
+    if @collection
+      @deeds = Deed.find(:all, 
+                        {:limit => limit, 
+                         :offset => @offset, 
+                         :order => 'created_at DESC',
+                         :conditions => [ 'collection_id = ?', @collection.id ]})
+    else
+      @deeds = Deed.find(:all, 
+                        {:limit => limit, 
+                         :offset => @offset, 
+                         :order => 'created_at DESC'})
+    end
   end
 
   def short_list
