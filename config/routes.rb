271f05d91e86e45b637becf94b73f985d22e2a70
Eric MacAdie <Eric@MacAdie.net>
2013-02-12 23:33:43 -0600

    JJJJJJ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Fromthepage::Application.routes.draw do
   get '/account/login' => 'account#login' # , :as => 'websites'
-  get '/static/splash' => 'static#splash'
+  get '/static_splash' => 'static#splash'
+  match '/static/splash' => 'static#splash'
   match '/' => 'static#splash'
   match 'ZenasMatthews' => 'collection#show', :collection_id => 7
   match 'JuliaBrumfield' => 'collection#show', :collection_id => 1
