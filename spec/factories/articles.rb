c0173255bf0201f1df94f881d4380a7ac8fbb48a
Eric MacAdie <Eric@MacAdie.net>
2013-02-23 15:33:12 -0600

    Tested Article.clear_links

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -4,24 +4,7 @@ FactoryGirl.define do
     title  "joejoejoe"
     source_text  "password"
     lock_version 0
-    collection_id 1
+    :collection
   end
 
-=begin
-  factory :article_version do
-    title "hjoehjoe"
-    source_text "jdjdjddj"
-    :article
-  end
-=end
-=begin
-  factory :user2, class: User do
-    login  "moemoemoe"
-    password  "password"
-    password_confirmation  "password"
-    display_name  "moemoemoe"
-    print_name  "moemoemoe"
-    email  "moe@example.com"
-  end
-=end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,13 +3,12 @@ require 'spec_helper'
 describe Article do
 
   before(:each) do
+    # FactoryGirl does not seem to work here
     @article = Article.new
     @article.title = "Hello"
     @article.collection_id = FactoryGirl.create(:collection).id
     @article.save
-    # puts "article.id: #{@article.id}"
-    # puts "article.title: #{@article.title}"
-    # puts "article.inspect: #{@article.inspect}"
+    
   end
 
   subject { @article }
@@ -18,9 +17,9 @@ describe Article do
   it { should respond_to(:source_text) }
 
   it "should create version" do
-    puts "article title: #{@article.title}"
-    puts "@article.instance_variable_names: #{@article.instance_variable_names.sort}"
-    puts "@article.instance_variable_get(@title_dirty): #{@article.instance_variable_get("@title_dirty")}"
+    # puts "article title: #{@article.title}"
+    # puts "@article.instance_variable_names: #{@article.instance_variable_names.sort}"
+    # puts "@article.instance_variable_get(@title_dirty): #{@article.instance_variable_get("@title_dirty")}"
     
     puts "ArticleVersion.count: #{ArticleVersion.count}"
     @article.source_text = "Chumba My Wumba"
@@ -38,77 +37,21 @@ describe Article do
     expect{ @article.create_link(article2, "display_text") }.to change{ ArticleArticleLink.count }.by(1)
   end
 
-=begin
-  it "login must be unique" do
-    user2 = User.new
-    user2.login = @user.login
-    user2.password = "1234567"
-    user2.password_confirmation = "1234567"
-    user2.should_not be_valid
-    User.count.should == 1
-  end
+  it "should delete links" do
+    aa = 10
+    aa.times { |i| 
+      article_i = Article.new
+      article_i.title = "x" * i
+      article_i.collection_id = Collection.first.id
+      article_i.save
+      expect{ @article.create_link(article_i, "display text" + i.to_s) }.to change{ ArticleArticleLink.count }.by(1)
+    }
+    puts "ArticleArticleLink.count: #{ArticleArticleLink.count}"
+    
+    expect{ @article.clear_links }.to change{ ArticleArticleLink.count }.to(0)
+    puts "after clear_links: ArticleArticleLink.count: #{ArticleArticleLink.count}"
 
-  it "confirms password" do
-    user2 = User.new
-    user2.login = "qwerrty"
-    user2.password = "1234567"
-    user2.password_confirmation = "1234568"
-    user2.should_not be_valid
-    User.count.should == 1
+    # ArticleArticleLink.count
   end
-
-  describe "when login is not present" do
-    before { @user.login = " " }
-    it { should_not be_valid }
-  end
-
-  # I don't know why this doesn't work.
-  xit "when password is not present" do
-    before { @user.password = nil }
-    it { should_not be_valid }
-  end
-
-  describe "when password_confirmation is not present" do
-    before { @user.password_confirmation = nil }
-    it { should_not be_valid }
-  end
-
-  # It seems like this should work
-  xit "checks the length of the username" do
-    test_name = "e" * 200
-    user3 = User.new
-    user3.login = test_name
-    user3.password = "123467"
-    user3.password_confirmation = "1234657"
-    user3.should_not be_valid
-   
-    puts "test_name: #{test_name}"
-    puts "User count: #{User.count}"
-  end
-
-  describe "when login is too long" do
-    before { @user.login = "a" * 41 }
-    it { should_not be_valid }
-  end
-
-  describe "when login is too short" do
-    before { @user.login = "a" * 2 }
-    it { should_not be_valid }
-  end
-
-  describe "when password is too long" do
-    before { @user.password = "a" * 41 }
-    it { should_not be_valid }
-  end
-
-  describe "when password is too short" do
-    before { @user.password = "a" * 3 }
-    it { should_not be_valid }
-  end
-
-  it "gets the count" do
-    puts "User count: #{User.count}"
-  end
-=end
 
 end
