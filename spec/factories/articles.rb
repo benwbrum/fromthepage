c6a1274b0f1da6457e70f73a6e804b0aaa7520d9
Eric MacAdie <Eric@MacAdie.net>
2013-02-23 17:47:01 -0600

    Still restoring stuff

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -7,21 +7,4 @@ FactoryGirl.define do
     collection_id 1
   end
 
-=begin
-  factory :article_version do
-    title "hjoehjoe"
-    source_text "jdjdjddj"
-    :article
-  end
-=end
-=begin
-  factory :user2, class: User do
-    login  "moemoemoe"
-    password  "password"
-    password_confirmation  "password"
-    display_name  "moemoemoe"
-    print_name  "moemoemoe"
-    email  "moe@example.com"
-  end
-=end
 end
