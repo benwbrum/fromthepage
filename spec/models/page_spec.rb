0884a680815c1aa697d29b60daee5bdb819bdbeb
Eric MacAdie <Eric@MacAdie.net>
2013-03-16 18:28:13 -0500

    The page specs seem to be working again

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -3,7 +3,24 @@ require 'spec_helper'
 describe Page do
   before(:each) do
     # WorkController.class.skip_before_filter :authorized?
-    @page = FactoryGirl.create(:page)
+    @work = FactoryGirl.create(:work)
+    puts "@work is a #{@work.class}"
+    @page = Page.new
+    
+    @page.title = "Wednesday, January 2, 1918"
+    @page.source_text  = "From the Page is awesoooooome"
+    @page.base_image = "/home/fromthe/fromthepage/current/public/images/working/6/img_3862.jpg"
+    @page.base_width = 1944
+    @page.base_height = 2592
+    @page.shrink_factor = 2
+    @page.position = 1
+    @page.lock_version = 3
+    @page.xml_text = "<?xml version='1.0' encoding='ISO-8859-15'?> \n <page>\n <p/>\n
+\ </page>\n"
+    @work.pages << @page 
+    # @page = 
+    
+    # @work.pages << @page
   end
 
   subject { @page }
