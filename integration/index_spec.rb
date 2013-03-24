eeb1c9e6271c41090032760129586eb4731e8e45
Eric MacAdie <Eric@MacAdie.net>
2013-03-24 15:20:06 -0500

    There is now a test for bad logins

--- integration/index_spec.rb
+++ integration/index_spec.rb
@@ -6,7 +6,6 @@ describe "index page" do
     # @book2 = FactoryGirl.create(:book2)
   end
 
-
   it "should have a link to log in and sign up if not logged in" do
     visit "/"
     page.should_not have_content("Log out")
@@ -15,51 +14,26 @@ describe "index page" do
   end
 
   it "should have link to log out if logged in" do
-    
     p = SessionProvider.get_session
     visit "/"
     page.should have_content("Log out")
     page.should_not have_content("Log in")
     page.should_not have_content("Sign Up")
-    
   end
 
-
-=begin
-  it "gets a session from Provider " do
-
+  it "should not allow invalid logins" do
     user2 = FactoryGirl.create(:user2)
+    visit "/account/login"
+    fill_in "Login",    :with => "moemoemoe"
+    fill_in "Password", :with => "chumba-wumba"
+    click_button "Log in"
 
-    fd = Deed.new 
-    fd.note_id = 1
-    fd.page_id = 1
-    fd.work_id = 1
-    fd.collection_id = 1
-    fd.deed_type = Deed::PAGE_TRANSCRIPTION
-    fd.user_id = user2.id
-    fd.save!
-=end
-=begin
-deedid: 1, deed_type: "page_trans", page_id: 632, work_id: 2, collection_id: 1, article_id: nil, user_id: 2, note_id: nil, created_at: "2008-03-10 12:42:23", updated_at: "2008-04-07 22:34:06"> 
-1.9.3-p0 :002 > ld = Deed.last
-  Deed Load (0.4ms)  SELECT `deeds`.* FROM `deeds` ORDER BY `deeds`.`id` DESC LIMIT 1
- => #<Deed id: 4948, deed_type: "page_edit", page_id: 2056, work_id: 16, collection_id: 1, article_id: nil, user_id: 221, note_id: nil, created_at: "2012-12-07 04:39:02", updated_at: "2012-12-07 04:39:02"> 
-    deed = Deed.new
-    deed.note = @note
-    deed.page = @page
-    deed.work = @work
-    deed.collection = @collection
-    deed.deed_type = Deed::NOTE_ADDED
-    deed.user = current_user
-    deed.save!
-=end
-################
-
-=begin    
-    p = SessionProvider.get_session
-    visit "/dashboard/main_dashboard"
-    page.should have_content("Log out")
+    # puts "page.body: #{page.body}"
+    page.should have_content("Your login and password did not match.")
+    page.should_not have_content("Log out")
+    page.should have_content("Log in")
+    page.should have_content("Sign Up")
   end
-=end
+
 
 end
