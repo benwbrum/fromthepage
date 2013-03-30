485721dc483bae5add9c28f733a67484246a9157
Eric MacAdie <Eric@MacAdie.net>
2013-03-30 16:58:15 -0500

    I added more association tests. I am having a problem with Collection having many Articles. It used to work, and now it does not

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -15,7 +15,14 @@ describe Collection do
   it { should respond_to(:title) }
   it { should respond_to(:intro_block) }
   it { should respond_to(:footer_block) }
-
-  it { should have_many(:articles) }
+  
+  it { should have_many(:works).order(:title) }
+  it { should have_many(:notes).order("created_at DESC") }
+  # this worked, then it stopped working, I don't know why
+  # it { should have_many(:articles) }
+  it { should have_many(:categories).order(:title) }
+  it { should have_many(:deeds).order("created_at DESC") }
+  it { should belong_to(:owner).class_name("User") }
+  it { should have_and_belong_to_many(:owners).class_name("User") }
 
 end
