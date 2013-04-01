d193d1e70beaf07262be6b2489789ac8f2530477
Eric MacAdie <Eric@MacAdie.net>
2013-03-31 20:13:00 -0500

    I have a test for deed.short_partials, with lots of comments for future reference

--- spec/models/deed_spec.rb
+++ spec/models/deed_spec.rb
@@ -19,4 +19,24 @@ describe Deed do
 
   it { should ensure_inclusion_of(:deed_type).in_array( types ) } 
 
+  it 'tests short partial method' do
+    deed = Deed.new
+    deed.note = @note
+    deed.page = @page
+    deed.work = @work
+    deed.collection = @collection
+    # deed.deed_type = Deed::NOTE_ADDED
+    deed.user = FactoryGirl.create(:user1)
+    types.each do |type|
+      deed.deed_type = type
+      puts "Here is deed.short_partial: #{deed.short_partial}"
+      puts "Here is deed.deed_type: #{deed.deed_type}"
+      # puts "Here is deed::SHORT_PARTIALS[deed.deed_type]: #{Deed::SHORT_PARTIALS[deed.deed_type]}"
+      puts "Here is deed::SHORT_PARTIALS[deed.deed_type]: #{Deed::SHORT_PARTIALS[type]}"
+      # puts "Here is deed::SHORT_PARTIALS[deed.deed_type]: #{Deed::SHORT_PARTIALS}"
+      deed.short_partial.should == Deed::SHORT_PARTIALS[type]
+    end
+    # @deed.short_partial.should == 'deed/' + @deed.deed_type + '_short.html.erb'
+  end
+
 end
