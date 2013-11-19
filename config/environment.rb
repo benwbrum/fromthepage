42284cc5ad632f32bcac2d160f926782a055d53c
Ben W. Brumfield <benwbrum@gmail.com>
2013-11-19 14:39:41 -0600

    Back-ported TEI export to FromThePage in Rails 2

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 #RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
-RAILS_GEM_VERSION = '2.3.16' unless defined? RAILS_GEM_VERSION
+#RAILS_GEM_VERSION = '2.3.17' unless defined? RAILS_GEM_VERSION
 
 
 
