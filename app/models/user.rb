e892312a8860ca82db8c0aa74a1ecb032c2b5912
Diego Viola <diego.viola@gmail.com>
2014-03-24 18:12:33 -0300

    remove some old cruft

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,3 @@
-require 'digest/sha1'
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -28,7 +27,6 @@ class User < ActiveRecord::Base
   validates_length_of       :login,    :within => 3..40
 #  validates_length_of       :email,    :within => 3..100, :if => :email_required?
   validates_uniqueness_of   :login, :case_sensitive => false
-  before_save :encrypt_password
 
   has_many(:owner_works, 
            { :foreign_key => "owner_user_id",
@@ -63,53 +61,4 @@ class User < ActiveRecord::Base
     return false
   end
   
-  # Authenticates a user by their login name and unencrypted password.  Returns the user or nil.
-  def self.authenticate(login, password)
-    u = find_by_login(login) # need to get the salt
-    u && u.authenticated?(password) ? u : nil
-  end
-
-  # Encrypts some data with the salt.
-  def self.encrypt(password, salt)
-    Digest::SHA1.hexdigest("--#{password_salt}--#{password}--")
-  end
-
-  # Encrypts the password with the user salt
-  def encrypt(password)
-    self.class.encrypt(password, password_salt)
-  end
-
-  def authenticated?(password)
-    crypted_password == encrypt(password)
-  end
-
-  def remember_token?
-    remember_token_expires_at && Time.now.utc < remember_token_expires_at 
-  end
-
-  # These create and unset the fields required for remembering users between browser closes
-  def remember_me
-    self.remember_token_expires_at = 2.weeks.from_now.utc
-    self.remember_token            = encrypt("#{email}--#{remember_token_expires_at}")
-    save(:validate => false)
-  end
-
-  def forget_me
-    self.remember_token_expires_at = nil
-    self.remember_token            = nil
-    # save(false)
-    save(:validate => false)
-  end
-
-  protected
-    # before filter 
-    def encrypt_password
-      return if password.blank?
-      self.salt = Digest::SHA1.hexdigest("--#{Time.now.to_s}--#{login}--") if new_record?
-      self.crypted_password = encrypt(password)
-    end
-    
-    def password_required?
-      crypted_password.blank? || !password.blank?
-    end
 end
