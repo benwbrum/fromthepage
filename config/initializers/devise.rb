0ec4e88995f0da5b2682b1eba3b9b5eb0f2f700c
Diego Viola <diego.viola@gmail.com>
2014-03-26 00:15:55 -0300

    swap emails for logins for authentication

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -30,6 +30,7 @@ Devise.setup do |config|
   # You can also supply a hash where the value is a boolean determining whether
   # or not authentication should be aborted when the value is not present.
   # config.authentication_keys = [ :email ]
+  config.authentication_keys = [ :login ]
 
   # Configure parameters from the request object used for authentication. Each entry
   # given should be a request method and it will automatically be passed to the
