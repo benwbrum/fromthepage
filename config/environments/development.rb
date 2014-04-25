b1f133d87750b620a9412bab02a998d50fe3a605
Diego Viola <diego.viola@gmail.com>
2014-04-25 02:07:25 -0300

    add rails4 files and changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,42 +1,29 @@
 Fromthepage::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
+  # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # Do not eager load code on boot.
   config.eager_load = false
 
-  # config.serve_static_assets = true
-
-  # Show full error reports and disable caching
+  # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
+  # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  # Print deprecation notices to the Rails logger
+  # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Only use best-standards-support built into browsers
-  config.action_dispatch.best_standards_support = :builtin
+  # Raise an error on page load if there are pending migrations
+  config.active_record.migration_error = :page_load
 
-  # Do not compress assets
-  config.assets.compress = false
-
-  # Expands the lines which load the assets
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
   config.assets.debug = true
-
-  # from Rails 2. Is this still needed?
-  # Show full error reports and disable caching
-  # CAUSES ERROR
-  # config.action_view.debug_rjs = true
-
-  # where is NEATO located on this machine?
-  NEATO = '/usr/bin/neato'
-  # RAKE = '/usr/bin/env rake'
-
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Fromthepage::Application.config.secret_token = 'f8c44530b771b72efa651cc396daaab74c49436eb988f2686ba8387879303e036749b94c48f2a778399b80c25eeafe2f646fd5ccbcfd3b1be41ee96a7334080c'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fromthepage::Application.config.secret_key_base = 'bc401c143e29a516abec1d4b45036779097b296d9f3dd69a37cd961ab7890b670a138955f2603a0d9f6fce93f654a49b70b343566e9fd03efa41d5649a6777f6'
