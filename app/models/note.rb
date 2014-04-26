373da5af65a669a6b5cad3a34f71bafeb1936f70
Diego Viola <diego.viola@gmail.com>
2014-04-26 05:38:43 -0300

    remove old restful_comments code

--- app/models/note.rb
+++ app/models/note.rb
@@ -12,45 +12,4 @@ class Note < ActiveRecord::Base
   belongs_to :work
   belongs_to :collection
   has_one :deed
-
-  #########################
-  # from restful_comments
-  #########################
-  def before_create
-    # handle depth
-    if !self[:parent_id].to_i.zero?
-      self[:depth] = parent[:depth].to_i + 1
-    else
-      self[:depth] = 0
-    end
-  end
-
-  def descendants
-    result = []
-    for note in self.children do
-      result << note
-      result += note.descendants
-    end
-    result
-  end
-
-  def self.for_each( comments, &block )
-    for_each_children( nil, comments, &block ).to_s
-  end
-
-  def to_html
-    txt = self[:body].gsub( '<', '&lt;' ).gsub( '>', '&gt;' ).gsub( '\n','<br />' )
-    return txt 
-    # TODO add whitelist, possibly RedCloth
-  end
-
-  private
-  def self.for_each_children( parent_id, nodes, &block )
-    for node in nodes
-      if node.parent_id.to_i == parent_id.to_i
-	yield node 
-	for_each_children( node.id, nodes, &block )
-      end
-    end
-  end
 end
