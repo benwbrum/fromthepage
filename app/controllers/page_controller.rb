ece1f76e9f3b7216e9ab629ba1c55af5ca275d3e
Diego Viola <diego.viola@gmail.com>
2014-05-14 23:45:32 -0300

    require image helper

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,6 +1,7 @@
 # handles administrative tasks for the page object
 class PageController < ApplicationController
   require 'RMagick'
+  require 'image_helper'
   include ImageHelper
 
   before_filter :authorized?
