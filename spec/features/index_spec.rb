a168a0ec9b3ed80ecfe96918d52efb0cd9d2dad5
Diego Viola <diego.viola@gmail.com>
2014-05-28 15:14:30 -0300

    Use the new rspec syntax

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -3,9 +3,9 @@ require "spec_helper"
 describe "index page" do
   it "should have a link to log in and sign up if not logged in" do
     visit root_path
-    page.should_not have_content("Log out")
-    page.should have_content("Log in")
-    page.should have_content("Sign Up")
+    expect(page).not_to have_content("Log out")
+    expect(page).to have_content("Log in")
+    expect(page).to have_content("Sign Up")
   end
 
   it "should have link to log out if logged in" do
@@ -13,9 +13,9 @@ describe "index page" do
     fill_in "Login", with: "benwbrum"
     fill_in "Password", with: "password"
     click_button "Sign in" do
-      page.should have_content("Log out")
-      page.should_not have_content("Log in")
-      page.should_not have_content("Sign Up")
+      expect(page).to have_content("Log out")
+      expect(page).not_to have_content("Log in")
+      expect(page).not_to have_content("Sign Up")
     end
   end
 
@@ -25,9 +25,9 @@ describe "index page" do
     fill_in "Password", with: "chumba-wumba"
     click_button "Sign in"
 
-    page.should have_content("Invalid user or password.")
-    page.should_not have_content("Log out")
-    page.should have_content("Log in")
-    page.should have_content("Sign Up")
+    expect(page).to have_content("Invalid user or password.")
+    expect(page).not_to have_content("Log out")
+    expect(page).to have_content("Log in")
+    expect(page).to have_content("Sign Up")
   end
 end
