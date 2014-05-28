9f64d4039a12e11f36f04f0513a44283791e2729
Diego Viola <diego.viola@gmail.com>
2014-05-28 15:02:42 -0300

    Fix capybara spec

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,39 +1,33 @@
 require "spec_helper"
 
 describe "index page" do
-  before(:all) do
-    # @book = FactoryGirl.create(:book)
-    # @book2 = FactoryGirl.create(:book2)
-  end
-
   it "should have a link to log in and sign up if not logged in" do
-    visit "/"
+    visit root_path
     page.should_not have_content("Log out")
     page.should have_content("Log in")
     page.should have_content("Sign Up")
   end
 
   it "should have link to log out if logged in" do
-    p = SessionProvider.get_session
-    visit "/"
-    page.should have_content("Log out")
-    page.should_not have_content("Log in")
-    page.should_not have_content("Sign Up")
+    visit new_user_session_path
+    fill_in "Login", with: "benwbrum"
+    fill_in "Password", with: "password"
+    click_button "Sign in" do
+      page.should have_content("Log out")
+      page.should_not have_content("Log in")
+      page.should_not have_content("Sign Up")
+    end
   end
 
   it "should not allow invalid logins" do
-    user2 = FactoryGirl.create(:user2)
-    visit "/account/login"
-    fill_in "Login",    :with => "moemoemoe"
-    fill_in "Password", :with => "chumba-wumba"
-    click_button "Log in"
+    visit new_user_session_path
+    fill_in "Login",    with: "moemoemoe"
+    fill_in "Password", with: "chumba-wumba"
+    click_button "Sign in"
 
-    # puts "page.body: #{page.body}"
-    page.should have_content("Your login and password did not match.")
+    page.should have_content("Invalid user or password.")
     page.should_not have_content("Log out")
     page.should have_content("Log in")
     page.should have_content("Sign Up")
   end
-
-
 end
