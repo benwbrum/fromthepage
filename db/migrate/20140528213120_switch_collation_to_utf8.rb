d53853d7008a6bd4903d1bf4897fcbd1ebd07c39
Ben W. Brumfield <benwbrum@gmail.com>
2014-06-24 05:59:09 -0500

    Added encoding warning message to encoding migration.
    While we can't automatically convert from latin-1 to utf-8 in mysql,
    we can warn the sysadmin of which pages need review.

--- db/migrate/20140528213120_switch_collation_to_utf8.rb
+++ db/migrate/20140528213120_switch_collation_to_utf8.rb
@@ -4,5 +4,10 @@ class SwitchCollationToUtf8 < ActiveRecord::Migration
       puts "Converting #{table} to UTF-8"
       ActiveRecord::Base.connection.execute("ALTER TABLE #{table} CONVERT TO CHARACTER SET utf8")
     end
+    
+    Article.where("title regexp '[^a-zA-Z0-9[:space:][:punct:]]' = 1").each { |a| print "Subject #{a.id}, #{a.title} needs title review\n" }
+    Article.where("xml_text regexp '[^a-zA-Z0-9[:space:][:punct:]]' = 1").each { |a| print "Subject #{a.id}, #{a.title} needs content review\n" }
+    Page.where("xml_text regexp '[^a-zA-Z0-9[:space:][:punct:]]' = 1").each { |a| print "Page #{a.id}, #{a.work.title}: #{a.title} needs content review\n" }
+
   end
 end
