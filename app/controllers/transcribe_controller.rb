26eceeec39582b7b88a48b6c6fc5e820f05ee86b
Ben W. Brumfield <benwbrum@gmail.com>
2014-07-20 11:57:43 -0500

    Added new logging code for all attempts to save transcripts

--- app/controllers/transcribe_controller.rb
+++ app/controllers/transcribe_controller.rb
@@ -26,23 +26,31 @@ class TranscribeController  < ApplicationController
     old_link_count = @page.page_article_links.count
     @page.attributes=params[:page]
     if params['save']
-      if @page.save
-        record_deed
-        # use the new links to blank the graphs
-        @page.clear_article_graphs
-        
-        new_link_count = @page.page_article_links.count
-        logger.debug("DEBUG old_link_count=#{old_link_count}, new_link_count=#{new_link_count}")
-        if old_link_count == 0 && new_link_count > 0
-          record_index_deed
-        end
-        @work.work_statistic.recalculate if @work.work_statistic
-        #redirect_to :action => 'display_page', :page_id => @page.id, :controller => 'display'
-        redirect_to :action => 'assign_categories', :page_id => @page.id
-      else
-        flash[:error] = @page.errors[:base].join('<br />')
-        render :action => 'display_page'
-      end    
+      log_transcript_attempt
+      begin
+        if @page.save
+          log_transcript_success
+          record_deed
+          # use the new links to blank the graphs
+          @page.clear_article_graphs
+          
+          new_link_count = @page.page_article_links.count
+          logger.debug("DEBUG old_link_count=#{old_link_count}, new_link_count=#{new_link_count}")
+          if old_link_count == 0 && new_link_count > 0
+            record_index_deed
+          end
+          @work.work_statistic.recalculate if @work.work_statistic
+          #redirect_to :action => 'display_page', :page_id => @page.id, :controller => 'display'
+          redirect_to :action => 'assign_categories', :page_id => @page.id
+        else
+          log_transcript_error
+          flash[:error] = @page.errors[:base].join('<br />')
+          render :action => 'display_page'
+        end    
+      rescue => ex
+        log_transcript_exception(ex)
+        raise ex
+      end
     elsif params['preview']
       @preview_xml = @page.generate_preview
       render :action => 'display_page'
@@ -165,6 +173,33 @@ class TranscribeController  < ApplicationController
   end
 
 protected
+  def log_transcript_attempt
+    # we have access to @page, @user, and params
+    @transcript_date = Time.now
+    log_message = "TRANSCRIPTION\t#{@transcript_date}\n"
+    log_message << "TRANSCRIPTION\tUser\tID: #{current_user.id}\tEmail: #{current_user.email}\tDisplay Name: #{current_user.display_name}\n"
+    log_message << "TRANSCRIPTION\tCollection\tID: #{@collection.id}\tTitle:#{@collection.title}\tOwner Email: #{@collection.owner.email}\n"
+    log_message << "TRANSCRIPTION\tWork\tID: #{@work.id}\tTitle: #{@work.title}\n"
+    log_message << "TRANSCRIPTION\tPage\tID: #{@page.id}\tPosition: #{@page.position}\tTitle:#{@page.title}\n"
+    log_message << "TRANSCRIPTION\tSource Text:\nBEGIN_SOURCE_TEXT\n#{params[:page][:source_text]}\nEND_SOURCE_TEXT\n\n"
+
+    logger.info(log_message)
+  end
+
+  def log_transcript_exception(ex)
+    log_message = "TRANSCRIPTION\t#{@transcript_date}\tERROR\tEXCEPTION\t"
+    logger.error(log_message + ex.message)
+  end
+
+  def log_transcript_error
+    log_message = "TRANSCRIPTION\t#{@transcript_date}\tERROR\t"
+    logger.info(@page.errors[:base].join("\t#{log_message}"))    
+  end
+  
+  def log_transcript_success
+    log_message = "TRANSCRIPTION\t#{@transcript_date}\tSUCCESS\t"
+    logger.info(log_message)
+  end
 
   def record_deed
     deed = stub_deed
