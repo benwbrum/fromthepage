ec3c2be2374dfa1e1c65b8e9fdced01df4b2f8f1
Ben W. Brumfield <benwbrum@gmail.com>
2014-09-19 06:30:18 -0500

    Copied environment globals from rails2_production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  # location of system calls on this machine
+  NEATO = '/usr/bin/neato'
+  RAKE = '/usr/bin/env rake'
 end
