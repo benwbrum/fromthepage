ba9d3e13539259823368f1cf184882d54aa551de
Ben W. Brumfield <benwbrum@gmail.com>
2014-11-24 09:37:49 -0600

    Refactored import code to model and removed obsolete action for #100

--- app/controllers/ia_controller.rb
+++ app/controllers/ia_controller.rb
@@ -9,98 +9,17 @@ class IaController < ApplicationController
   end
 
   def convert
-    work = Work.new
-    work.owner = current_user
-    work.title = @ia_work.title
-    work.description = @ia_work.description
-    work.physical_description = @ia_work.notes
-    work.author = @ia_work.creator
-    work.description += "<br/>Sponsored by: "+ @ia_work.sponsor
-    work.description += "<br/>Contributed by: "+ @ia_work.contributor
-    work.save!
-
-    @ia_work.ia_leaves.each do |leaf|
-      page = Page.new
-      page.base_image = nil
-      page.base_height = leaf.page_h
-      page.base_width = leaf.page_w
-      page.title = leaf.page_number
-      work.pages << page #necessary to make acts_as_list work here
-      work.save!
-      leaf.page_id = page.id
-      leaf.save!
-    end
-    work.save!
-    @ia_work.work = work
-    @ia_work.save!
+    work = @ia_work.convert_to_work
     flash[:notice] = "#{@ia_work.title} has been converted into a FromThePage work."
 
     redirect_to :controller => 'work', :action => 'edit', :work_id => work.id
 
-    #
-#    @image_set.titled_images.each do |titled_image|
-#      page = Page.new
-#      page.base_image = titled_image.original_file
-#      if File.exists?(page.base_image)
-#        image = Magick::ImageList.new(page.base_image)
-#        page.base_height = image.rows
-#        page.base_width = image.columns
-#        image = nil
-#        GC.start
-#      end
-#      # width
-#      # height
-#      page.shrink_factor = @image_set.original_to_base_halvings
-#      page.title = titled_image.title
-#      work.pages << page
-#    end
-#    work.save!
-#    redirect_to :controller => 'work', :action => 'edit', :work_id => work.id
   end
 
-
-
-  def purge_type_delete
-    leaves_to_delete = @ia_work.ia_leaves.find_all_by_page_type('Delete')
-    leaves_to_delete.each do |l|
-      IaLeaf.destroy(l.id)
-    end
-    flash[:notice] = "Delete leaves have been purged"
-    redirect_to :action => 'manage', :ia_work_id => @ia_work.id
-  end
 
   def title_from_ocr
-    loc_doc = fetch_loc_doc(@ia_work.book_id)
-    scandata_file, djvu_file = files_from_loc(loc_doc)
-
-    djvu_url =  "http://#{@ia_work.server}#{@ia_work.ia_path}/#{djvu_file}"
-    logger.debug(djvu_url)
-    djvu_doc = Nokogiri::XML(open(djvu_url))
-    leaf_objects = djvu_doc.search('OBJECT')
-    leaf_objects.each do |e|
-
-      page_id = e.search('PARAM[@name="PAGE"]').first['value']
-      page_id[/\w*_0*/]=""
-      page_id[/\.djvu/]=''
-      logger.debug(page_id)
-      # there may well be an off-by-one error in the source.  I'm seeing page_id 7
-      # correspond with leaf_id 6
-      leaf_number = page_id.to_i
-
-      line = e.search('LINE').first
-      if(line)
-        words = []
-
-        line.search('WORD').each { |e| words << e.inner_text.capitalize }
-        title = words.join(" ")
-        logger.debug(title)
-
-        ia_leaf = @ia_work.ia_leaves.find_by_leaf_number(leaf_number)
-        ia_leaf.page_number = title
-        ia_leaf.save!
-
-      end
-    end
+    @ia_work.title_from_ocr
+    
     flash[:notice] = "Pages have been renamed."
     redirect_to :action => 'manage', :ia_work_id => @ia_work.id
   end
@@ -126,108 +45,16 @@ class IaController < ApplicationController
     detail_url = params[:detail_url]
     id = detail_url.split('/').last
 
-    loc_doc = fetch_loc_doc(id)
-    location = loc_doc.search('results').first
-    server = location['server']
-    dir = location['dir']
-
     # pull relevant info about the work from here
     @ia_work = IaWork.new
-    @ia_work.server = server
-    @ia_work.ia_path = dir
-    @ia_work.user_id = @current_user.id
+    @ia_work.user = current_user
     @ia_work.detail_url = detail_url
 
-    @ia_work.book_id = loc_doc.search('identifier').text
-    @ia_work[:title] = loc_doc.search('title').text             #work title
-    @ia_work[:creator] = loc_doc.search('creator').text          #work author
-    @ia_work[:collection] = loc_doc.search('collection').text   #?
-    @ia_work[:description] = loc_doc.search('description').text #description
-    @ia_work[:subject] = loc_doc.search('subject').text         #description
-    @ia_work[:notes] = loc_doc.search('notes').text             #physical description
-    @ia_work[:contributor] = loc_doc.search('contributor').text #description
-    @ia_work[:sponsor] = loc_doc.search('sponsor').text         #description
-    @ia_work[:image_count] = loc_doc.search('imagecount').text
+    @ia_work.ingest_work(id)
 
-    image_format, archive_format = formats_from_loc(loc_doc)
-    logger.debug("image_format, archive_format = #{image_format}, #{archive_format}")
-    @ia_work[:image_format] = image_format
-    @ia_work[:archive_format] = archive_format
-    scandata_file, djvu_file, zip_file = files_from_loc(loc_doc)
-    @ia_work[:scandata_file] = scandata_file
-    @ia_work[:djvu_file] = djvu_file
-    @ia_work[:zip_file] = zip_file
-
-    @ia_work.save!
-    # now fetch the scandata.xml file and parse it
-    scandata_url = "http://#{server}#{dir}/#{scandata_file}" # will not work on new format: we cannot assume filenames are re-named with their content
-
-    sd_doc = Nokogiri::XML(open(scandata_url))
-
-    @pages = sd_doc.search('page')
-    @pages.each do |page|
-      leaf = IaLeaf.new
-      leaf.leaf_number = page['leafNum']
-      if nil == leaf.leaf_number
-        leaf.leaf_number = page['leafnum'] #bpoc installation downcases this for some reason
-      end
-      leaf.page_number = page.search('pagenumber').text
-      leaf.page_type = page.search('pagetype').text
-      leaf.page_w = page.search('w').text
-      leaf.page_h = page.search('h').text
-      @ia_work.ia_leaves << leaf
-
-      if leaf.page_type == 'Title'
-        @ia_work.title_leaf = leaf.leaf_number
-      end
-    end
-    @ia_work.save!
     flash[:notice] = "#{@ia_work.title} has been imported into your staging area."
 
     redirect_to :action => 'manage', :ia_work_id => @ia_work.id
-  end
-
-private
-  ARCHIVE_FORMATS = ['zip', 'tar']
-  IMAGE_FORMATS = ['jp2', 'jpg']
-
-  def formats_from_loc(loc_doc)
-    files = loc_doc.search 'file'
-    locations = files.map { |f| f['location'] }
-    # handle new upload format
-    if locations.uniq == [nil]
-      return ['jp2', 'zip']
-    end
-    # handle old upload format
-    ARCHIVE_FORMATS.each do |aft|
-      IMAGE_FORMATS.each do |ift|
-        suffix = "#{ift}.#{aft}"
-        if locations.count { |l| l.end_with? suffix} > 0
-          return [ift, aft]
-        end
-      end
-    end
-  end
-
-  def fetch_loc_doc(id)
-    # first get the call the location API and parse that document
-    api_url = 'http://www.archive.org/services/find_file.php?file='+id
-    logger.debug(api_url)
-    loc_doc = Nokogiri::XML(open(api_url))
-    return loc_doc
-  end
-
-  def files_from_loc(loc_doc)
-    formats = loc_doc.search('file').search('format')
-    scandata = formats.select{|e| e.inner_text=='Scandata'}.first.parent['name']
-    djvu = formats.select{|e| e.inner_text=='Djvu XML'}.first.parent['name']
-    zips = formats.select{|e| e.inner_text=='Single Page Processed JP2 ZIP'}
-    if zips.size < 1
-      zips = formats.select{|e| e.inner_text=='Single Page Processed JP2 Tar'}
-    end
-    zip = zips.first.parent['name']
-
-    return [scandata, djvu, zip]
   end
 
 end
--- app/models/ia_work.rb
+++ app/models/ia_work.rb
@@ -54,4 +54,174 @@ class IaWork < ActiveRecord::Base
 
   end
 
+
+
+  # IA importer code refactored from ia_controller.rb
+  def convert_to_work
+    work = Work.new
+    work.owner = self.user
+    work.title = self.title
+    work.description = self.description
+    work.physical_description = self.notes
+    work.author = self.creator
+    work.description += "<br/>Sponsored by: "+ self.sponsor
+    work.description += "<br/>Contributed by: "+ self.contributor
+    work.save!
+
+    self.ia_leaves.each do |leaf|
+      page = Page.new
+      page.base_image = nil
+      page.base_height = leaf.page_h
+      page.base_width = leaf.page_w
+      page.title = leaf.page_number
+      work.pages << page #necessary to make acts_as_list work here
+      work.save!
+      leaf.page_id = page.id
+      leaf.save!
+    end
+    work.save!
+    self.work = work
+    self.save!
+
+    work
+  end
+
+  def ingest_work(id)
+    
+    loc_doc = fetch_loc_doc(id)
+    location = loc_doc.search('results').first
+    server = location['server']
+    dir = location['dir']
+
+    self.server = server
+    self.ia_path = dir
+
+    self.book_id = loc_doc.search('identifier').text
+    self[:title] = loc_doc.search('title').text             #work title
+    self[:creator] = loc_doc.search('creator').text          #work author
+    self[:collection] = loc_doc.search('collection').text   #?
+    self[:description] = loc_doc.search('description').text #description
+    self[:subject] = loc_doc.search('subject').text         #description
+    self[:notes] = loc_doc.search('notes').text             #physical description
+    self[:contributor] = loc_doc.search('contributor').text #description
+    self[:sponsor] = loc_doc.search('sponsor').text         #description
+    self[:image_count] = loc_doc.search('imagecount').text
+
+    image_format, archive_format = formats_from_loc(loc_doc)
+    logger.debug("image_format, archive_format = #{image_format}, #{archive_format}")
+    self[:image_format] = image_format
+    self[:archive_format] = archive_format
+    scandata_file, djvu_file, zip_file = files_from_loc(loc_doc)
+    self[:scandata_file] = scandata_file
+    self[:djvu_file] = djvu_file
+    self[:zip_file] = zip_file
+
+    self.save!
+    # now fetch the scandata.xml file and parse it
+    scandata_url = "http://#{server}#{dir}/#{scandata_file}" # will not work on new format: we cannot assume filenames are re-named with their content
+
+    sd_doc = Nokogiri::XML(open(scandata_url))
+
+    @pages = sd_doc.search('page')
+    @pages.each do |page|
+      leaf = IaLeaf.new
+      leaf.leaf_number = page['leafNum']
+      if nil == leaf.leaf_number
+        leaf.leaf_number = page['leafnum'] #bpoc installation downcases this for some reason
+      end
+      leaf.page_number = page.search('pagenumber').text
+      leaf.page_type = page.search('pagetype').text
+      leaf.page_w = page.search('w').text
+      leaf.page_h = page.search('h').text
+      self.ia_leaves << leaf
+
+      if leaf.page_type == 'Title'
+        self.title_leaf = leaf.leaf_number
+      end
+    end
+    self.save!
+
+    self
+  end
+  
+  def title_from_ocr
+        loc_doc = fetch_loc_doc(self.book_id)
+    scandata_file, djvu_file = files_from_loc(loc_doc)
+
+    djvu_url =  "http://#{self.server}#{self.ia_path}/#{djvu_file}"
+    logger.debug(djvu_url)
+    djvu_doc = Nokogiri::XML(open(djvu_url))
+    leaf_objects = djvu_doc.search('OBJECT')
+    leaf_objects.each do |e|
+
+      page_id = e.search('PARAM[@name="PAGE"]').first['value']
+      page_id[/\w*_0*/]=""
+      page_id[/\.djvu/]=''
+      logger.debug(page_id)
+      # there may well be an off-by-one error in the source.  I'm seeing page_id 7
+      # correspond with leaf_id 6
+      leaf_number = page_id.to_i
+
+      line = e.search('LINE').first
+      if(line)
+        words = []
+
+        line.search('WORD').each { |e| words << e.inner_text.capitalize }
+        title = words.join(" ")
+        logger.debug(title)
+
+        ia_leaf = self.ia_leaves.find_by_leaf_number(leaf_number)
+        ia_leaf.page_number = title
+        ia_leaf.save!
+
+      end
+    end
+
+  end
+  
+  
+private
+  ARCHIVE_FORMATS = ['zip', 'tar']
+  IMAGE_FORMATS = ['jp2', 'jpg']
+
+  def formats_from_loc(loc_doc)
+    files = loc_doc.search 'file'
+    locations = files.map { |f| f['location'] }
+    # handle new upload format
+    if locations.uniq == [nil]
+      return ['jp2', 'zip']
+    end
+    # handle old upload format
+    ARCHIVE_FORMATS.each do |aft|
+      IMAGE_FORMATS.each do |ift|
+        suffix = "#{ift}.#{aft}"
+        if locations.count { |l| l.end_with? suffix} > 0
+          return [ift, aft]
+        end
+      end
+    end
+  end
+
+  def fetch_loc_doc(id)
+    # first get the call the location API and parse that document
+    api_url = 'http://www.archive.org/services/find_file.php?file='+id
+    logger.debug(api_url)
+    loc_doc = Nokogiri::XML(open(api_url))
+    return loc_doc
+  end
+
+  def files_from_loc(loc_doc)
+    formats = loc_doc.search('file').search('format')
+    scandata = formats.select{|e| e.inner_text=='Scandata'}.first.parent['name']
+    djvu = formats.select{|e| e.inner_text=='Djvu XML'}.first.parent['name']
+    zips = formats.select{|e| e.inner_text=='Single Page Processed JP2 ZIP'}
+    if zips.size < 1
+      zips = formats.select{|e| e.inner_text=='Single Page Processed JP2 Tar'}
+    end
+    zip = zips.first.parent['name']
+
+    return [scandata, djvu, zip]
+  end
+
+
 end
