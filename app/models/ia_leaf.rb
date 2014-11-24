7f248aa944f69f1be1b055980e96bb21035dc3f3
Ben W. Brumfield <benwbrum@gmail.com>
2014-11-24 09:41:35 -0600

    Refactored ia thumbnails to model code for #100

--- app/models/ia_leaf.rb
+++ app/models/ia_leaf.rb
@@ -2,4 +2,8 @@ class IaLeaf < ActiveRecord::Base
   self.table_name = "ia_leaves"
   belongs_to :ia_work
   belongs_to :page
+  
+  def thumb_url
+    "http://www.archive.org/download/#{ia_work.book_id}/page/leaf#{leaf_number}_thumb.jpg"
+  end
 end
