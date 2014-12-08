152ce10ff8e58c106480751736311dc7f04720d4
Ben W. Brumfield <benwbrum@gmail.com>
2014-12-08 06:24:55 -0600

    Implemented single-page toggle between translation and facsimile views for #81

--- app/helpers/display_helper.rb
+++ app/helpers/display_helper.rb
@@ -1,6 +1,15 @@
 module DisplayHelper
   include AbstractXmlHelper
 
+  def has_translation?
+    @work.supports_translation && !@page.source_translation.blank?
+  end
+
+  def translation_mode?
+    # this expects a page to exist
+    @work.supports_translation && params[:translation] && params[:translation] != 'false'
+  end
+
 
   def notes_for( commentable )
     render({:partial => 'note/notes',
