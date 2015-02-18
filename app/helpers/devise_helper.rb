82b0c615854514d31b3200dc2b31e11747bd9c1c
Nick Seryakov <nick.seryakov@gmail.com>
2015-02-18 05:56:31 +0400

    Devise helper to prevent showing flash messages

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,32 @@
+module DeviseHelper
+
+  def devise_error_messages!
+    flash_alerts = []
+    error_key = 'errors.messages.not_saved'
+
+    if !flash.empty?
+      flash_alerts.push(flash[:error]) if flash[:error]
+      flash_alerts.push(flash[:alert]) if flash[:alert]
+      flash_alerts.push(flash[:notice]) if flash[:notice]
+      error_key = 'errors.messages.not_signed'
+      flash.clear
+    end
+
+    return "" if resource.errors.empty? && flash_alerts.empty?
+    errors = resource.errors.empty? ? flash_alerts : resource.errors.full_messages
+
+    messages = errors.map { |msg| content_tag(:li, msg) }.join
+    sentence = I18n.t(error_key, :count    => errors.count,
+                                 :resource => resource.class.model_name.human.downcase)
+
+    html = <<-HTML
+    <div class="devise-error">
+      <h5 class="devise-error_title">#{sentence}</h5>
+      <ul class="devise-error_summary">#{messages}</ul>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
+end
