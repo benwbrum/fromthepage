eee4fc54cf2fbe858883baaadd13cc51bd49a300
Ben W. Brumfield <benwbrum@gmail.com>
2015-02-19 16:44:05 -0600

    Fixed mass assignment and link_to_function deprecation for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ActiveRecord::Base
   acts_as_tree :order => 'title'
   belongs_to :collection
   has_and_belongs_to_many :articles, :order => 'title', :uniq => true
-  attr_accessible :collection_id
+  attr_accessible :collection_id, :title
 
 #  def destroy_but_attach_children_to_parent
 #    self.children.each do |child|
