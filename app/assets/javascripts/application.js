2003ddd04b57956d014c1e637835f4f9e558a569
Nick Seryakov <nick.seryakov@gmail.com>
2015-02-26 04:31:46 +0400

    Dropdown component

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -44,9 +44,49 @@ $.fn.flashclose = function(s) {
   });
 };
 
+
+// Custom drop-down open/close toggle
+// $('element').dropdown();
+// <element data-dropdown='{ "selectable": true, "openclass": "visible"}'>
+$.fn.dropdown = function(s) {
+  s = $.extend({
+    trigger:    'dt',        // A child element which trigger open/close
+    items:      'dd > a',    // Clickable elements inside the drop-down
+    openclass:  'open',      // CSS class name for the open state
+    selectable: false        // TRUE to change trigger text on select
+  }, s || {});
+
+  return this.each(function() {
+    var $element = $(this);
+    var cfg = $.extend({}, s, $element.data('dropdown'));
+    var $trigger = $(cfg.trigger, this);
+
+    // Open/close when clicked on the trigger
+    $trigger.on('click.DropDown', function() {
+      $element.toggleClass(cfg.openclass);
+    });
+
+    // Close when clicked on an item
+    $(s.items, this).on('click.DropDown', function() {
+      $element.removeClass(cfg.openclass);
+      if(cfg.selectable) {
+        $trigger.text($(this).text());
+      }
+    });
+
+    // Close if clicked outside
+    $(document).on('click.DropDown', function(e) {
+      if($(e.target).closest($element).length === 0) {
+        $element.removeClass(cfg.openclass);
+      }
+    });
+  });
+};
+
 })(jQuery, window, document);
 
 
 $(function() {
   $('.flash').flashclose();
+  $('.dropdown').dropdown();
 });
