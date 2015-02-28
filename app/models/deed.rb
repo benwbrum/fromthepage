114f1fca2902573f8e55459e9632c0a62a148d20
Nick Seryakov <nick.seryakov@gmail.com>
2015-02-28 20:09:36 +0400

    Old deed partial templates deleted

--- app/models/deed.rb
+++ app/models/deed.rb
@@ -6,16 +6,6 @@ class Deed < ActiveRecord::Base
   ARTICLE_EDIT = 'art_edit'
   NOTE_ADDED = 'note_add'
 
-  SHORT_PARTIALS =
-    { PAGE_TRANSCRIPTION => 'deed/page_transcription_short.html.erb',
-      PAGE_EDIT => 'deed/page_edit_short.html.erb',
-      PAGE_INDEXED => 'deed/page_indexed_short.html.erb',
-      ARTICLE_EDIT => 'deed/article_edit_short.html.erb',
-      NOTE_ADDED => 'deed/note_added_short.html.erb' }
-
-  LONG_PARTIALS =
-    { NOTE_ADDED => 'deed/note_added_long.html.erb' }
-
   # associations
   belongs_to :article
   belongs_to :collection
@@ -25,14 +15,4 @@ class Deed < ActiveRecord::Base
   belongs_to :work
 
   validates_inclusion_of :deed_type, :in => [ PAGE_TRANSCRIPTION, PAGE_EDIT, PAGE_INDEXED, ARTICLE_EDIT, NOTE_ADDED ]
-
-  # tested
-  def short_partial
-    SHORT_PARTIALS[self.deed_type]
-  end
-
-  # tested
-  def long_partial
-    LONG_PARTIALS[self.deed_type] || SHORT_PARTIALS[self.deed_type]
-  end
 end
