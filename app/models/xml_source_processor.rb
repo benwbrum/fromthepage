a765fedff8159eddc2658c7ad6da422d878d109b
Ben W. Brumfield <benwbrum@gmail.com>
2015-03-08 12:26:27 -0500

    Removed debugger calls

--- app/models/xml_source_processor.rb
+++ app/models/xml_source_processor.rb
@@ -66,7 +66,6 @@ module XmlSourceProcessor
   #
   ##############################################
   def process_source
-    binding.pry
     if @text_dirty
       self.xml_text = wiki_to_xml(self.source_text)
     end
@@ -165,12 +164,11 @@ EOF
 
   def update_links_and_xml(xml_string, preview_mode=false)
     # first clear out the existing links
-    binding.pry
     clear_links
     processed = ""
     # process it
     doc = REXML::Document.new xml_string
-    binding.pry
+
     doc.elements.each("//link") do |element|
       # default the title to the text if it's not specified
       if !(title=element.attributes['target_title'])
@@ -184,7 +182,7 @@ EOF
       end
       debug("link display_text = #{display_text}")
       # create new blank articles if they don't exist already
-      binding.pry
+
       if !article = collection.articles.where(:title => title).first
         article = Article.new
         article.title = title
