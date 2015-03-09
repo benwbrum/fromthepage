77cf5710f5636704da9138c2e6ce6ef1afa8065a
Nick Seryakov <nick.seryakov@gmail.com>
2015-03-09 23:42:28 +0400

    Home page image changed, flash message delay time decreased

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,7 +21,7 @@
 // $('element').flash();
 $.fn.flashclose = function(s) {
   s = $.extend({
-    delay:      8000         // Auto close delay, 0 = don't close
+    delay:      4000         // Auto close delay, 0 = don't close
   }, s || {});
 
   return this.each(function() {
