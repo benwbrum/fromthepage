445f0306511d9cae3914ed016dee7d20cfee70b9
Nick Seryakov <nick.seryakov@gmail.com>
2015-03-31 07:01:34 +0400

    Resolved #115 - Modal dialog for "Import from Archive.org" form

--- app/controllers/ia_controller.rb
+++ app/controllers/ia_controller.rb
@@ -1,6 +1,11 @@
 class IaController < ApplicationController
   require 'open-uri'
+  include ActiveModel::Validations
+
   before_filter :load_ia_work_from_params
+
+  # no layout if xhr request
+  layout Proc.new { |controller| controller.request.xhr? ? false : nil }, :only => [:ia_book_form, :confirm_import]
 
   def load_ia_work_from_params
     unless params[:ia_work_id].blank?
@@ -15,24 +20,22 @@ class IaController < ApplicationController
     end
 
     work = @ia_work.convert_to_work
-    flash[:notice] = "#{@ia_work.title} has been converted into a FromThePage work."
-
+    flash[:notice] = "#{@ia_work.title} has been converted into a FromThePage work"
     redirect_to :controller => 'work', :action => 'edit', :work_id => work.id
-
   end
 
 
   def title_from_ocr_top
     @ia_work.title_from_ocr(:top)
-    
-    flash[:notice] = "Pages have been renamed with the top line of OCR text."
+
+    flash[:notice] = "Pages have been renamed with the top line of OCR text"
     redirect_to :action => 'manage', :ia_work_id => @ia_work.id
   end
 
   def title_from_ocr_bottom
     @ia_work.title_from_ocr(:bottom)
-    
-    flash[:notice] = "Pages have been renamed with the bottom line of OCR text."
+
+    flash[:notice] = "Pages have been renamed with the bottom line of OCR text"
     redirect_to :action => 'manage', :ia_work_id => @ia_work.id
   end
 
@@ -40,11 +43,16 @@ class IaController < ApplicationController
     @detail_url = params[:detail_url]
     #id = detail_url.split('/').last
 
-    @matches = IaWork.where(:detail_url => @detail_url)
-    if @matches.size() == 0
-      # nothing to do here
-      redirect_to :action => 'import_work', :detail_url => @detail_url
-      return
+    if @detail_url =~ /https?:\/\/(www\.)?archive\.org\/.+/
+      @matches = IaWork.where(:detail_url => @detail_url)
+      if @matches.size() == 0
+        # nothing to do here
+        ajax_redirect_to :action => 'import_work', :detail_url => @detail_url
+        return
+      end
+    else
+      errors.add(:base, "Please enter a valid Archive.org book URL to import")
+      render :action => 'ia_book_form'
     end
   end
 
@@ -54,6 +62,7 @@ class IaController < ApplicationController
       redirect_to dashboard_path
       return
     end
+
     detail_url = params[:detail_url]
     id = detail_url.split('/').last
 
@@ -64,8 +73,7 @@ class IaController < ApplicationController
 
     @ia_work.ingest_work(id)
 
-    flash[:notice] = "#{@ia_work.title} has been imported into your staging area."
-
+    flash[:notice] = "#{@ia_work.title} has been imported into your staging area"
     redirect_to :action => 'manage', :ia_work_id => @ia_work.id
   end
 
