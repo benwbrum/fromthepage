44c6078e33e4700db2accca9c3b6b193d35d0ecd
Nick Seryakov <nick.seryakov@gmail.com>
2015-04-22 03:29:34 +0400

    Resolved #150 - Restyle page version comparison

--- app/controllers/page_version_controller.rb
+++ app/controllers/page_version_controller.rb
@@ -1,2 +1,14 @@
 class PageVersionController < ApplicationController
+
+  before_filter :set_versions
+
+  def set_versions
+    @version_current = @page
+    @version_compare = @page_version.present? ? @page_version : @page.page_versions.first
+  end
+
+  def list
+    render 'show'
+  end
+
 end
