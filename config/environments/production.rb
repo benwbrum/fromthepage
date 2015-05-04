104e49e1276e654389785abe04ba95abee541647
saracarl <saracarl@gmail.com>
2015-05-04 15:18:14 -0500

    Digest turned back on; moved banner image out of asset pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Generate digests for assets URLs.
-  config.assets.digest = false
+  config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
