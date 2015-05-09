64a433a4009feeb5581be4386afba4021e658021
Nick Seryakov <nick.seryakov@gmail.com>
2015-05-09 21:42:40 +0400

    Resolved #171 - Preview lightbox and new feature view

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,10 @@
 class StaticController < ApplicationController
+
   def splash
+    if !session[:welcome_lightbox]
+      @show_welcome_lightbox = true
+      session[:welcome_lightbox] = true
+    end
   end
+
 end
