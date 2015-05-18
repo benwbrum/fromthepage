ba4c43d29dc625f06813aea775259c1337467848
Nick Seryakov <nick.seryakov@gmail.com>
2015-05-18 05:04:22 +0400

    #133 - Category manage view restyled and moved to the Subjects tab

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -2,36 +2,54 @@ class CategoryController < ApplicationController
   public :render_to_string
   protect_from_forgery :except => [:expand_category_for_work, :expand_category, :expand_article]
 
+  # no layout if xhr request
+  layout Proc.new { |controller| controller.request.xhr? ? false : nil }, :only => [:edit, :add_new, :update, :create]
+
   def expand_category
     render :layout => false
   end
+
   def expand_category_for_work
     render :layout => false
   end
+
   def expand_article
     render :layout => false
   end
 
+  def edit
+  end
+
+  def update
+    if @category.update_attributes(params[:category])
+      flash[:notice] = "Category has been updated"
+      ajax_redirect_to "#{request.env['HTTP_REFERER']}#category-#{@category.id}"
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def add_new
+    @new_category = Category.new({ :collection_id => @collection.id })
+    @new_category.parent = @category if @category.present?
+  end
 
   def create
-    category = Category.new(params[:category])
-    category.save!
-    redirect_to :action => 'manage', :collection_id => @collection.id
+    @new_category = Category.new(params[:category])
+    @new_category.parent = Category.find(params[:category][:parent_id]) if params[:category][:parent_id].present?
+    if @new_category.save
+      flash[:notice] = "Category has been created"
+      ajax_redirect_to "#{request.env['HTTP_REFERER']}#category-#{@new_category.id}"
+    else
+      render :action => 'add_new'
+    end
   end
 
   def delete
+    anchor = @category.parent_id.present? ? "#category-#{@category.parent_id}" : nil
     @category.destroy #_but_attach_children_to_parent
-    redirect_to :action => 'manage', :collection_id => @collection.id
-  end
-
-  def manage
-    @category = Category.new({ :collection_id => @collection.id })
-  end
-
-  def add_child
-    @category.children.create(:collection_id => @collection.id,
-                              :title => params[:title])
-    redirect_to :action => 'manage', :collection_id => @collection.id
+    flash[:notice] = "Category has been deleted"
+    redirect_to "#{request.env['HTTP_REFERER']}#{anchor}"
   end
 
 end
