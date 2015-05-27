ac8172a8b704d077c853e0ccdde9f30f8bee1117
Nick Seryakov <nick.seryakov@gmail.com>
2015-05-27 23:24:42 +0400

    #150 - Separated javascript files

---
+++ app/assets/javascripts/textdiff.js
@@ -0,0 +1,3 @@
+// Text diff match javascript files
+//
+//= require_tree ./textdiff
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,2 @@
 Rails.application.config.assets.precompile += %w( bookreader.js )
+Rails.application.config.assets.precompile += %w( textdiff.js )
