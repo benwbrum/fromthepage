381aca915b1e0bbc079f6151a5438b16376acba4
Nick Seryakov <nick.seryakov@gmail.com>
2015-05-28 00:27:20 +0400

    #152 - Article versions diff match

--- app/controllers/article_version_controller.rb
+++ app/controllers/article_version_controller.rb
@@ -1,2 +1,14 @@
 class ArticleVersionController < ApplicationController
+
+  before_filter :set_versions
+
+  def set_versions
+    @selected_version = @article_version.present? ? @article_version : @article.article_versions.first
+    @previous_version = @selected_version.prev
+  end
+
+  def list
+    render 'show'
+  end
+
 end
--- app/models/article_version.rb
+++ app/models/article_version.rb
@@ -1,4 +1,9 @@
 class ArticleVersion < ActiveRecord::Base
   belongs_to :article
   belongs_to :user
+
+  def prev
+    article.article_versions.where("id < ?", id).first
+  end
+
 end
