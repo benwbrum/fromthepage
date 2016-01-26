0bc489cf7976a1226ad0351402f1a2bc9c7b7048
Nick Seryakov <nick.seryakov@gmail.com>
2016-01-26 20:11:32 +0400

    Resolved #202 - auto fullscreen

--- app/controllers/transcribe_controller.rb
+++ app/controllers/transcribe_controller.rb
@@ -14,6 +14,7 @@ class TranscribeController  < ApplicationController
   end
 
   def display_page
+    @auto_fullscreen = cookies[:auto_fullscreen] || 'no';
     @layout_mode = cookies[:transcribe_layout_mode] || 'ltr';
     if params[:preview] == 'true'
       @preview_xml = @page.generate_preview
