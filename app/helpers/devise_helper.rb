7c58f523d8015c6f0d3daf8eb6ce6459fbff1573
Nick Seryakov <nick.seryakov@gmail.com>
2016-05-02 23:48:27 +0400

    Resolved #284 - The appropriate style for a 'success' devise messages

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -5,10 +5,17 @@ module DeviseHelper
     error_key = 'errors.messages.not_saved'
 
     if !flash.empty?
-      flash_alerts.push(flash[:error]) if flash[:error]
-      flash_alerts.push(flash[:alert]) if flash[:alert]
-      flash_alerts.push(flash[:notice]) if flash[:notice]
       error_key = 'errors.messages.not_signed'
+
+      if flash[:error]
+        flash_alerts.push(flash[:error])
+      elsif flash[:alert]
+        flash_alerts.push(flash[:alert])
+      elsif flash[:notice]
+        flash_alerts.push(flash[:notice])
+        error_key = nil
+      end
+
       flash.clear
     end
 
@@ -16,15 +23,22 @@ module DeviseHelper
     errors = resource.errors.empty? ? flash_alerts : resource.errors.full_messages
 
     messages = errors.map { |msg| content_tag(:li, msg) }.join
-    sentence = I18n.t(error_key, :count    => errors.count,
-                                 :resource => resource.class.model_name.human.downcase)
 
-    html = <<-HTML
-    <div class="validation">
-      <h5 class="validation_title">#{sentence}</h5>
-      <ul class="validation_summary">#{messages}</ul>
-    </div>
-    HTML
+    if error_key
+      sentence = I18n.t(error_key, :count => errors.count, :resource => resource.class.model_name.human.downcase)
+      html = <<-HTML
+      <div class="validation">
+        <h5 class="validation_title">#{sentence}</h5>
+        <ul class="validation_summary">#{messages}</ul>
+      </div>
+      HTML
+    else
+      html = <<-HTML
+      <div class="validation success">
+        <ul class="validation_summary">#{messages}</ul>
+      </div>
+      HTML
+    end
 
     html.html_safe
   end
