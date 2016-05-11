31a13cbcd1ae8abfeda7a92a9a307632b055e4c1
saracarl <saracarl@gmail.com>
2016-05-11 14:36:21 -0500

    to run rails migrations during deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,7 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
+server 'fromthepage.com', user: 'deploy', roles: %w{web app db}
 set :application, 'fromthepage'
 set :repo_url, 'git@github.com:benwbrum/fromthepage.git'
 
