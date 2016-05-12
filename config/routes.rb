3eda6a777df0bfc336025b728ca1a6cf3aa90ee4
saracarl <saracarl@gmail.com>
2016-05-12 11:09:09 -0500

    remove hardcoded routes for open source version

--- config/routes.rb
+++ config/routes.rb
@@ -39,9 +39,6 @@ Fromthepage::Application.routes.draw do
 #  get   'document_set/:document_set_id', :to => 'document_sets#show'
 #  resources :document_sets
 
-  get   'ZenasMatthews' => 'collection#show', :collection_id => 7
-  get   'JuliaBrumfield' => 'collection#show', :collection_id => 1
-
   patch 'work/update_work', :to => 'work#update_work'
   patch 'transcribe/save_transcription', :to => 'transcribe#save_transcription'
   patch 'transcribe/save_translation', :to => 'transcribe#save_translation'
