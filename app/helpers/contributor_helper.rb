219678f9fa7544cab9becfa120d4a2cb35bc7c49
Trisha <trishablewis@gmail.com>
2016-11-17 11:38:17 -0500

    #451, added ocr deeds to collaborators tab and activity list

--- app/helpers/contributor_helper.rb
+++ app/helpers/contributor_helper.rb
@@ -5,6 +5,7 @@ module ContributorHelper
 
       #set variables
       trans_type = ["page_trans", "page_edit"]
+      ocr_type = "ocr_corr"
       note_type = "note_add"
       article_type = "art_edit"
       translate_type = ["pg_xlat", "pg_xlat_ed"]
@@ -22,12 +23,14 @@ module ContributorHelper
       note_deeds = @collection.deeds.where(deed_type: note_type)
       article_deeds = @collection.deeds.where(deed_type: article_type)
       translate_deeds = @collection.deeds.where(deed_type: translate_type)
+      ocr_deeds = @collection.deeds.where(deed_type: ocr_type)
       
       #find deeds for the date range
       @recent_notes = note_deeds.where(condition, start_date, end_date)
       @recent_transcriptions = transcription_deeds.where(condition, start_date, end_date)
       @recent_articles = article_deeds.where(condition, start_date, end_date)
       @recent_translations = translate_deeds.where(condition, start_date, end_date)
+      @recent_ocr = ocr_deeds.where(condition, start_date, end_date)
 
       #get distinct user ids per deed and create list of users
       user_deeds = transcription_deeds.distinct.pluck(:user_id)
