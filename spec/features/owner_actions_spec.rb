4c4d76283a2dc915b03b4ed210967b3caf7e8ac6
Trisha <trishablewis@gmail.com>
2016-12-01 10:33:06 -0500

    updated spec for adding empty work

--- spec/features/owner_actions_spec.rb
+++ spec/features/owner_actions_spec.rb
@@ -8,6 +8,7 @@ describe "owner actions", :order => :defined do
     @collections = @user.all_owner_collections
     @collection = @collections.first
     @works = @user.owner_works
+    @title = "This is an empty work"
   end
 
   it "starts a new project from tab" do
@@ -24,6 +25,16 @@ describe "owner actions", :order => :defined do
     #separate test due to the way the rake task runs
   end
 
+  it "fails to upload a document" do
+    login_as(@user, :scope => :user)
+    visit dashboard_owner_path
+    page.find('.tabs').click_link("Start A Project")
+    select(@collections.first.title, :from => 'document_upload_collection_id')
+    click_button('Upload File')
+    expect(page).to have_content("prohibited the form from being saved")
+    expect(page).to have_content("File can't be blank")
+  end
+
   it "creates a new collection" do
     collection_count = @user.all_owner_collections.count
     login_as(@user, :scope => :user)
@@ -36,7 +47,24 @@ describe "owner actions", :order => :defined do
     expect(page).to have_content("#{test_collection.title}")
     expect(page).to have_content("Manage Works")
   end
-
+=begin
+  #this test applies once bug-fix-460 branch is merged
+  it "creates an empty new work in a collection" do
+    test_collection = Collection.find_by(title: 'New Test Collection')
+    work_title = "New Test Work"
+    login_as(@user, :scope => :user)
+    visit dashboard_owner_path
+    click_link("#{test_collection.title}")
+    click_link("Add a new work")
+    expect(page).to have_content("#{test_collection.title}")
+    expect(page).to have_content("Create Empty Work")
+    fill_in 'work_title', with: work_title
+    fill_in 'work_description', with: "This work contains no pages."
+    click_button('Create Work')
+    expect(page).to have_content("Here you see the list of all pages in the work.")
+    expect(Work.find_by(title: work_title)).not_to be nil
+  end
+=end
   it "deletes a collection" do
     test_collection = Collection.find_by(title: 'New Test Collection')
     collection_count = @user.all_owner_collections.count
@@ -82,7 +110,19 @@ describe "owner actions", :order => :defined do
   end
 
 =begin
-These tests apply after bug-fix-460 branch has been merged
+#These tests apply after bug-fix-460 branch has been merged
+
+  it "fails to create an empty work" do
+    login_as(@user, :scope => :user)
+    visit dashboard_owner_path
+    page.find('.tabs').click_link("Start A Project")
+    select(@collections.last.title, :from => 'work_collection_id')
+    fill_in 'work_description', with: "This work should fail to create."
+    click_button('Create Work')
+    expect(page).to have_content("Create Empty Work")
+    expect(page).to have_content("Title can't be blank")
+  end
+
   it "creates an empty work" do
     login_as(@user, :scope => :user)
     visit dashboard_owner_path
@@ -103,15 +143,29 @@ These tests apply after bug-fix-460 branch has been merged
     page.find('.tabs').click_link("Pages")
     page.find('a', text: "Add New Page").click
     attach_file('page_base_image', './test_data/uploads/JWGravesAmnestyPage1.jpg')
-    click_button('Save')
-    pages = Work.find_by(title: @title).pages
+    click_button('Save & Add Next Page')
+    work = Work.find_by(title: @title)
+    pages = work.pages
     expect(pages).not_to be nil
-    expect(page).to have_content("Categories")
     expect(page).to have_content(pages.first.title)
-    #to save and next page involves ajax
-    #expect(h1).to have_content(Add New Page)
-    #attach_file('page_base_image', './test_data/uploads/JWGravesAmnestyPage2.jpg')
-    #click_button('Save')
+    page.find('a', text: "Add New Page").click
+    attach_file('page_base_image', './test_data/uploads/JWGravesAmnestyPage2.jpg')
+    click_button('Save & New Work')
+    count = work.pages.count
+    expect(count).to eq 2
+    expect(page).to have_content("Create Empty Work")
+    #testing the cancel button involves ajax
   end
-=end
+
+  it "deletes a work" do
+    login_as(@user, :scope => :user)
+    visit dashboard_owner_path
+    page.find('a', text: @title).click
+    expect(page).to have_content(@title)
+    expect(page).to have_content("Work title")
+    click_link("Delete Work")
+    expect(page.current_path).to eq dashboard_owner_path
+    expect(page).not_to have_content(@title)
+  end
+=end  
 end
