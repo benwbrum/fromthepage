99524b9a12b48e55896ad4f7caff2d4bc25adb99
Trisha <trishablewis@gmail.com>
2017-01-18 16:13:45 -0500

    autolinking, #495

--- app/controllers/abstract_xml_controller.rb
+++ app/controllers/abstract_xml_controller.rb
@@ -7,7 +7,11 @@ module AbstractXmlController
   # belongs here.
   ##############################################
 
+  #constant - words to ignore when autolinking
+  STOPWORDS = ["Mrs", "Mr.", "Dr."]
+
   def autolink(text)
+    #find the list of articles
     sql = 'select distinct article_id, '+
           'display_text '+
           'from page_article_links ' +
@@ -17,37 +21,41 @@ module AbstractXmlController
     logger.debug(sql)
     matches =
       Page.connection.select_all(sql).to_a
-
     # Bug 18 -- longest possible match is best
     matches.sort! { |x,y| x['display_text'].length <=> y['display_text'].length }
     matches.reverse!
-
+    #for each article, check text to see if it needs to be linked
     for match in matches
-      match_regex = Regexp.new('\b'+match['display_text'].gsub('?', '\?')+'\b')
+      match_regex = Regexp.new('\b'+match['display_text'].gsub('?', '\?').gsub('.', '\.')+'\b', Regexp::IGNORECASE)
       display_text = match['display_text']
       logger.debug("DEBUG looking for #{match_regex}")
-      if text.match match_regex
-        logger.debug("DEBUG found #{match_regex}")
-        # is this already within a link?
-
-        match_start = text.index match_regex
-        if word_not_okay(text, match_start, display_text)||within_link(text, match_start)
-          # within a link, but try again somehow
-        else
-          # not within a link, so create a new one
-          logger.debug("DEBUG #{match_regex} is not a link 2")
-          article = Article.find(match['article_id'].to_i)
-          # Bug 19 -- simplify when possible
-          if article.title == display_text
-            text.sub!(match_regex, "[[#{article.title}]]")
+      if display_text.in?(STOPWORDS)
+        #if the match is a stopword, ignore it and move to the next match
+                
+      else
+        #find each match to the regex in the text and create an array of the indices
+        positions = text.enum_for(:scan, match_regex).map {Regexp.last_match.begin(0)}
+        
+        for position in positions
+          #check to see if the regex is already within a link, from each index
+          if word_not_okay(text, position, display_text) || within_link(text, position)
+            #if it's in a link, don't do anything  
           else
-            text.sub!(match_regex, "[[#{article.title}|#{display_text}]]")
+            # not within a link, so create a new one
+            article = Article.find(match['article_id'].to_i)
+            # Bug 19 -- simplify when possible
+            if article.title == display_text
+              text.sub!(/\b(?<!\[\[)#{match_regex}(?!\]\]\b)/, "[[#{article.title}]]")
+            else
+              text.sub!(/\b(?<!\[\[)#{match_regex}(?!\]\])\b/, "[[#{article.title}|#{display_text}]]")
+            end
+          end
           end
         end
       end
-    end
     return text
   end
+
   # check for word boundaries on preceding and following sides
   def word_not_okay(text, index, display_text)
     # test for characters before the display text
