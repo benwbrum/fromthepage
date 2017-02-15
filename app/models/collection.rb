42b4241dfc3a98daeb72a19c705399fc8abb6c5e
Trisha <trishablewis@gmail.com>
2017-02-15 11:07:49 -0500

    #524 - document set deletion

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -6,7 +6,7 @@ class Collection < ActiveRecord::Base
   has_many :works, -> { order 'title' } #, :order => :position
   has_many :notes, -> { order 'created_at DESC' }
   has_many :articles
-  has_many :document_sets, -> { order 'title' }
+  has_many :document_sets, -> { order 'title' }, :dependent => :destroy
   has_many :categories, -> { order 'title' }
   has_many :deeds, -> { order 'created_at DESC' }
   belongs_to :owner, :class_name => 'User', :foreign_key => 'owner_user_id'
