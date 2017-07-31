41d55516e1eb0e16a90b8642fe4bb5e1c5f0e8cc
Trisha <trishablewis@gmail.com>
2017-07-31 16:33:46 -0400

    #736 hide completed works

---
+++ app/helpers/collection_helper.rb
@@ -0,0 +1,34 @@
+module CollectionHelper
+  def work_stats(work)
+    unless work.supports_translation
+      @progress_annotated = work.work_statistic.pct_annotated.round
+      @progress_review = work.work_statistic.pct_needs_review.round
+      @progress_completed = work.work_statistic.pct_completed.round
+
+      if work.ocr_correction
+        @completed = "corrected"
+      else
+        @completed = "transcribed"
+      end
+    else
+      @progress_annotated = work.work_statistic.pct_translation_annotated.round
+      @progress_review = work.work_statistic.pct_translation_needs_review.round
+      @progress_completed = work.work_statistic.pct_translation_completed.round
+      @completed = "translated"
+    end
+
+    if @collection.subjects_disabled
+      unless @progress_review == 0
+        @wording = "#{@progress_completed}% #{@completed}, #{@progress_review}% needs review"
+      else
+        @wording = "#{@progress_completed}% #{@completed}"
+      end
+    elsif @progress_review == 0
+      @wording = "#{@progress_annotated}% indexed, #{@progress_completed}% #{@completed}"
+    else
+      @wording = "#{@progress_annotated}% indexed, #{@progress_completed}% #{@completed}, #{@progress_review}% needs review"
+    end
+    
+  end
+
+end
