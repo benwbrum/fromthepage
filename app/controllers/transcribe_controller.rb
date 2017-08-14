d8297e1e575beb2ffc3910ac71e489765048c433
Ben W. Brumfield <benwbrum@gmail.com>
2017-08-14 16:30:54 -0500

    Save action should not move user to display of the same page

--- app/controllers/transcribe_controller.rb
+++ app/controllers/transcribe_controller.rb
@@ -92,6 +92,7 @@ class TranscribeController  < ApplicationController
       begin
         if @page.save
           log_transcript_success
+          flash[:notice] = "Saved"
           if @page.work.ocr_correction
             record_correction_deed
           else
@@ -170,9 +171,9 @@ class TranscribeController  < ApplicationController
     end
     # no uncategorized articles found, skip to display
     if @translation
-      redirect_to collection_display_page_path(@collection.owner, @collection, @page.work, @page.id, translation: true)
+      redirect_to collection_translate_page_path(@collection.owner, @collection, @work, @page.id)
     else
-      redirect_to collection_display_page_path(@collection.owner, @collection, @page.work, @page.id)
+      redirect_to collection_transcribe_page_path(@collection.owner, @collection, @work, @page.id)
     end
   end
 
