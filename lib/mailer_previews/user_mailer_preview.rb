1cdc1777de9b22275e493864aba98c81decac6d5
Trisha <trishablewis@gmail.com>
2017-09-20 13:17:27 -0400

    user notifications iiif collection import

--- lib/mailer_previews/user_mailer_preview.rb
+++ lib/mailer_previews/user_mailer_preview.rb
@@ -1,16 +1,7 @@
 class UserMailerPreview < ActionMailer::Preview
 
-  def iiif_collection_import_failed
-    manifests = ScManifest.last(5)
-    errors = {}
-    manifests.each do |m|
-      errors.store(m.at_id, m.label)
-    end
-    UserMailer.iiif_collection_import_failed(916, 168, errors)
+  def upload_finished
+    UserMailer.upload_finished(DocumentUpload.last)
   end
 
-  def iiif_collection_import_succeeded
-    UserMailer.iiif_collection_import_succeeded(916, 168)
-  end
-  
 end
