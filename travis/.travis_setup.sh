f6ba58409eded1a9c95a357e8273088bed7e8496
Trisha <trishablewis@gmail.com>
2017-11-15 12:37:38 -0500

    fixed typo in travis setup

--- travis/.travis_setup.sh
+++ travis/.travis_setup.sh
@@ -2,10 +2,10 @@ set -eux
 
 # Install MySQL 5.7 if DB=mysql57
 if [[ -n ${DB-} && x$DB =~ ^xmysql57 ]]; then
-  sudo bash .travis_mysql57.sh
+  sudo bash travis/.travis_mysql57.sh
 fi
 
 #Update mysql connector for maria db
 if [[ -n ${DB-} && x$DB =~ ^xmariadb ]]; then
-  sudo bash .travis_mariadb.sh
+  sudo bash travis/.travis_mariadb.sh
 fi
