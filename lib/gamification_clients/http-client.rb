f93c1461522536b64424139b525f2121e4cb1365
cbracco <bracco66.cb@gmail.com>
2017-12-08 14:17:16 -0300

    Gamificacion: Agrego cliente metagame

---
+++ lib/gamification_clients/http-client.rb
@@ -0,0 +1,48 @@
+require 'net/http'
+
+class HttpClient
+
+    def initialize(baseURL,headers = {})
+        @baseURL = baseURL
+        @headers = headers
+    end
+
+    private
+    def do_post(path, params = {}, bodyParams = {})
+        send_request(Net::HTTP::Post, path, params, bodyParams)
+    end
+
+    private
+    def do_get(path, params = {})
+        send_request(Net::HTTP::Get, path, params)
+    end
+
+    private
+    def do_put(path, params = {}, bodyParams = {})
+        send_request(Net::HTTP::Put, path, params, bodyParams)
+    end
+
+    private
+    def do_delete(path, params = {}, bodyParams = {})
+        send_request(Net::HTTP::Delete, path, params, bodyParams)
+    end
+
+    private
+    def send_request(methodClass, path, params = {}, bodyParams = {})
+        uri=URI(@baseURL)
+        uri.path=path
+        uri.query=URI.encode_www_form(params)
+
+        req = methodClass.new(uri)
+        # Set headers in request
+        @headers.each do |key, value|
+            req[key] = value
+        end
+        req.set_form_data(bodyParams)
+
+        res = Net::HTTP.start(uri.hostname,uri.port, :use_ssl => uri.scheme == 'https') {|http|
+            http.request(req)
+        }
+    end
+
+end
---
+++ lib/gamification_clients/metagame-client.rb
@@ -0,0 +1,69 @@
+require_relative 'http-client'
+
+class MetagameClient < HttpClient
+
+    def initialize(baseURL, token)
+        headers = {"Authorization":'Token token="'+ token +'"'}
+        super(baseURL,headers)
+    end
+
+    #### Players Methods #####
+
+    def list_players(filters = {})
+        do_get('/players', filters)
+    end
+
+    def get_player(id)
+        do_get("/players/#{id}")
+    end
+
+    def add_player(email)
+        do_post("/players",{email:email})
+    end
+
+    def update_player(id,email)
+        do_put("/players/#{id}",{email:email})
+    end
+
+    def delete_player(id)
+        do_delete("/players/#{id}")
+    end
+
+    def player_info(email)
+        do_get("/player_info",{email:email})
+    end
+
+    #### Badges Methods ####
+
+    def list_badges(filters = {})
+        do_get("/badges",filters)
+    end
+
+    def add_badge(badge)
+        do_post("/badges",badge)
+    end
+
+    def update_badge(badge)
+        do_put("/badges",badge)
+    end
+
+    def delete_badge(id)
+        do_delete("/badges/#{id}")
+    end
+
+    #### Records Methods ####
+
+    def list_records(filters = {})
+        do_get("/records", filters)
+    end
+
+    def list_records_type(type, filters = {})
+        do_get("/records/#{type}",filters)
+    end
+
+    #### Activities Methods ####
+    
+    def make_activity(params)
+        do_get("/activities",params)
+    end
+end
