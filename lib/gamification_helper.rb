872d29bfc91547978ee96b0049dd725cd48af908
cbracco <bracco66.cb@gmail.com>
2017-12-09 17:33:09 -0300

    Middleman gemificacion: creo metodos de clase y muevo el archivo a lib

--- lib/gamification_helper.rb
+++ lib/gamification_helper.rb
@@ -1,45 +1,45 @@
-class MiddlemanMetagame
+require 'gamification_clients/metagame-client'
 
-    def initialize()
-        #Deberia tomar los params de la config
-        @metagameClient = MetagameClient.new("URL","token")
+class GamificationHelper
 
-    end
+    @@metagameClient = MetagameClient.new(ENV['METAGAME_URL'],ENV['METAGAME_TOKEN'])
 
-    def loginEvent(mail)
+    def self.loginEvent(mail)
         activity={"email":mail,"project":"cientopolis","event":"login","count":1}
         @metagameClient.make_activity(activity)
 
     end
 
-    def registerEvent(mail)
+    def self.registerEvent(mail)
         activity={"email":mail,"project":"cientopolis","event":"register","count":1}
         @metagameClient.make_activity(activity)
     end
 
-    def newProjectEvent()
+    def self.newProjectEvent()
         activity={"email":mail,"project":"cientopolis","event":"new_project","count":1}
         @metagameClient.make_activity(activity)
     end
 
-    def createWorkEvent()
+    def self.createWorkEvent()
         activity={"email":mail,"project":"cientopolis","event":"createWork","count":1}
         @metagameClient.make_activity(activity)
     end
 
-    def pushCollectionEvent()
+    def self.pushCollectionEvent()
         activity={"email":mail,"project":"cientopolis","event":"login","count":1}
         @metagameClient.make_activity(activity)
     end
 
-    def upgradeTranscriptionEvent()
+    def self.upgradeTranscriptionEvent()
         activity={"email":mail,"project":"cientopolis","event":"login","count":1}
         @metagameClient.make_activity(activity)
     end
 
-    def getBadgesEvent()
+    def self.getBadgesEvent()
     end
 
+    def self.clientParams
+        puts ENV['METAGAME_URL']
+    end
 
 end
-
