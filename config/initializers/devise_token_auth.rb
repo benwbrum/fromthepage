ef1d05268bcb9d45356378cc348a0b2fa2f81c5c
cbracco <bracco66.cb@gmail.com>
2018-01-06 19:43:38 -0300

    Token Authentication: token generation on user model persist

---
+++ config/initializers/devise_token_auth.rb
@@ -0,0 +1,10 @@
+Devise::TokenAuthenticatable.setup do |config|
+  
+  # enable reset of the authentication token before the model is saved,
+  # defaults to false
+  config.should_reset_authentication_token = true
+
+  # enables the setting of the authentication token - if not already - before the model is saved,
+  # defaults to false
+  config.should_ensure_authentication_token = true
+end
