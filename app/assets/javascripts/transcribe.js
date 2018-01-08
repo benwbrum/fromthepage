cb09a7983ae41407ea6c4ca65a7aa47369dfe4bb
Trisha <trishablewis@gmail.com>
2018-01-08 13:35:09 -0500

    voice recognition jquery

--- app/assets/javascripts/transcribe.js
+++ app/assets/javascripts/transcribe.js
@@ -1,7 +1,8 @@
 //functions to allow for speech to text
 
-function startDictation(){
+function startDictation(form){
   if (window.hasOwnProperty('webkitSpeechRecognition')){
+    var voiceData = form.find('textarea')[0]
     var recognition = new webkitSpeechRecognition();
     recognition.continuous = true;
     recognition.interimResults = false;
@@ -13,14 +14,16 @@ function startDictation(){
       for (var i = e.resultIndex; i < e.results.length; ++i){
           final_transcript += e.results[i][0].transcript;
       }
-      $('#transcription')[0].innerHTML = final_transcript;
+      voiceData.innerHTML = final_transcript;
     };
     recognition.onerror = function(e){
       recognition.stop();
     }
-    $('#stop').click(function(e){
+    $('#stop_img').click(function(e){
       e.preventDefault();
       recognition.stop();
+      $(this).hide();
+      $('#start_img').show();
     })
   }
 }
