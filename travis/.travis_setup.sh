b3f78b15ccca480b1d50c081f39d2bfc2c0314ac
Trisha <trishablewis@gmail.com>
2018-01-18 10:07:43 -0500

    changes to travis setup

--- travis/.travis_setup.sh
+++ travis/.travis_setup.sh
@@ -1,13 +1,5 @@
 set -eux
 
-sudo apt-get -qq update
-sudo apt-get install ghostscript
-sudo apt-get install graphviz
-sudo apt-get install qt5-default libqt5webkit5-dev gstreamer1.0-plugins-base gstreamer1.0-tools gstreamer1.0-x
-qtchooser -qt=qt5
-gem uninstall capybara-webkit
-gem install capybara-webkit
-
 # Install MySQL 5.7 if DB=mysql57
 if [[ -n ${DB-} && x$DB =~ ^xmysql57 ]]; then
   sudo bash travis/.travis_mysql57.sh
