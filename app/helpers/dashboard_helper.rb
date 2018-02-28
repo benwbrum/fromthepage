bac73e61566ea5d68f21e6518727d08c59d0c1a0
Trisha <trishablewis@gmail.com>
2018-02-28 11:11:14 -0500

    updated landing page

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -6,11 +6,12 @@ module DashboardHelper
   end
 
   def find_link_work(c)
+
     work = c.works.joins(:work_statistic).where.not(work_statistics: {complete: 100}).distinct.sample(1)
   end
 
   def owner_projects(owner)
-    owner.all_owner_collections
+    owner.all_owner_collections.unrestricted
   end
 
 end
