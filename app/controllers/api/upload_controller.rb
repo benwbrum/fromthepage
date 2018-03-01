a4ae875f8de34b1233822030ea1127c02fd2fea0
cbracco <bracco66.cb@gmail.com>
2018-03-01 20:13:19 -0300

    Upload Endpoint

---
+++ app/controllers/api/upload_controller.rb
@@ -0,0 +1,26 @@
+class Api::UploadController < Api::ApiController
+  
+  def create
+    @document_upload = DocumentUpload.new(params[:document_upload])
+    @document_upload.user = current_user
+
+    if @document_upload.save
+
+      if SMTP_ENABLED
+        begin
+          # SystemMailer.new_upload(@document_upload).deliver!
+          # "Document has been uploaded and will be processed shortly. We'll email you at #{@document_upload.user.email} when ready."
+        rescue StandardError => e
+          log_smtp_error(e, current_user)
+        end
+      end
+      # ajax_redirect_to controller: 'collection', action: 'show', collection_id: @document_upload.collection.id
+      @document_upload.submit_process
+      render_serialized ResponseWS.simple_ok('api.upload.create.success')
+    else
+      # render action: 'upload'
+      render_serialized ResponseWS.simple_error('api.upload.create.error')
+    end
+  end
+
+end
