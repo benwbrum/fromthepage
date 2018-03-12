f4f3e28de257aea679b10c984d451ea92e60c8de
saracarl <saracarl@gmail.com>
2018-03-12 11:45:44 -0500

    cap deploy to link initializer

--- config/initializers/01fromthepage.rb
+++ config/initializers/01fromthepage.rb
@@ -1,6 +1,6 @@
 ADMIN_EMAILS = 'saracarl@gmail.com, benwbrum@gmail.com'
 SENDING_EMAIL_ADDRESS = 'support@fromthepage.com'
-SMTP_ENABLED = true
+SMTP_ENABLED = false
 USE_PNG_LOGO = false
 GUEST_DEED_COUNT = 3
 FACEBOOK_PIXEL_ID = ''
