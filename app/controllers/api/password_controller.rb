1bcce1e421e43ce64d9c221baae82ac819c8e596
cbracco <bracco66.cb@gmail.com>
2018-04-16 23:40:34 -0300

    base endpoints

--- app/controllers/api/password_controller.rb
+++ app/controllers/api/password_controller.rb
@@ -1,16 +1,28 @@
-require 'gamification_helper'
 class Api::PasswordController < Api::ApiController
+  
+  def public_actions
+    return [:create,:confirm]
+  end
  
   def create
-    #puts "create!!!!"
-   #puts User.reset_password_keys("new_password", "new_password") 
-    @user = User.find_by(email: params[:user][:email].downcase)
+    @user = User.find_by(email: params[:email].downcase)
     if @user
       @user.created_at_was
       @user.send_reset_password_instructions
-      #hay que crear los textos de response para password , lepuse ese paraque devoviera algo mientras peleaba con device
-      render_serialized ResponseWS.simple_error('api.login.fail')
+      render_serialized ResponseWS.simple_ok('api.reset_password.success')
+    else
+      render_serialized ResponseWS.simple_error('api.reset_password.fail')
     end
   end
 
+
+  def confirm
+    user = User.reset_password_by_token(params);
+    if user.errors.empty?
+      render_serialized ResponseWS.ok('api.reset_password.confirm.success', user)
+    else
+      render_serialized ResponseWS.simple_error(user.errors.full_messages.to_sentence)
+    end
+  end
+  
 end
