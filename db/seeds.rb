d8f95751433f1013a6f3a86378f8a33aff5b7ce0
Lucas <lucascuevas@live.com.ar>
2018-05-16 23:10:30 -0300

    add guest user with acces token

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+
+#user : create a guest user
+user = User.find_by(login: "guest_user")
+
+if !user
+    password = Devise.friendly_token.first(8)
+    guest_user = User.new
+    guest_user.login = "guest_user"
+    guest_user.email = "guest_user@fromthepage.com"
+    guest_user.display_name = "Guest User"
+    guest_user.password = password
+    guest_user.password_confirmation = password
+    guest_user.save!
+end
