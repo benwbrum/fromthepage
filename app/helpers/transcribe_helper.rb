ecc57426de258062d465bf88bfa84792f16535b9
Jack Weinbender <jack.weinbender@gmail.com>
2018-08-07 13:53:21 -0500

    Cleanup dead code

--- app/helpers/transcribe_helper.rb
+++ app/helpers/transcribe_helper.rb
@@ -1,31 +1,4 @@
 module TranscribeHelper
-=begin
-  include AbstractXmlHelper
-  
-  def relevant_transcription_statuses(work)
-    statuses = Page::STATUSES
-    
-    statuses = statuses.invert
-    statuses.delete(Page::STATUS_UNCORRECTED_OCR) # never relevant to transcribers in this venue
-    statuses.delete(Page::STATUS_INCOMPLETE) if work.ia_work && work.ia_work.use_ocr
-    statuses.delete(Page::STATUS_INCOMPLETE_OCR) unless work.ia_work && work.ia_work.use_ocr
-    statuses.delete(Page::STATUS_INCOMPLETE_TRANSLATION) #never relevant to transcriptin screen
-
-    statuses.invert
-  end
-  
-  def relevant_translation_statuses(work)
-    statuses = Page::STATUSES
-    
-    statuses.keep_if { |k,v| v == Page::STATUS_INCOMPLETE_TRANSLATION}
-    
-    statuses
-  end
-  
-  def relevant_status_help(work)
-    Page::STATUS_HELP.values.join(" ")
-  end
-=end
 
   def subject_context(text, title, line_radius=3)
     line_radius = 1 if line_radius < 2 # Just in case
