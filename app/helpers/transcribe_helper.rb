0c071b168b8f0120478269d546bddf9af20d5097
Jack Weinbender <jack.weinbender@gmail.com>
2018-08-09 19:35:35 -0500

    rubocoped

--- app/helpers/transcribe_helper.rb
+++ app/helpers/transcribe_helper.rb
@@ -5,26 +5,27 @@ module TranscribeHelper
     line_radius += 1 # Makes the "radius" make more sense as a value
 
     output = "<b>#{title}</b>" # have something to return if the match fails
-    
+
     regexed_title = /(\[\[#{title.gsub(/\s*/, '\s*')}.*?\]\])/m
     match = text.match(regexed_title)
 
-    unless match == nil
+    unless match.nil?
 
       pivot, end_index = match.offset(0)
 
       # Generate a list of \n indexes including 0 index and final index
-      linebreaks = [0]
-      text.to_enum(:scan,/\n/).each {|m,| linebreaks.push $`.size}
-      linebreaks.push(text.length)
-      
+      newlines = [0]
+      text.to_enum(:scan, /\n/).each { |m,| newlines.push $`.size }
+      newlines.push(text.length)
+
       ## Sensible index defaults
       pre = 0
       post = text.length - 1
 
       # Separate the \n before and after the main match (ignore \n in the title)
-      left, right = linebreaks.uniq.reject{|idx| idx > pivot && idx < end_index }
-        .partition {|idx| idx < pivot }
+      left, right = newlines.uniq
+                            .reject { |idx| idx > pivot && idx < end_index }
+                            .partition { |idx| idx < pivot }
 
       # Set new pre/post indexes based on line radius
       pre = left.last(line_radius).min + 1 unless left.empty?
