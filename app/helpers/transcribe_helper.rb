1fb225a1c099cfc01bec62e6ea08fb57e4e8b6bf
Jack Weinbender <jack.weinbender@gmail.com>
2018-08-13 12:11:29 -0500

    Refactor with tests

--- app/helpers/transcribe_helper.rb
+++ app/helpers/transcribe_helper.rb
@@ -1,6 +1,22 @@
 module TranscribeHelper
+  def excerpt_subject(page, title, options = {})
+    options[:text_type] ||= 'transcription'
+    options[:radius] ||= 3
 
-  def subject_context(text, title, line_radius=3)
+    search_text = case options[:text_type]
+                  when 'translation'
+                    page.source_translation
+                  when 'transcription'
+                    page.source_text
+                  else
+                    "[[#{title}]]"
+                  end
+    subject_context(search_text, title, options[:radius])
+  end
+
+  private
+
+  def subject_context(text, title, line_radius)
     line_radius = 0 if line_radius < 0 # Just in case
     line_radius += 1 # Makes the "radius" make more sense as a value
 
@@ -15,7 +31,7 @@ module TranscribeHelper
 
       # Generate a list of \n indexes including 0 index and final index
       newlines = [0]
-      text.to_enum(:scan, /\n/).each { |m,| newlines.push $`.size }
+      text.to_enum(:scan, /\n/).each { |_m,| newlines.push $`.size }
       newlines.push(text.length)
 
       ## Sensible index defaults
@@ -31,10 +47,10 @@ module TranscribeHelper
       pre = left.last(line_radius).min + 1 unless left.empty?
       post = right.first(line_radius).max unless right.empty?
 
-      output = text[pre..post].sub(regexed_title, '<b>\1</b>').strip()
+      output = text[pre..post].sub(regexed_title, '<b>\1</b>').strip
 
     end
 
-    return output
+    output
   end
 end
