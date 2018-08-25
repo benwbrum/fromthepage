ad473e120cd9ef1b2d3727a40363f4c251e7f550
Jack Weinbender <jack.weinbender@gmail.com>
2018-08-25 12:13:26 -0500

    Refactored Eeexporter class. Adds Section Info to link export data

--- spec/models/subject_exporter_spec.rb
+++ spec/models/subject_exporter_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 RSpec.describe SubjectExporter do
   describe 'sections export' do
-
     let(:collection) { FactoryBot.build_stubbed(:collection) }
     it 'exports headers for a blank collection' do
       subjects = SubjectExporter.new(FactoryBot.build_stubbed(:collection))
@@ -18,12 +17,11 @@ RSpec.describe SubjectExporter do
     it 'should include work, page, link, article, and category data' do
       collection_with_works = FactoryBot.build_stubbed(:collection, :with_links)
       subjects = SubjectExporter.new(collection_with_works)
-      expect(subjects.export).to include('"Work 1","work_id_1","Page 1"')
-      expect(subjects.export).to include('Page 1')
+      expect(subjects.export).to include('"Work 1","work_id_1"')
+      expect(subjects.export).to include('"Page 1","1"')
       expect(subjects.export).to include('"Article 1","display_text_1"')
       expect(subjects.export).to include('"Category 1|Category 2|Category 3"')
     end
-    it 'should include section title'
-    it 'should include section id'
   end
 end
+
