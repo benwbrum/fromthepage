eb3e354cc72b1c1986379b2cf7e7517b85e53074
Anne Richardson <richardson.ae@gmail.com>
2018-09-26 16:47:23 -0500

    Removes full set of attributes from factories

--- spec/factories/collection.rb
+++ spec/factories/collection.rb
@@ -1,26 +1,5 @@
 FactoryBot.define do
   factory :collection do
-    owner_user { association(:owner_user) }
-
     sequence(:title) { |n| "title_#{n}" }
-    sequence(:intro_block) { |n| "intro_block_#{n}" }
-    sequence(:footer_block) { |n| "footer_block_#{n}" }
-    sequence(:picture) { |n| "picture_#{n}.jpg" }
-    sequence(:transcription_conventions) { |n| "transcription_conventions_#{n}" }
-    sequence(:slug) { |n| "slug_#{n}" }
-    sequence(:help) { |n| "help_#{n}" }
-    sequence(:link_help) { |n| "link_help_#{n}" }
-    sequence(:language) { |n| "language_#{n}" }
-    sequence(:text_language) { |n| "text_language_#{n}" }
-    sequence(:license_key) { |n| "license_key_#{n}" }
-    sequence(:pct_completed) { |n| n }
-    sequence(:default_orientation) { |n| "default_orientation_#{n}" }
-    supports_document_sets { false }
-    restricted { false }
-    hide_completed { true }
-    subjects_disabled { false }
-    review_workflow { false }
-    field_based { false }
-    voice_recognition { false }
   end
 end
--- spec/factories/work.rb
+++ spec/factories/work.rb
@@ -1,26 +1,6 @@
 FactoryBot.define do
   factory :work do
-    # owner_user { association(:owner_user) }
     collection { association(:collection) }
-
     sequence(:title) { |n| "title_#{n}" }
-    sequence(:description) { |n| "description_#{n}" }
-    sequence(:restrict_scribes) { false }
-    sequence(:transcription_version) { |n| n }
-    sequence(:physical_description) { |n| "physical_description_#{n}" }
-    sequence(:document_history) { |n| "document_history_#{n}" }
-    sequence(:permission_description) { |n| "permission_description_#{n}" }
-    sequence(:location_of_composition) { |n| "location_of_composition_#{n}" }
-    sequence(:author) { |n| "author_#{n}" }
-    sequence(:transcription_conventions) { |n| "transcription_conventions_#{n}" }
-    sequence(:scribes_can_edit_titles) { false }
-    sequence(:supports_translation) { false }
-    sequence(:translation_instructions) { |n| "translation_instructions_#{n}" }
-    sequence(:pages_are_meaningful) { true }
-    sequence(:ocr_correction) { true }
-    sequence(:slug) { |n| "slug_#{n}" }
-    sequence(:picture) { |n| "picture_#{n}" }
-    sequence(:featured_page) { |n| n }
-    sequence(:identifier) { |n| "identifier_#{n}" }
   end
 end
