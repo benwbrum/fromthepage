5113df09604a8b62c7cb0302cf175d59beb8e843
Anne Richardson <richardson.ae@gmail.com>
2018-10-09 12:18:19 -0500

    Reverts to original state

--- spec/features/guest_spec.rb
+++ spec/features/guest_spec.rb
@@ -1,19 +1,18 @@
 require 'spec_helper'
 
 describe "guest user actions" do
-  let(:owner) { User.find_by(login: OWNER) }
-  let(:admin) { User.find_by(login: ADMIN) }
-  let(:guest_user) { User.last }
 
-  let(:all_collections) { Collection.all }
-  let(:collection) { Collection.last }
-  let(:work) { collection.works.last }
-  # 'page' is a protected word in Capybara, so our instance of
-  # the 'Page' class is called 'page1' to avoid collision.
-  let(:page1) { work.pages.last }
+  before :all do
+    @collections = Collection.all
+    @collection = @collections.last
+    @work = @collection.works.last
+    @page = @work.pages.last
+    @owner = User.find_by(login: OWNER)
+    @admin = User.find_by(login: ADMIN)
+  end
 
   it "tests guest account creation and migration" do
-    visit "/display/display_page?page_id=#{page1.id}"
+    visit "/display/display_page?page_id=#{@page.id}"
     page.find('.tabs').click_link("Transcribe")
     expect(page).to have_content("Sign In")
     expect(page).not_to have_content("Signed In As")
@@ -21,25 +20,16 @@ describe "guest user actions" do
     click_button("Transcribe as guest")
     expect(page).to have_content("Signed In As")
     expect(page).to have_button("Save Changes")
-    expect(guest_user.guest).to be true
-    # Guests get to save n many changes before an account is required:
-    GUEST_DEED_COUNT.times do
-      click_button("Save Changes")
-      sleep(3)
-    end
+    @guest = User.last
+    expect(@guest.guest).to be true
+    expect(page).to have_link("Sign In")
+    first(:link, 'Sign In').click
+    expect(page).to have_link("Sign Up Now")
+    click_link("Sign Up Now")
     expect(page.current_path).to eq new_user_registration_path
-    find_link("Sign in existing account", href: "/users/sign_in").click
-    expect(page.current_path).to eq new_user_session_path
  end
 
   it "tests guest account transcription" do
-    @collections = Collection.all
-    @collection = @collections.last
-    @work = @collection.works.last
-    @page = @work.pages.last
-    @owner = User.find_by(login: OWNER)
-    @admin = User.find_by(login: ADMIN)
-
     visit collection_display_page_path(@collection.owner, @collection, @work, @page.id)
     page.find('.tabs').click_link("Transcribe")
     click_button("Transcribe as guest")
@@ -81,12 +71,12 @@ describe "guest user actions" do
   it "looks at the landing page" do
     visit landing_page_path
     expect(page).to have_selector('.carousel')
-    expect(page.find('.maincol')).to have_link(owner.display_name)
-    page.find('.maincol').click_link(owner.display_name)
+    expect(page.find('.maincol')).to have_link(@owner.display_name)
+    page.find('.maincol').click_link(@owner.display_name)
     expect(page).to have_content("Recent Activity")
-    expect(page.find('.maincol')).not_to have_content(admin.display_name)
-    expect(page.find('h1')).to have_content owner.display_name
-    expect(page.current_path).to eq user_profile_path(owner)
+    expect(page.find('.maincol')).not_to have_content(@admin.display_name)
+    expect(page.find('h1')).to have_content @owner.display_name
+    expect(page.current_path).to eq user_profile_path(@owner)
   end
 
   it "searches the landing page" do
@@ -94,13 +84,13 @@ describe "guest user actions" do
     page.fill_in 'search', with: 'Import'
     click_button('Search')
     expect(page).not_to have_selector('.carousel')
-    expect(page.find('.maincol')).to have_content owner.display_name
-    expect(page.find('.maincol')).to have_content all_collections.second.title
-    expect(page.find('.maincol')).not_to have_content all_collections.first.title
+    expect(page.find('.maincol')).to have_content @owner.display_name
+    expect(page.find('.maincol')).to have_content @collections.second.title
+    expect(page.find('.maincol')).not_to have_content @collections.first.title
   end
 
   it "checks guest start transcribing path" do
-    visit collection_start_transcribing_path(collection.owner, collection)
+    visit collection_start_transcribing_path(@collection.owner, @collection)
     expect(page.current_path).not_to eq dashboard_path
     expect(page).to have_button("Transcribe as guest")
   end
