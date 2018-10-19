1c7a41fdefc0f5934703bf8fbddb1c79861a36aa
Jack Weinbender <jack.weinbender@gmail.com>
2018-10-19 10:25:06 -0500

    More robust config logic for users without ReCAPTCHA configured.

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,20 @@
+# Set constants to nil if they aren't defined already so
+# the app doesn't blow up when it's updtaed
+RECAPTCHA_SITE_KEY = nil unless defined?(RECAPTCHA_SITE_KEY)
+RECAPTCHA_SECRET_KEY = nil unless defined?(RECAPTCHA_SITE_KEY)
 
-if RECAPTCHA_ENABLED
+# Conditionally set RECAPTCHA_ENABLED based on whether ReCAPTCHA configs
+# are set. Can be overridden by setting the RECAPTCHA_ENABLED flag explicitly.
+if defined?(RECAPTCHA_ENABLED) == nil
+  if RECAPTCHA_SITE_KEY && RECAPTCHA_SECRET_KEY
+    RECAPTCHA_ENABLED = true
+  else
+    RECAPTCHA_ENABLED = false
+  end
+end
+
+# Only config if the variables are not nil
+if RECAPTCHA_SITE_KEY && RECAPTCHA_SECRET_KEY
   Recaptcha.configure do |config|
     config.site_key  = RECAPTCHA_SITE_KEY
     config.secret_key = RECAPTCHA_SECRET_KEY
@@ -7,4 +22,3 @@ if RECAPTCHA_ENABLED
     # config.proxy = 'http://myproxy.com.au:8080'
   end
 end
-
