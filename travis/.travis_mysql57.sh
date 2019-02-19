88b72ec3d81327903ad3848fc5ac6cac248be6bf
Jack Weinbender <jack.weinbender@gmail.com>
2019-02-19 09:58:11 -0600

    Added script back to travis build

---
+++ travis/.travis_mysql57.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -eux
+
+apt-get purge -qq '^mysql*' '^libmysql*'
+rm -fr /etc/mysql
+rm -fr /var/lib/mysql
+add-apt-repository 'deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-5.7'
+apt-get update -qq
+apt-get install -qq mysql-server libmysqlclient-dev
+
+# https://www.percona.com/blog/2016/03/16/change-user-password-in-mysql-5-7-with-plugin-auth_socket/
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY ''"
