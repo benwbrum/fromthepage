ea66db64a53e18e7b29377acf27a5603b280bb20
Jack Weinbender <jack.weinbender@gmail.com>
2019-03-02 23:52:15 -0600

    Test refactor

--- spec/mailers/admin_mailer_owner_collection_activity_spec.rb
+++ spec/mailers/admin_mailer_owner_collection_activity_spec.rb
@@ -8,11 +8,21 @@ RSpec.describe AdminMailer::OwnerCollectionActivity do
         @collection = create(:collection, owner_user_id: @owner.id)
         @new_collaborator = create(:user)
         @old_collaborator = create(:user)
+        @new_deed = create(:deed, {
+          deed_type: DeedType::WORK_ADDED,
+          collection_id: @collection.id,
+          user_id: @old_collaborator.id
+        })
+        @old_deed = create(:deed, {
+          deed_type: DeedType::WORK_ADDED,
+          collection_id: @collection.id,
+          user_id: @old_collaborator.id,
+          created_at: 2.days.ago
+        })
       end
   
       after :each do
-        @new_deed.destroy if @new_deed
-        @old_deed.destroy if @old_deed
+        @new_collaborator_deed.destroy if @new_collaborator_deed
       end
   
       after :all do
@@ -20,6 +30,8 @@ RSpec.describe AdminMailer::OwnerCollectionActivity do
         @collection.destroy
         @new_collaborator.destroy
         @old_collaborator.destroy
+        @new_deed.destroy
+        @old_deed.destroy
       end
   
       it "stores the owner as an attribute" do
@@ -33,39 +45,15 @@ RSpec.describe AdminMailer::OwnerCollectionActivity do
       end
       
       it "retrieves new owner collaborators and sets as an attribute" do
-        @new_deed = create(:deed, {
-          deed_type: DeedType::WORK_ADDED,
-          collection_id: @collection.id,
-          user_id: @new_collaborator.id
-        })
-        activity = AdminMailer::OwnerCollectionActivity.build(@owner)
-        expect(activity.collaborators.first).to eq(@new_collaborator)
-        expect(activity.collaborators.last).to eq(@new_collaborator)
-      end
-      
-      it "ensures it doesn't include old collaborators" do
-        @new_deed = create(:deed, {
-          deed_type: DeedType::WORK_ADDED,
-          collection_id: @collection.id,
-          user_id: @old_collaborator.id
-        })
-        @old_deed = create(:deed, {
-          deed_type: DeedType::WORK_ADDED,
-          collection_id: @collection.id,
-          user_id: @old_collaborator.id,
-          created_at: 2.days.ago
-        })
         @new_collaborator_deed = create(:deed, {
           deed_type: DeedType::WORK_ADDED,
           collection_id: @collection.id,
           user_id: @new_collaborator.id
         })
-
         activity = AdminMailer::OwnerCollectionActivity.build(@owner)
-        expect(activity.collaborators.length).to eq(1)
         expect(activity.collaborators.first).to eq(@new_collaborator)
-        
-        @new_collaborator_deed.destroy
+        # Make sure the old collaborator isn't listed
+        expect(activity.collaborators.length).to eq(1)
       end
   end
 end
