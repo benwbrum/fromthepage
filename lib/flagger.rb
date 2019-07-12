c8789e0bd8f33f678bd7be3ea0a42463709c1249
Ben W. Brumfield <benwbrum@gmail.com>
2019-07-12 14:15:09 -0500

    Look for more content to flag

--- lib/flagger.rb
+++ lib/flagger.rb
@@ -1,17 +1,27 @@
 module Flagger
+  BLACKLIST = [
+    'href',
+    '.info',
+    '.in',
+    '.net',
+    '.store'
+  ]
+
+
+
   def self.check(content)
     # look for suspicious strings
-    if content && content.match(/(.{,80})(\S+)(\.com)(.*{,80})/m)
-      # return a bad snippet if we find them
-      prefix = $1
-      domain = $2
-      fixed = $3
-      suffix = $4
+    BLACKLIST.each do |badness|
+      if content && content.match(/(.{,80})(\S+)(#{badness})(.*{,80})/m)
+        # return a bad snippet if we find them
+        prefix = $1
+        domain = $2
+        fixed = $3
+        suffix = $4
 
-      "#{prefix}#{domain}#{fixed}#{suffix}"
-    else
-      # return nil if we don't
-      nil
+        return "#{prefix}#{domain}#{fixed}#{suffix}"
+      end
     end
+    return nil
   end
 end
