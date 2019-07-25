ff47c1857edae14c684b446cf15c318b9f9d1e95
Diego Viola <diego.viola@gmail.com>
2019-07-24 21:12:41 -0300

    iiif_manifests_spec: switch ID back to 1

--- spec/requests/iiif_manifests_spec.rb
+++ spec/requests/iiif_manifests_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe "IIIF Manifests API", :type => :request do
   it "returns JSON" do
-    get "/iiif/2/manifest"
+    get "/iiif/1/manifest"
     expect(response.content_type).to eq("application/json")
   end
 
   it "returns a URL with the corresponding ID" do
-    get "/iiif/2/manifest"
+    get "/iiif/1/manifest"
     json = JSON.parse(response.body)
-    expect(json['within']['@id']).to eql("http://www.example.com/iiif/collection/2")
+    expect(json['within']['@id']).to eql("http://www.example.com/iiif/collection/1")
   end
 end
