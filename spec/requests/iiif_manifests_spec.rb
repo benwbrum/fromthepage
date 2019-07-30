f39f5d7a2c5d31ccec00238e3d106122d1bb555d
Diego Viola <diego.viola@gmail.com>
2019-07-30 13:22:36 -0300

    iiif_manifests_spec.rb: call it using a resource

--- spec/requests/iiif_manifests_spec.rb
+++ spec/requests/iiif_manifests_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe "IIIF Manifests API", :type => :request do
   it "returns JSON" do
-    get "/iiif/1/manifest"
+    get iiif_manifest_path(1)
     expect(response.content_type).to eq("application/json")
   end
 
   it "returns a URL with the corresponding ID" do
-    get "/iiif/1/manifest"
+    get iiif_manifest_path(1)
     json = JSON.parse(response.body)
     expect(json['within']['@id']).to eql("http://www.example.com/iiif/collection/1")
   end
