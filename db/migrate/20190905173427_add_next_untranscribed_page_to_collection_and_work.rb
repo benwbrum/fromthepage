a84ccc6afb40552d5f927de823e3c8f7bbdccc65
Jack Weinbender <jack.weinbender@gmail.com>
2019-09-05 16:38:26 -0500

    Add columns migration

---
+++ db/migrate/20190905173427_add_next_untranscribed_page_to_collection_and_work.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class AddNextUntranscribedPageToCollectionAndWork < ActiveRecord::Migration
+  def change
+    add_column :collections, :next_untranscribed_page_id, :integer
+    add_column :works, :next_untranscribed_page_id, :integer
+  end
+end
