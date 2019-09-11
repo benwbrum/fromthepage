cad6c56b71b168143ec935dc097ccacaba607800
Jack Weinbender <jack.weinbender@gmail.com>
2019-09-11 11:14:33 -0500

    Add tests

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -3,6 +3,12 @@
 require 'spec_helper'
 
 RSpec.describe Collection, type: :model do
+  before :each do
+    DatabaseCleaner.start
+  end
+  after :each do
+    DatabaseCleaner.clean
+  end
   describe '#is_public' do
     it 'returns true if a collection is not restricted' do
       user = build_stubbed(:user)
@@ -16,6 +22,46 @@ RSpec.describe Collection, type: :model do
       collection = build_stubbed(:collection, owner_user_id: user.id, restricted: true)
 
       expect(collection.is_public).to be false
+    end
+  end
+  describe '#set_next_untranscribed_page' do
+    let(:collection){ create(:collection, works: []) }
+    let(:work){ create(:work, collection_id: collection.id) }
+    it "sets nil with no works" do
+      collection.set_next_untranscribed_page
+      expect(collection.next_untranscribed_page).to eq(nil)
+    end
+    it "sets to untranscribed page in work" do
+      page = create(:page, work_id: work.id)
+
+      work.set_next_untranscribed_page
+      expect(work.next_untranscribed_page).to eq(page)
+
+      collection.set_next_untranscribed_page
+      expect(collection.next_untranscribed_page).to eq(page)
+    end
+    it "sets to nil for no works with untranscribed pages" do
+      create(:page, work_id: work.id, status: Page::STATUS_TRANSCRIBED)
+
+      work.set_next_untranscribed_page
+      expect(work.next_untranscribed_page).to eq(nil)
+
+      collection.set_next_untranscribed_page
+      expect(collection.next_untranscribed_page).to eq(nil)
+    end
+    it "sets to NUP of work with least complete" do
+      create(:page, work_id: work.id, status: Page::STATUS_TRANSCRIBED)
+      work_incomplete = create(:work, collection_id: collection.id)
+      page_incomplete = create(:page, status: nil, work_id: work_incomplete.id)
+      create(:page, status: Page::STATUS_TRANSCRIBED, work_id: work_incomplete.id)
+      
+      work.set_next_untranscribed_page
+      work.save!
+      work_incomplete.set_next_untranscribed_page
+      work_incomplete.save!
+
+      collection.set_next_untranscribed_page
+      expect(collection.next_untranscribed_page).to eq(page_incomplete)
     end
   end
 end
