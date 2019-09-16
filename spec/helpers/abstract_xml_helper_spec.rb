b611ba9d7dc3778a2b5a2edfbb8154f6858dd4f1
Diego Viola <diego.viola@gmail.com>
2019-09-16 15:24:12 -0300

    AbstractXmlHelper: get rid of xml_text duplication

--- spec/helpers/abstract_xml_helper_spec.rb
+++ spec/helpers/abstract_xml_helper_spec.rb
@@ -7,24 +7,26 @@ RSpec.describe AbstractXmlHelper, type: :helper do
 
   before do
     @collection = Collection.first
+
+    @xml_text = "<?xml version='1.0' encoding='UTF-8'?>    \n      <page>\n \
+    <p>guide the reader <lb/>to the correct pron<lb break='no'/>ounciation \
+    [sic].</p><p>I am Dear Sir<lb/>Yours Faithfully<lb/>Samuel Gason</p>\n \
+    </page>\n"
   end
 
   it "returns a <br> tag with preserve_lb=true" do
-    xml_text = "<?xml version='1.0' encoding='UTF-8'?>    \n      <page>\n        <p>guide the reader <lb/>to the correct pron<lb break='no'/>ounciation [sic].</p><p>I am Dear Sir<lb/>Yours Faithfully<lb/>Samuel Gason</p>\n      </page>\n"
-    expect(xml_to_html(xml_text, true, true)).to include("correct pron<br/>\nounciation")
+    expect(xml_to_html(@xml_text, true, true)).to include("correct pron<br/>\nounciation")
   end
 
   it "returns a <span> tag and a hyphen with preserve_lb=false" do
-    xml_text = "<?xml version='1.0' encoding='UTF-8'?>    \n      <page>\n        <p>guide the reader <lb/>to the correct pron<lb break='no'/>ounciation [sic].</p><p>I am Dear Sir<lb/>Yours Faithfully<lb/>Samuel Gason</p>\n      </page>\n"
-    expect(xml_to_html(xml_text, false, true)).to include("pron<span class='line-break'>-</span>ounciation")
+    expect(xml_to_html(@xml_text, false, true)).to include("pron<span class='line-break'>-</span>ounciation")
   end
 
   context "with params" do
     let(:params) { { action: "read_work" } }
 
     it "adds a whitespace after a hard break with preserve_lb=false" do
-      xml_text = "<?xml version='1.0' encoding='UTF-8'?>    \n      <page>\n        <p>guide the reader <lb/>to the correct pron<lb break='no'/>ounciation [sic].</p><p>I am Dear Sir<lb/>Yours Faithfully<lb/>Samuel Gason</p>\n      </page>\n"
-      expect(xml_to_html(xml_text, false, true)).to include("guide the reader <span class='line-break'> </span>to")
+      expect(xml_to_html(@xml_text, false, true)).to include("guide the reader <span class='line-break'> </span>to")
     end
   end
 end
