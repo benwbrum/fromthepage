5debf6e226016b5c5e08b77d7051a644ee4318d7
Diego Viola <diego.viola@gmail.com>
2019-09-16 15:25:40 -0300

    AbstractXmlHelper: wording fix

--- spec/helpers/abstract_xml_helper_spec.rb
+++ spec/helpers/abstract_xml_helper_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe AbstractXmlHelper, type: :helper do
     expect(xml_to_html(@xml_text, true, true)).to include("correct pron<br/>\nounciation")
   end
 
-  it "returns a <span> tag and a hyphen with preserve_lb=false" do
+  it "returns a <span> tag with a hyphen with preserve_lb=false" do
     expect(xml_to_html(@xml_text, false, true)).to include("pron<span class='line-break'>-</span>ounciation")
   end
 
