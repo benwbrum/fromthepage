c05dcff676b942d05c49881e8586cfc5d0cfc27a
Diego Viola <diego.viola@gmail.com>
2019-10-10 04:19:46 -0300

    dashboard: fix tests
    that were broken due to sections being collapsed in "Start A Project".
    Expanding those sections from the same feature specs fixes the problem.

--- spec/features/import_data_spec.rb
+++ spec/features/import_data_spec.rb
@@ -17,36 +17,40 @@ describe "import data" do
         let(:repository_url){ 'https://cdm16488.contentdm.oclc.org/' }
         let(:bad_item_url)  { 'https://cdm16488.contentdm.oclc.org/digital/collection/MPD01/id/1' }
 
-        it "browses a single record" do 
+        it "browses a single record", :js => true do 
             visit dashboard_owner_path
-            click_link("Start A Project")
+            page.find('.tabs').click_link("Start A Project")
+            page.find(:css, "#import-contentdm").click
             VCR.use_cassette('cdm/midpoint-shelwater-item') do
                 page.fill_in 'cdm_url', with: item_url
                 page.find('#cdm_import').click
             end
             expect(page).to have_content("Manifest: Letter with envelope from Virginia Shewalter")
         end
-        it "browses records from a collection" do
+        it "browses records from a collection", :js => true do
             visit dashboard_owner_path
-            click_link("Start A Project")
+            page.find('.tabs').click_link("Start A Project")
+            page.find(:css, "#import-contentdm").click
             VCR.use_cassette('cdm/midpoint-shelwater-collection') do
                 page.fill_in 'cdm_url', with: collection_url
                 page.find('#cdm_import').click
             end
             expect(page).to have_content("Collection: The Virginia Shewalter Letters Collection")
         end
-        it "browses collections from a repository" do
+        it "browses collections from a repository", :js => true do
             visit dashboard_owner_path
-            click_link("Start A Project")
+            page.find('.tabs').click_link("Start A Project")
+            page.find(:css, "#import-contentdm").click
             VCR.use_cassette('cdm/midpoint-repository') do
                 page.fill_in 'cdm_url', with: repository_url
                 page.find('#cdm_import').click
             end
             expect(page).to have_content("Collections:")
         end
-        it "Gives an error for a well-formed Cdm URL with a bad/empty IIIF manifest" do
+        it "Gives an error for a well-formed Cdm URL with a bad/empty IIIF manifest", :js => true do
             visit dashboard_owner_path
-            click_link("Start A Project")
+            page.find('.tabs').click_link("Start A Project")
+            page.find(:css, "#import-contentdm").click
             VCR.use_cassette('cdm/bad_iiif_manifest') do
                 page.fill_in 'cdm_url', with: bad_item_url
                 page.find('#cdm_import').click
