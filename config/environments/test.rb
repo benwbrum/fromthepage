caf30f97a644a796bdea3a591ad21eb9588a0b79
Diego Viola <diego.viola@gmail.com>
2019-10-29 16:26:49 -0300

    environments: re-add variables to new config

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -43,4 +43,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+
+  RAKE = '/usr/bin/env rake'
 end
