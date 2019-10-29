cd757ddeb66707591e519d7609785167341e0a06
Diego Viola <diego.viola@gmail.com>
2019-10-29 16:17:25 -0300

    application.rb: set belongs_to_required_by_default to false
    Rails 5 makes every association not optional, some of our forms break
    with this change. This configuration alleviates the issue but emulates
    the old behavior of Rails 4.
    
    TODO: remove this config and fix the problem.

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module Fromthepage
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.active_record.belongs_to_required_by_default = false
   end
 end
