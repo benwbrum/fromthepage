d3c4717df8a2ee6f4eed7b993c3e8fd9f0896eff
Diego Viola <diego.viola@gmail.com>
2019-11-03 06:02:46 -0300

    display_controller: swap uniq for distinct

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -51,7 +51,7 @@ class DisplayController < ApplicationController
     if @article
       # restrict to pages that include that subject
       @pages = Page.order('work_id, position').joins('INNER JOIN page_article_links pal ON pages.id = pal.page_id').where([ 'pal.article_id = ?', @article.id ]).where(work_id: @collection.works.ids).paginate(page: params[:page], per_page: PAGES_PER_SCREEN)
-      @pages.uniq!
+      @pages.distinct!
     else
       @pages = Page.paginate :all, :page => params[:page],
                                         :order => 'work_id, position',
