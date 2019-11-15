9876def731a8b4195a6926ee871ac3fe67dd1ff3
Diego Viola <diego.viola@gmail.com>
2019-11-15 12:21:41 -0300

    UpdatePageBlockLinks: use a regex instead

--- db/migrate/20191114174113_update_page_block_links.rb
+++ db/migrate/20191114174113_update_page_block_links.rb
@@ -1,13 +1,14 @@
 class UpdatePageBlockLinks < ActiveRecord::Migration[5.2]
   def change
     pages = PageBlock.all
+
     pages.each do |page|
-      if page.html&.include?("link_to('partly transcribed', :controller => 'demo')")
-        page.html.gsub!("link_to('partly transcribed', :controller => 'demo')",
+      if page.html&.include?("partly transcribed")
+        page.html.gsub!(/(link_to\('partly transcribed'.+'\))/,
                         "link_to('partly transcribed', demo_path)")
         page.save
-      elsif page.html&.include?("link_to(\"sign up\", \n                    { :controller => 'account',\n                      :action => 'signup', \n                      :ol => 'acct_login_sulnk' } )")
-        page.html.gsub!("link_to(\"sign up\", \n                    { :controller => 'account',\n                      :action => 'signup', \n                      :ol => 'acct_login_sulnk' } )",
+      elsif page.html&.include?("sign up")
+        page.html.gsub!(/(link_to\(."sign up.+ \))/,
                         "link_to('sign up', new_user_registration_path)")
         page.save
       end
