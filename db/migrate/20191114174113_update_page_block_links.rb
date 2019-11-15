54334e1394da5e2268834b501d5497c8d3c8ffef
Diego Viola <diego.viola@gmail.com>
2019-11-15 13:49:22 -0300

    UpdatePageBlockLinks: get rid of unnecessary logic

--- db/migrate/20191114174113_update_page_block_links.rb
+++ db/migrate/20191114174113_update_page_block_links.rb
@@ -3,15 +3,11 @@ class UpdatePageBlockLinks < ActiveRecord::Migration[5.2]
     pages = PageBlock.all
 
     pages.each do |page|
-      if page.html&.include?("partly transcribed")
-        page.html.gsub!(/link_to\('partly transcribed'.+'\)/,
-                        "link_to('partly transcribed', demo_path)")
-        page.save
-      elsif page.html&.include?("sign up")
-        page.html.gsub!(/link_to\("sign up.+\)/m,
-                        "link_to('sign up', new_user_registration_path)")
-        page.save
-      end
+      page.html&.gsub!(/link_to\('partly transcribed'.+'\)/,
+                       "link_to('partly transcribed', demo_path)")
+      page.html&.gsub!(/link_to\("sign up.+\)/m,
+                       "link_to('sign up', new_user_registration_path)")
+      page.save
     end
   end
 end
