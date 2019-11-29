447e3858107722d62438de5c5ff0bd77608c5466
Diego Viola <diego.viola@gmail.com>
2019-11-29 01:59:56 -0300

    tf: indent

--- app/models/transcription_field.rb
+++ app/models/transcription_field.rb
@@ -1,13 +1,11 @@
 class TranscriptionField < ApplicationRecord
+  belongs_to :collection, optional: true
+  acts_as_list :scope => :collection
+  has_many :table_cells
+  validates :options, presence: true, if: Proc.new {|field| field.input_type == 'select'}, on: [:create, :update]
 
-belongs_to :collection, optional: true
-acts_as_list :scope => :collection
-has_many :table_cells
-validates :options, presence: true, if: Proc.new {|field| field.input_type == 'select'}, on: [:create, :update]
+  validates :percentage, numericality: { allow_nil: true, greater_than: 0, less_than_or_equal_to: 100 }
+  validates :page_number, numericality: { allow_nil: true, greater_than: 0, less_than_or_equal_to: 1000 }
 
-validates :percentage, numericality: { allow_nil: true, greater_than: 0, less_than_or_equal_to: 100 }
-validates :page_number, numericality: { allow_nil: true, greater_than: 0, less_than_or_equal_to: 1000 }
-
-INPUTS = ["text", "select", "textarea", "description", "instruction"]
-
+  INPUTS = ["text", "select", "textarea", "description", "instruction"]
 end
