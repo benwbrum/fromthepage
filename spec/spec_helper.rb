a66a0cb544b23e02b7cf3dab1cd66cba9d17ceaf
Diego Viola <diego.viola@gmail.com>
2019-11-29 22:15:25 -0300

    spec_helper: disable maintain_test_schema!
    otherwise our test database tables get dropped after we run our tests
    with:
    
      xvfb-run rspec spec/features
    
    Disabling ActiveRecord::Migration.maintain_test_schema! fixes the
    problem.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,7 +23,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.maintain_test_schema!
+#ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # ## Mock Framework
