06781753faa7634176e49ec55eb7ef371c4a09ef
Diego Viola <diego.viola@gmail.com>
2019-12-02 02:17:07 -0300

    page: update_attributes -> update

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -77,7 +77,7 @@ class PageController < ApplicationController
 
   def update
     page = Page.find(params[:id])
-    page.update_attributes(page_params)
+    page.update(page_params)
     flash[:notice] = 'Page has been successfully updated'
 
     if params[:page][:base_image]
