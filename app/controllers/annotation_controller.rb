74db47b006e42bd85d41aeafd7bb75baf39756c0
Diego Viola <diego.viola@gmail.com>
2019-12-11 03:54:28 -0300

    annotation: reindent

--- app/controllers/annotation_controller.rb
+++ app/controllers/annotation_controller.rb
@@ -1,14 +1,16 @@
 class AnnotationController < ApplicationController
-    include AbstractXmlHelper
+  include AbstractXmlHelper
 
-    def page_transcription_html
-        render_xml_to_html @page.xml_text
-    end
-    def page_translation_html
-        render_xml_to_html @page.xml_translation
-    end
-    def render_xml_to_html(xml)
-        annotation = xml_to_html(xml)
-        render  :layout => false, :content_type => "text/html", :plain => annotation
-    end
+  def page_transcription_html
+    render_xml_to_html @page.xml_text
+  end
+
+  def page_translation_html
+    render_xml_to_html @page.xml_translation
+  end
+
+  def render_xml_to_html(xml)
+    annotation = xml_to_html(xml)
+    render  :layout => false, :content_type => "text/html", :plain => annotation
+  end
 end
