715870649038d0c35d2ecf3fc6fa1b217b1e5a23
Diego Viola <diego.viola@gmail.com>
2019-12-12 09:30:22 -0300

    collection: add individual researcher message

--- spec/features/owner_actions_spec.rb
+++ spec/features/owner_actions_spec.rb
@@ -272,5 +272,18 @@ describe "owner actions", :order => :defined do
     expect(rtl_collection.text_language).to eq 'eng'
   end
 
+  it "warns if account type is Individual Researcher" do
+    @owner.account_type = "Individual Researcher"
+    visit dashboard_owner_path
+    page.find('a', text: 'Create a Collection').click
+    expect(@owner.collections.count).to be >= 1
+    expect(page).to have_content("Individual Researcher Accounts are limited to a single collection.")
+  end
 
+  it "does not warn with another account type" do
+    @owner.account_type = "Trial"
+    visit dashboard_owner_path
+    page.find('a', text: 'Create a Collection').click
+    expect(page).not_to have_content("Individual Researcher Accounts are limited to a single collection.")
+  end
 end
