2c6b703639a331aada66bf7f7a20a8f52382ada2
Diego Viola <diego.viola@gmail.com>
2020-01-22 11:33:25 -0300

    application.rb: re-add default_headers config option

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module Fromthepage
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => "GET"
+    }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
