b12905bb789aa1a4f1368ea387a8788ede67023f
CEBracco <development@ubuntu.home>
2020-01-27 23:58:39 +0000

    fix i18n helper

--- app/helpers/i18n_helper.rb
+++ app/helpers/i18n_helper.rb
@@ -12,7 +12,7 @@ module I18nHelper
    
   private
     def extract_locale_from_accept_language_header
-      request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+      request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first if request.env['HTTP_ACCEPT_LANGUAGE']
     end
 
 end
