541cb9a4f1d07dc1411ecb12d75daf46d8f76593
Diego Viola <diego.viola@gmail.com>
2020-02-01 03:44:43 -0300

    export: refactor

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -3,6 +3,7 @@ require 'contentdm_translator'
 class ExportController < ApplicationController
   require 'zip'
   include CollectionHelper,ExportHelper
+  include ExportService
 
   # no layout if xhr request
   layout Proc.new { |controller| controller.request.xhr? ? false : nil } #, :only => [:update, :update_profile]
@@ -90,75 +91,6 @@ class ExportController < ApplicationController
               :filename => "fromthepage_tables_export_#{@work.id}_#{Time.now.utc.iso8601}.csv",
               :type => "application/csv")
     cookies['download_finished'] = 'true'
-
-  end
-
-  def export_plaintext_transcript(name:, dirname:, out:)
-    path = File.join dirname, 'plaintext', "#{name}_transcript.txt"
-
-    out.put_next_entry path
-
-    case name
-    when "verbatim"
-      out.write @work.verbatim_transcription_plaintext
-    when "emended"
-      out.write @work.emended_transcription_plaintext
-    when "searchable"
-      out.write @work.searchable_plaintext
-    end
-  end
-
-  def export_plaintext_translation(name:, dirname:, out:)
-    path = File.join dirname, 'plaintext', "#{name}_translation.txt"
-
-    out.put_next_entry path
-
-    out.write @work.public_send "#{name}_translation_plaintext"
-  end
-
-  def export_plaintext_transcript_pages(name:, dirname:, out:, page:)
-    path = File.join dirname, 'plaintext', "#{name}_transcript_pages", "#{page.title}.txt"
-
-    out.put_next_entry path
-
-    out.write page.public_send "#{name}_transcription_plaintext"
-  end
-
-  def export_plaintext_translation_pages(name:, dirname:, out:, page:)
-    path = File.join dirname, 'plaintext', "#{name}_translation_pages", "#{page.title}.txt"
-
-    out.put_next_entry path
-
-    out.write page.public_send "#{name}_translation_plaintext"
-  end
-
-  def export_view(name:, dirname:, out:)
-    path = File.join dirname, 'html', "#{name}.html"
-    out.put_next_entry path
-
-    case name
-    when "full"
-      full_view = render_to_string(:action => 'show', :formats => [:html], :work_id => @work.id, :layout => false, :encoding => 'utf-8')
-      out.write full_view
-    when "text"
-      text_view = render_to_string(:action => 'text', :formats => [:html], :work_id => @work.id, :layout => false, :encoding => 'utf-8')
-      out.write text_view
-    when "transcript"
-      transcript_view = render_to_string(:action => 'transcript', :formats => [:html], :work_id => @work.id, :layout => false, :encoding => 'utf-8')
-      out.write transcript_view
-    when "translation"
-      translation_view = render_to_string(:action => 'translation', :formats => [:html], :work_id => @work.id, :layout => false, :encoding => 'utf-8')
-      out.write translation_view
-    end
-  end
-
-  def export_html_full_pages(dirname:, out:, page:)
-    path = File.join dirname, 'html', 'full_pages', "#{page.title}.html"
-
-    out.put_next_entry path
-
-    page_view = render_to_string('display/display_page.html.slim', :locals => {:@work => @work, :@page => page}, :layout => false)
-    out.write page_view
   end
 
   def export_work
