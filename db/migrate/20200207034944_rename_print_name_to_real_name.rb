634b3595feadc18e114757008aebfd4eb3707ae8
Diego Viola <diego.viola@gmail.com>
2020-02-12 21:25:21 -0300

    Set migration prefix to 5.2

--- db/migrate/20200207034944_rename_print_name_to_real_name.rb
+++ db/migrate/20200207034944_rename_print_name_to_real_name.rb
@@ -1,4 +1,4 @@
-class RenamePrintNameToRealName < ActiveRecord::Migration
+class RenamePrintNameToRealName < ActiveRecord::Migration[5.2]
   def change
     rename_column :users, :print_name, :real_name
   end
