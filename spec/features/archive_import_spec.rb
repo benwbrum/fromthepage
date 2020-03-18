cd61c64a6542d97aa28953100bcabbcc65d52f08
Diego Viola <diego.viola@gmail.com>
2020-03-18 19:25:24 -0300

    archive_import_spec: use VCR

--- spec/features/archive_import_spec.rb
+++ spec/features/archive_import_spec.rb
@@ -16,47 +16,51 @@ describe "IA import actions", :order => :defined do
   end    
 
   it "imports a work from IA", :js => true do
-    ia_work_count = IaWork.all.count
-    ia_link = "https://archive.org/details/lettertosamuelma00estl"
-    visit dashboard_owner_path
-    page.find('.tabs').click_link("Start A Project")
-    page.find(:css, "#import-internet-archive").click
-    click_link("Import From Archive.org")
-    fill_in 'detail_url', with: ia_link
-    click_button('Import Work')
-    if page.has_button?('Import Anyway')
-      click_button('Import Anyway')
+    VCR.use_cassette("ia/imports_a_work", :record => :new_episodes) do
+      ia_work_count = IaWork.all.count
+      ia_link = "https://archive.org/details/lettertosamuelma00estl"
+      visit dashboard_owner_path
+      page.find('.tabs').click_link("Start A Project")
+      page.find(:css, "#import-internet-archive").click
+      click_link("Import From Archive.org")
+      fill_in 'detail_url', with: ia_link
+      click_button('Import Work')
+      if page.has_button?('Import Anyway')
+        click_button('Import Anyway')
+      end
+      expect(page).to have_content("Manage Archive.org Import")
+      select @collection.title, from: 'collection_id'
+      click_button('Publish Work')
+      expect(page).to have_content("has been converted into a FromThePage work")
+      expect(ia_work_count + 1).to eq IaWork.all.count
     end
-    expect(page).to have_content("Manage Archive.org Import")
-    select @collection.title, from: 'collection_id'
-    click_button('Publish Work')
-    expect(page).to have_content("has been converted into a FromThePage work")
-    expect(ia_work_count + 1).to eq IaWork.all.count
   end
 
   it "uses OCR when importing a work from IA", :js => true do
-    ia_work_count = IaWork.all.count
-    ia_link = "https://archive.org/details/lettertodeargarr00mays"
-    visit dashboard_owner_path
-    page.find('.tabs').click_link("Start A Project")
-    page.find(:css, "#import-internet-archive").click
-    click_link("Import From Archive.org")
-    fill_in 'detail_url', with: ia_link
-    click_button('Import Work')
-    if page.has_button?('Import Anyway')
-      click_button('Import Anyway')
+    VCR.use_cassette("ia/uses_ocr", :record => :new_episodes) do
+      ia_work_count = IaWork.all.count
+      ia_link = "https://archive.org/details/lettertodeargarr00mays"
+      visit dashboard_owner_path
+      page.find('.tabs').click_link("Start A Project")
+      page.find(:css, "#import-internet-archive").click
+      click_link("Import From Archive.org")
+      fill_in 'detail_url', with: ia_link
+      click_button('Import Work')
+      if page.has_button?('Import Anyway')
+        click_button('Import Anyway')
+      end
+      expect(ia_work_count + 1).to eq IaWork.all.count
+      expect(page).to have_content("Manage Archive.org Import")
+      page.check('use_ocr')
+      select @collection.title, from: 'collection_id'
+      click_button('Publish Work')
+      new_work = Work.find_by(title: @title)
+      first_page = new_work.pages.first
+      expect(new_work.ocr_correction).to be 
+      expect(page).to have_content("has been converted into a FromThePage work")
+      expect(page.find('h1')).to have_content(new_work.title)
+      expect(first_page.source_text).not_to be_nil
     end
-    expect(ia_work_count + 1).to eq IaWork.all.count
-    expect(page).to have_content("Manage Archive.org Import")
-    page.check('use_ocr')
-    select @collection.title, from: 'collection_id'
-    click_button('Publish Work')
-    new_work = Work.find_by(title: @title)
-    first_page = new_work.pages.first
-    expect(new_work.ocr_correction).to be 
-    expect(page).to have_content("has been converted into a FromThePage work")
-    expect(page.find('h1')).to have_content(new_work.title)
-    expect(first_page.source_text).not_to be_nil
   end
 
   it "tests ocr correction" do
