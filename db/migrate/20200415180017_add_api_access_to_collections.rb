f7d77211354bd6c54645c1ac4537056d7f2865ca
Diego Viola <diego.viola@gmail.com>
2020-04-21 17:25:39 -0300

    Fix migration

--- db/migrate/20200415180017_add_api_access_to_collections.rb
+++ db/migrate/20200415180017_add_api_access_to_collections.rb
@@ -1,4 +1,4 @@
-class AddApiAccessToCollections < ActiveRecord::Migration
+class AddApiAccessToCollections < ActiveRecord::Migration[6.0]
   def change
     add_column :collections, :api_access, :boolean, default: false
   end
