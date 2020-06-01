fcd613c95fd2264e473136f8ed846b5e8309413c
Diego Viola <diego.viola@gmail.com>
2020-06-01 04:50:02 -0300

    Delete unused specs

--- spec/factories/facet_configs.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :facet_config do
-    label { "MyString" }
-    input_type { "MyString" }
-    order { "MyString" }
-    metadata_coverage { nil }
-  end
-end
--- spec/factories/metadata_coverages.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :metadata_coverage do
-    key { "MyString" }
-    count { 1 }
-    collection { nil }
-  end
-end
