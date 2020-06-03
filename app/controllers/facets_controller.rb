0a20e5f87d308b2b0f94f270fe3906b8a0439f6f
Diego Viola <diego.viola@gmail.com>
2020-06-03 14:41:17 -0300

    facets: call update instead

--- app/controllers/facets_controller.rb
+++ app/controllers/facets_controller.rb
@@ -18,10 +18,7 @@ class FacetsController < ApplicationController
 
     collection.metadata_coverages.each do |m|
       metadata = params[:metadata][m[:key]]
-      m.facet_config.label = metadata['label']
-      m.facet_config.input_type = metadata['input_type']
-      m.facet_config.order = metadata['order']
-      m.facet_config.save
+      m.facet_config.update(label: metadata['label'], input_type: metadata['input_type'], order: metadata['order'])
     end
 
     redirect_to collection_facets_path(collection.owner, collection), notice: "Collection facets updated successfully"
