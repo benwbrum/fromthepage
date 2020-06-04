fae0c245c3dfd70e5bad9d475b3f5096332d98a9
Ben W. Brumfield <benwbrum@gmail.com>
2020-06-04 12:41:32 -0500

    Fixed BigInt migration

--- db/migrate/20200601042436_create_metadata_coverages.rb
+++ db/migrate/20200601042436_create_metadata_coverages.rb
@@ -3,9 +3,14 @@ class CreateMetadataCoverages < ActiveRecord::Migration[6.0]
     create_table :metadata_coverages do |t|
       t.string :key
       t.integer :count
-      t.references :collection, null: false, foreign_key: true
+
+      # This replaces t.references :collection, foreign_key: true
+      t.integer :collection_id, null: false, index: true
 
       t.timestamps
     end
+
+    # This replaces t.references :collection, foreign_key: true
+    add_foreign_key :metadata_coverages, :collections
   end
 end
