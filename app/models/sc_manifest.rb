2175da650f2c5c6e5433751ca27a0c59ee6b141e
saracarl <saracarl@gmail.com>
2020-06-18 15:53:30 -0500

    parse IIIF manifest metadata and show on the work's about page

--- app/models/sc_manifest.rb
+++ app/models/sc_manifest.rb
@@ -130,18 +130,8 @@ class ScManifest < ActiveRecord::Base
     unless self.service.description.blank?
       description += flatten_element(self.service.description) + "\n<br /><br />\n"
     end
-    description += "\n<br /><br />\nMetadata:" + self.html_metadata
     
     description
-  end
-  
-  def html_metadata
-    html = ""
-    self.service.metadata.each do |md|
-      html += "<br />\n#{md["label"]}: #{md["value"]}"
-    end    
-    
-    html
   end
 
 protected
