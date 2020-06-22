d2026e829733ec668240e60ab60f45462c34adaf
saracarl <saracarl@gmail.com>
2020-06-22 16:10:03 -0500

    change json field type to string

--- db/migrate/20200303211551_add_original_metadata_to_work.rb
+++ db/migrate/20200303211551_add_original_metadata_to_work.rb
@@ -1,5 +1,5 @@
 class AddOriginalMetadataToWork < ActiveRecord::Migration
   def change
-    add_column :works, :original_metadata, :json
+    add_column :works, :original_metadata, :text
   end
 end
