9ee9ff896a231bc62a9be742c5e9149dc9ee68d0
Diego Viola <diego.viola@gmail.com>
2020-06-24 02:22:11 -0300

    Revert "Fixed BigInt migration"
    This reverts commit fae0c245c3dfd70e5bad9d475b3f5096332d98a9.

--- db/migrate/20200601042436_create_metadata_coverages.rb
+++ db/migrate/20200601042436_create_metadata_coverages.rb
@@ -3,14 +3,9 @@ class CreateMetadataCoverages < ActiveRecord::Migration[6.0]
     create_table :metadata_coverages do |t|
       t.string :key
       t.integer :count
-
-      # This replaces t.references :collection, foreign_key: true
-      t.integer :collection_id, null: false, index: true
+      t.references :collection, null: false, foreign_key: true
 
       t.timestamps
     end
-
-    # This replaces t.references :collection, foreign_key: true
-    add_foreign_key :metadata_coverages, :collections
   end
 end
