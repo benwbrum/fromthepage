0cdca8324d78b19c9ad6b101b8549aedc01c1c3c
Diego Viola <diego.viola@gmail.com>
2020-06-24 21:38:13 -0300

    Fix migration

--- db/migrate/20200303211551_add_original_metadata_to_work.rb
+++ db/migrate/20200303211551_add_original_metadata_to_work.rb
@@ -1,4 +1,4 @@
-class AddOriginalMetadataToWork < ActiveRecord::Migration[4.2]
+class AddOriginalMetadataToWork < ActiveRecord::Migration[6.0]
   def change
     add_column :works, :original_metadata, :text
   end
