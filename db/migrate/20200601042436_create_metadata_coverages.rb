236472d236c18679615ded2e6789e67ab401c223
Diego Viola <diego.viola@gmail.com>
2020-07-06 20:20:26 -0300

    metadata_coverages: default count to 0

--- db/migrate/20200601042436_create_metadata_coverages.rb
+++ db/migrate/20200601042436_create_metadata_coverages.rb
@@ -2,7 +2,7 @@ class CreateMetadataCoverages < ActiveRecord::Migration[6.0]
   def change
     create_table :metadata_coverages do |t|
       t.string :key
-      t.integer :count
+      t.integer :count, default: 0
       t.integer :collection_id
 
       t.timestamps
