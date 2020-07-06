27e9a9fd5b3d0ad7ca27f8009a4721d84eed7c2c
Diego Viola <diego.viola@gmail.com>
2020-07-06 19:54:26 -0300

    metadata_coverage: drop initial_count

--- app/models/metadata_coverage.rb
+++ app/models/metadata_coverage.rb
@@ -1,14 +1,6 @@
 class MetadataCoverage < ApplicationRecord
   belongs_to :collection
   has_one :facet_config
-  after_create :initial_count
   validates :key, uniqueness: { case_sensitive: true }
   validates :key, presence: true
-
-  def initial_count
-    if self.count.nil?
-      self.count = 1
-      self.save
-    end
-  end
 end
