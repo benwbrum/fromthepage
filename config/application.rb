8a14a1296e410bf5402dd08ddb9d529b5d2fd244
Diego Viola <diego.viola@gmail.com>
2020-07-16 16:19:18 -0300

    i18n: add pt to supported languages

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,6 @@ module Fromthepage
 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**/*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
-    config.i18n.available_locales = [:en, :es]
+    config.i18n.available_locales = [:en, :es, :pt]
   end
 end
