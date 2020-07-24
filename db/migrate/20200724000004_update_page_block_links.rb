329f4081407a3e8fde3f7ec15c8bcc55f758053c
saracarl <saracarl@gmail.com>
2020-07-24 09:48:32 -0500

    copied rails6 migrations to end of list

---
+++ db/migrate/20200724000004_update_page_block_links.rb
@@ -0,0 +1,21 @@
+class UpdatePageBlockLinks < ActiveRecord::Migration[5.0]
+  def change
+    pages = PageBlock.all
+    pages.each do |page|
+      page_changed = false
+      unless page.html.match("link_to('partly transcribed', demo_path)")
+        page.html&.gsub!(/link_to\('partly transcribed'.+'\)/,
+                       "link_to('partly transcribed', demo_path)")
+        page_changed = true
+      end
+      unless page.html.match("link_to('sign up', new_user_registration_path)")
+        page.html&.gsub!(/link_to\("sign up.+\)/m,
+                       "link_to('sign up', new_user_registration_path)")
+        page_changed = true
+      end
+      if page_changed
+        page.save
+      end
+    end
+  end
+end
