2fba2912c1386ac8bbb19a942f4a64e2f111f3c7
Ben W. Brumfield <benwbrum@gmail.com>
2020-07-29 13:51:34 -0500

    Fixes #1903 by removing includes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -154,7 +154,7 @@ class DashboardController < ApplicationController
       @owners = User.where(id: search_user_ids).where.not(account_type: nil)
     else
       # Get random Collections and DocSets from paying users
-      @owners = User.non_trial_owners.includes(:random_collections, :random_document_sets).order(:display_name)
+      @owners = User.findaproject_owners.order(:display_name)
 
       # Sampled Randomly down to 8 items for Carousel
       docsets = DocumentSet.carousel.includes(:owner).where(owner_user_id: @owners.ids).sample(5)
