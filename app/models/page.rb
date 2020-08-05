6ebfa1b4c7eaac3a84ff8d5cfe084a5abc282555
Ben W. Brumfield <benwbrum@gmail.com>
2020-08-05 08:21:28 -0500

    Fixes #1943 by converting div and br to newlines on plaintext export

--- app/models/page.rb
+++ app/models/page.rb
@@ -385,6 +385,8 @@ UPDATE `articles` SET graph_image=NULL WHERE `articles`.`id` IN (SELECT article_
   def formatted_plaintext_doc(doc)
     doc.xpath("//p").each { |n| n.add_next_sibling("\n")}
     doc.xpath("//lb").each { |n| n.replace("\n")}
+    doc.xpath("//br").each { |n| n.replace("\n")}
+    doc.xpath("//div").each { |n| n.add_next_sibling("\n")}
     doc.text.sub(/^\s*/m, '')
   end
 
