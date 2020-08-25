f17da35c60b4341c79b94f1fdfe832ddd208ed12
Diego Viola <diego.viola@gmail.com>
2020-08-25 16:01:59 -0300

    metadata: add validation messages to the facets view

--- app/controllers/facets_controller.rb
+++ app/controllers/facets_controller.rb
@@ -15,14 +15,23 @@ class FacetsController < ApplicationController
 
   def update
     collection = Collection.find(params[:collection_id])
+    errors = []
 
     collection.metadata_coverages.each do |m|
       metadata = params[:metadata][m[:key]]
       m.facet_config.update(label: metadata['label'],
                             input_type: metadata['input_type'],
                             order: metadata['order'])
+
+      if m.facet_config.errors.any?
+        errors << m.facet_config.errors.full_messages.first
+      end
     end
 
-    redirect_to collection_facets_path(collection.owner, collection), notice: "Collection facets updated successfully"
+    if errors.empty?
+      redirect_to collection_facets_path(collection.owner, collection), notice: "Collection facets updated successfully"
+    else
+      render('collection/facets', :locals => { :@metadata_coverages => collection.metadata_coverages, :@errors => errors })
+    end
   end
 end
