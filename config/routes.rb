860236503241f23259e42f7567d7650c20bd0140
Ben W. Brumfield <benwbrum@gmail.com>
2020-08-29 14:07:32 -0500

    Resolves #1522 by linking to a new notes feed

--- config/routes.rb
+++ config/routes.rb
@@ -158,6 +158,7 @@ Fromthepage::Application.routes.draw do
 
   scope 'deed', as: 'deed' do
     get 'list', to: 'deed#list'
+    get 'notes/:collection_id', to: 'deed#notes', as: 'notes'
   end
 
   scope 'static', as: 'static' do
