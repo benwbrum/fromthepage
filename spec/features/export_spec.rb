c1f1c3d713018963f8530f8a6a9c8f0c0c363bf0
saracarl <saracarl@gmail.com>
2021-01-26 14:57:17 -0600

    updated spec to reflect new page instead of straight zip export

--- spec/features/export_spec.rb
+++ spec/features/export_spec.rb
@@ -20,7 +20,7 @@ describe "export tasks" do
     expect(page).to have_content("Export All Works")
     expect(page).to have_content(@work.title)
     page.find('#btnExportAll').click
-    expect(page.response_headers['Content-Type']).to eq 'application/zip'
+    expect(page.response_headers['Content-Type']).to eq 'text/html'
   end
 
   it "exports the subject index" do
