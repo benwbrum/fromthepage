07fdeaaf1c66c7594f205696f91555ead080322d
Ben W. Brumfield <benwbrum@gmail.com>
2021-02-01 14:03:46 -0600

    Added subject details export for #2322

---
+++ lib/subject_details_exporter.rb
@@ -0,0 +1,44 @@
+module SubjectDetailsExporter
+  class Exporter
+    include Rails.application.routes.url_helpers
+
+    def initialize(collection)
+      @collection=collection
+      @subjects = collection.articles.includes(:categories, :page_article_links).order('articles.title')
+      @headers = [
+        'Title',
+        'External URI',
+        'Categories',
+        'Subject URI',
+        'Article Length (Words)', 
+        'Article Length (Characters)', 
+        'Number Occurrences',
+        'Origin',
+        'Created'
+      ]  # research helpers may follow
+    end
+
+    def export
+      csv_string = CSV.generate(force_quotes: true) do |csv|
+        csv << @headers
+
+        @subjects.each do |subject|
+          row = []
+          row << subject.title
+          row << subject.uri
+          row << subject.categories.map { |category| category.title }.join("; ")
+          row << Rails.application.routes.url_helpers.collection_article_show_url(@collection.owner, @collection, subject.id)
+          row << subject.source_text.split(/\s/).count
+          row << subject.source_text.chars.count
+          row << subject.page_article_links.count
+          row << subject.provenance.blank? ? 'FromThePage' : subject.provenance
+          row << subject.created_on.iso8601
+
+          csv << row
+        end
+      end
+      csv_string
+    end
+
+  end
+end
