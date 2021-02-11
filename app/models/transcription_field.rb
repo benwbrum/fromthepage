255a0ddb8514a5b08054860f1b9278b51bee8cdb
Ben W. Brumfield <benwbrum@gmail.com>
2021-02-11 16:40:48 -0600

    Get preview working for spreadsheet column config on #2343

--- app/models/transcription_field.rb
+++ app/models/transcription_field.rb
@@ -2,7 +2,7 @@ class TranscriptionField < ApplicationRecord
   belongs_to :collection, optional: true
   acts_as_list :scope => :collection
   has_many :table_cells
-  has_many :spreadsheet_columns, :dependent => :destroy
+  has_many :spreadsheet_columns, -> { order 'position' }, :dependent => :destroy
   validates :options, presence: true, if: Proc.new {|field| field.input_type == 'select'}, on: [:create, :update]
 
   validates :percentage, numericality: { allow_nil: true, greater_than: 0, less_than_or_equal_to: 100 }
