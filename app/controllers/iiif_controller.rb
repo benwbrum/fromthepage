81a65ed3bae10d0e3f3e497d98456874d5ee0fd8
Ben W. Brumfield <benwbrum@gmail.com>
2021-04-23 08:51:49 -0500

    Fix string literal line breaks

--- app/controllers/iiif_controller.rb
+++ app/controllers/iiif_controller.rb
@@ -171,10 +171,8 @@ class IiifController < ApplicationController
     if work.original_metadata
       manifest.metadata += JSON[work.original_metadata]
     end
-    work_metadata = work.attributes.except("id", "title", "description","created_on", "transcription_version", "owner_user_id", "restrict_scribes", "transcri
-ption_version", "transcription_conventions", "collection_id", "scribes_can_edit_titles", "supports_translation", "translat
-ion_instructions", "pages_are_meaningful", "ocr_correction", "slug", "picture", "featured_page", "original_metadata", "nex
-t_untranscribed_page", "in_scope").delete_if{|k,v| v.blank?}
+    work_metadata = work.attributes.except("id", "title", "description","created_on", "transcription_version", "owner_user_id", "restrict_scribes", "transcription_version", "transcription_conventions", "collection_id", "scribes_can_edit_titles", "supports_translation", "translation_instructions", "pages_are_meaningful", "ocr_correction", "slug", "picture", "featured_page", "original_metadata", "next_untranscribed_page", "in_scope").delete_if{|k,v| v.blank?}
+
     work_metadata.each_pair { |label,value| manifest.metadata << { "label" => label.titleize, "value" => value.to_s } }
 
     if work.sc_manifest
