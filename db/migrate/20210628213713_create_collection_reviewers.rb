7884e2e21a136d99cc622d2182088624bf733ef3
Ben W. Brumfield <benwbrum@gmail.com>
2021-07-17 12:42:43 -0500

    Fix db for tests

--- db/migrate/20210628213713_create_collection_reviewers.rb
+++ db/migrate/20210628213713_create_collection_reviewers.rb
@@ -1,10 +1,10 @@
 class CreateCollectionReviewers < ActiveRecord::Migration[5.0]
   def change
-    # create_table :collection_reviewers do |t|
-    #   t.references :user
-    #   t.references :collection
+    create_table :collection_reviewers do |t|
+      t.references :user
+      t.references :collection
 
-    #   t.timestamps
-    # end
+      t.timestamps
+    end
   end
 end
