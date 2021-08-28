396d954841f0957e1fd0bebeeb8466a0bcad26c1
Ben W. Brumfield <benwbrum@gmail.com>
2021-08-28 15:49:00 -0500

    Rotate email param for #1853

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,13 +1,19 @@
 class ContactController < ApplicationController
+  include ContactHelper
+
+
   def send_email
+    binding.pry
     render
 
-    ContactMailer.contact(
-      first_name: params[:first_name],
-      last_name:  params[:last_name],
-      email:      params[:email],
-      reason:     params[:reason],
-      more:       params[:more]
-    ).deliver!
+    unless params[email_param].blank?
+      ContactMailer.contact(
+        first_name: params[:first_name],
+        last_name:  params[:last_name],
+        email:      params[email_param],
+        reason:     params[:reason],
+        more:       params[:more]
+      ).deliver!
+    end
   end
 end
