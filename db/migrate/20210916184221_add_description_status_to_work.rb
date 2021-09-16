e6922477f7d076ecfc903080a800cdf3ee6279f3
Ben W. Brumfield <benwbrum@gmail.com>
2021-09-16 13:52:18 -0500

    Metadata description status for #2096

---
+++ db/migrate/20210916184221_add_description_status_to_work.rb
@@ -0,0 +1,5 @@
+class AddDescriptionStatusToWork < ActiveRecord::Migration[5.0]
+  def change
+    add_column :works, :description_status, :string, default: Work::DescriptionStatus::UNDESCRIBED
+  end
+end
