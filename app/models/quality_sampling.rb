63b756513e1e4c3c46054380f7ad5a0370587b4f
Ben W. Brumfield <benwbrum@gmail.com>
2022-02-20 07:09:26 -0600

    Change algorithms for sample set calculation

--- app/models/quality_sampling.rb
+++ app/models/quality_sampling.rb
@@ -1,9 +1,20 @@
 class QualitySampling < ApplicationRecord
   belongs_to :user
   belongs_to :collection
-  validates_presence_of :percent
+  validates_presence_of :sample_type
   before_create :calculate_field
   before_create :calculate_times
+
+  attr_writer :additional_pages
+
+  module SamplingType
+    EACH_USER='each_user'
+    EACH_WORK='each_work'
+    TOGETHER='together'
+    PERMUTATIONS='permutations'
+
+    ALL=[EACH_WORK,EACH_USER,TOGETHER,PERMUTATIONS]
+  end
 
   def possible_field
     # TODO scope to sample date range
@@ -13,7 +24,108 @@ class QualitySampling < ApplicationRecord
 
   def calculate_field
     ids = self.possible_field.reorder('pages.id').pluck(:id)
-    self.field=ids.shuffle
+    unique_field = calculate_unique_field
+    self.pages_in_sample = unique_field.count + @additional_pages.to_i
+    self.field=unique_field + (ids - unique_field).shuffle
+  end
+
+  def work_count
+    self.possible_field.pluck(:work_id).uniq.count
+  end
+
+  def user_count
+    self.possible_field.pluck(:last_editor_user_id).uniq.count
+  end
+
+  def together_count
+    self.covered_set_together.count
+  end
+
+  def permutation_count
+    self.possible_field.pluck(:work_id, :last_editor_user_id).uniq.count
+  end    
+
+  def calculate_unique_field
+    if self.sample_type == SamplingType::EACH_USER
+      covered_set_per_user
+    elsif self.sample_type == SamplingType::EACH_WORK
+      covered_set_per_work
+    elsif self.sample_type == SamplingType::TOGETHER
+      covered_set_together
+    else
+      covered_set_per_permutation
+    end
+  end
+
+  def covered_set_per_permutation
+    possible_field.group(:work_id, :last_editor_user_id).minimum('pages.id').values
+  end
+
+  def covered_set_per_user
+    possible_field.group(:last_editor_user_id).minimum(:last_editor_user_id).values
+  end
+
+  def covered_set_per_work
+    possible_field.group(:work_id).minimum(:page_id).values
+  end
+
+
+  def covered_set_together
+    # users = self.possible_field.pluck(:last_editor_user_id).uniq
+    # works = self.possible_field.pluck(:work_id).uniq
+    # permutations = self.possible_field.pluck(:last_editor_user_id, :work_id).uniq
+
+
+    # pseudocode
+    array_of_work_user_page_ids = possible_field.pluck(:last_editor_user_id, :work_id, :id)
+    covered_user_ids = []
+    covered_work_ids = []
+    working_page_id_list = []
+
+    count_per_user = self.possible_field.group(:last_editor_user_id).count
+    count_per_work = self.possible_field.group(:work_id).count
+
+    max_number_pages = [count_per_user.max_by{|k,v| v}[1],count_per_work.max_by{|k,v| v}[1]].max
+
+    1.upto(max_number_pages) do |current_count|
+      # binding.pry if current_count > 20
+      users_at_current_count = count_per_user.select{|k,v| v==current_count}
+      users_at_current_count.each do |user_id, count|
+        unless covered_user_ids.include?(user_id)
+          new_unique_page_by_work = 
+            array_of_work_user_page_ids.select{|triple| triple[0]==user_id}.detect{|triple| !covered_work_ids.include?(triple[1])}
+          if new_unique_page_by_work
+            working_page_id_list << new_unique_page_by_work[2]
+            covered_user_ids << user_id
+            covered_work_ids << new_unique_page_by_work[1]
+          else
+            triple =
+              array_of_work_user_page_ids.select{|triple| triple[0]==user_id}.sample(1).first
+            working_page_id_list << triple[2]
+            covered_user_ids << user_id
+          end
+        end
+      end
+
+      works_at_current_count = count_per_work.select{|k,v| v==current_count}
+      works_at_current_count.each do |work_id, count|
+        unless covered_work_ids.include?(work_id)
+          unique_triple_by_user = 
+            array_of_work_user_page_ids.select{|triple| triple[1]==work_id}.detect{|triple| !covered_user_ids.include?(triple[0])}
+          if unique_triple_by_user
+            working_page_id_list << unique_triple_by_user[2]
+            covered_work_ids << work_id
+            covered_user_ids << unique_triple_by_user[0]
+          else
+            triple =
+              array_of_work_user_page_ids.select{|triple| triple[1]==work_id}.sample(1).first
+            working_page_id_list << triple[2]
+            covered_work_ids << work_id
+          end
+        end
+      end
+    end
+    working_page_id_list
   end
 
   def calculate_times
@@ -45,7 +157,7 @@ class QualitySampling < ApplicationRecord
   end
 
   def sample_page_count
-    total_field_size * percent / 100
+    self.pages_in_sample
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_09_205457) do
+ActiveRecord::Schema.define(version: 2022_02_17_214352) do
 
   create_table "ahoy_activity_summaries", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "date"
@@ -563,7 +563,6 @@ ActiveRecord::Schema.define(version: 2022_02_09_205457) do
   end
 
   create_table "quality_samplings", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.decimal "percent", precision: 10
     t.timestamp "start_time"
     t.timestamp "previous_start"
     t.integer "user_id", null: false
@@ -571,6 +570,8 @@ ActiveRecord::Schema.define(version: 2022_02_09_205457) do
     t.text "field", size: :medium
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "pages_in_sample"
+    t.string "sample_type"
     t.index ["collection_id"], name: "index_quality_samplings_on_collection_id"
     t.index ["user_id"], name: "index_quality_samplings_on_user_id"
   end
