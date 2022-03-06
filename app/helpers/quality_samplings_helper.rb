3a28f62c978863008f54b5edc5ff2fedf0cf4811
Nick Seryakov <nick.seryakov@gmail.com>
2022-03-07 03:03:06 +0400

    Adjust quality samplings markup and styles

--- app/helpers/quality_samplings_helper.rb
+++ app/helpers/quality_samplings_helper.rb
@@ -4,7 +4,7 @@ module QualitySamplingsHelper
     t(".approval_delta_display_#{approval_delta_to_quintile(mean_approval_delta,max_approval_delta)}")
   end
   def approval_delta_to_style(mean_approval_delta,max_approval_delta)
-    "approval-delta-#{approval_delta_to_quintile(mean_approval_delta,max_approval_delta)}"
+    "approval-delta approval-delta-#{approval_delta_to_quintile(mean_approval_delta,max_approval_delta)}"
   end
   def approval_delta_to_quintile(mean_approval_delta,max_approval_delta)
     ((mean_approval_delta / max_approval_delta)*4).round
