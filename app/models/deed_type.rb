59a8c7d1f9b79ed4fb5c844882adf4bb0852c2aa
Sylvie Daines <syisda@gmail.com>
2022-08-02 17:14:25 -0500

    Changed deed type hash to be translatable

--- app/models/deed_type.rb
+++ app/models/deed_type.rb
@@ -31,26 +31,26 @@ class DeedType
   # groups of deed types and also their human-readable names. Any new deed type
   # constant should also be added here.
   TYPES = {
-    PAGE_TRANSCRIPTION => 'Page Transcribed',
-    PAGE_EDIT => 'Page Edited',
-    PAGE_INDEXED => 'Page Indexed',
-    PAGE_MARKED_BLANK => 'Page Marked Blank',
-    ARTICLE_EDIT => 'Article Edited',
-    NOTE_ADDED => 'Note Added',
-    PAGE_TRANSLATED => 'Page Translated',
-    PAGE_TRANSLATION_EDIT => 'Translation Page Edited',
-    OCR_CORRECTED => 'Page OCR Corrected',
-    NEEDS_REVIEW => 'Page Needs Review',
-    TRANSLATION_REVIEW => 'Translation Page Needs Review',
-    TRANSLATION_INDEXED => 'Translation Page Indexed',
-    WORK_ADDED => 'Work Added',
-    COLLECTION_ACTIVE => 'Collection Active',
-    COLLECTION_INACTIVE => 'Collection Inactive',
-    COLLECTION_JOINED => 'Collection Joined',
-    PAGE_REVIEWED => 'Page Reviewed',
-    TRANSLATION_REVIEWED => 'Translation Reviewed',
-    DESCRIBED_METADATA => 'Described Metadata',
-    EDITED_METADATA => 'Edited Metadata Description'
+    PAGE_TRANSCRIPTION => I18n.t('deed.page_transcription'),
+    PAGE_EDIT => I18n.t('deed.page_edit'),
+    PAGE_INDEXED => I18n.t('deed.page_indexed'),
+    PAGE_MARKED_BLANK => I18n.t('deed.page_marked_blank'),
+    ARTICLE_EDIT => I18n.t('deed.article_edit'),
+    NOTE_ADDED => I18n.t('deed.note_added'),
+    PAGE_TRANSLATED => I18n.t('deed.page_translated'),
+    PAGE_TRANSLATION_EDIT => I18n.t('deed.page_translation_edit'),
+    OCR_CORRECTED => I18n.t('deed.ocr_corrected'),
+    NEEDS_REVIEW => I18n.t('deed.needs_review'),
+    TRANSLATION_REVIEW => I18n.t('deed.translation_review'),
+    TRANSLATION_INDEXED => I18n.t('deed.translation_indexed'),
+    WORK_ADDED => I18n.t('deed.work_added'),
+    COLLECTION_ACTIVE => I18n.t('deed.collection_active'),
+    COLLECTION_INACTIVE => I18n.t('deed.collection_inactive'),
+    COLLECTION_JOINED => I18n.t('deed.collection_joined'),
+    PAGE_REVIEWED => I18n.t('deed.page_reviewed'),
+    TRANSLATION_REVIEWED => I18n.t('deed.translation_reviewed'),
+    DESCRIBED_METADATA => I18n.t('deed.described_metadata'),
+    EDITED_METADATA => I18n.t('deed.edited_metadata') 
   }
 
   # This `class << self` inherited group replaces the need to call `self.` on
