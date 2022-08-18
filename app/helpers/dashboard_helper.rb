771977b2a8729ff7479d2876bd2c47c1914e32ff
Sylvie Daines <syisda@gmail.com>
2022-08-18 16:13:32 -0500

    Moved all snippet methods to helper method

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -15,9 +15,12 @@ module DashboardHelper
     end
   end
 
-  def strip_style(html)
-    doc = Nokogiri::HTML(html)
-    doc.xpath('//style').each { |n| n.remove }
-    doc.to_s
+  # makes an intro block into a snippet by removing style tag, stripping tags, and truncating
+  def to_snippet(intro_block)
+    # remove style tag, Loofah.fragment.text doesn't do this (strip_tags does)
+    doc = Nokogiri::HTML(intro_block)
+    doc.xpath('//style').each { |n| n.remove } 
+    # strip tags and truncate
+    truncate(Loofah.fragment(doc.to_s).text(encode_special_chars: false), length: 300, separator: ' ') || '' 
   end
 end
