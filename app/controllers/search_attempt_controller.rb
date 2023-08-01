aefccfe78f1830afacccef411b4ea580e09c6494
Sylvie Daines <syisda@gmail.com>
2023-08-01 15:44:17 -0500

    Fixed exception on guest user search

--- app/controllers/search_attempt_controller.rb
+++ app/controllers/search_attempt_controller.rb
@@ -1,9 +1,13 @@
 class SearchAttemptController < ApplicationController
     def create
-        @search_attempt = SearchAttempt.new(
-            query: params[:search], 
-            user_id: current_user.id, 
-            owner: current_user.owner)
+        if current_user.nil?
+            @search_attempt = SearchAttempt.new(query: params[:search])
+        else
+            @search_attempt = SearchAttempt.new(
+                query: params[:search], 
+                user_id: current_user.id, 
+                owner: current_user.owner)
+        end
         @search_attempt.save
         
         ajax_redirect_to(search_attempt_show_path(@search_attempt))
