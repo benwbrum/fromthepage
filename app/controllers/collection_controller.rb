85b6dfefe992a6d35954987a16ec7317abe18123
Sylvie Daines <syisda@gmail.com>
2023-08-24 11:24:39 -0600

    Show works list to transcribers for #3717

--- app/controllers/collection_controller.rb
+++ app/controllers/collection_controller.rb
@@ -10,7 +10,7 @@ class CollectionController < ApplicationController
                                    :set_collection_intro_block,
                                    :set_collection_footer_block]
 
-  before_action :authorized?, :only => [:new, :edit, :update, :delete, :works_list]
+  before_action :authorized?, :only => [:new, :edit, :update, :delete]
   before_action :review_authorized?, :only => [:reviewer_dashboard, :works_to_review, :one_off_list, :recent_contributor_list, :user_contribution_list]
   before_action :set_collection, :only => [:show, :edit, :update, :contributors, :new_work, :works_list, :needs_transcription_pages, :needs_review_pages, :start_transcribing]
   before_action :load_settings, :only => [:edit, :update, :upload, :edit_owners, :block_users, :remove_owner, :edit_collaborators, :remove_collaborator, :edit_reviewers, :remove_reviewer]
