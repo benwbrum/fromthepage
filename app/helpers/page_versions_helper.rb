2e7264e8bc84f24b23e685bd191d9178e42c7bff
symmetrically <sadanmasroor87@gmail.com>
2023-09-05 21:30:45 +0500

    internationalization for status

--- app/helpers/page_versions_helper.rb
+++ app/helpers/page_versions_helper.rb
@@ -1,14 +1,16 @@
 module PageVersionsHelper
     def render_status_icon(status)
-        case status
-        when "transcribed"
-            image_tag("icons/circle-check-solid.svg", alt: "Conplete Icon", class: "complete-icon", style: "width: 20px; height: 20px; display: inline; margin-left: 10px;", title: status)
-        when "incomplete"
-            image_tag("icons/caution-icon.svg", alt: "Incomplete Icon", class: "incomplete-icon", style: "width: 20px; height: 20px; display: inline; margin-left: 10px;", title: status)
-        when "review"
-            image_tag("icons/magnifying-glass-solid.svg", alt: "Review Icon", class: "review-icon", style: "width: 20px; height: 20px; display: inline; margin-left: 10px;", title: status)
-        when "blank"
-            image_tag("icons/circle.svg", alt: "Review Icon", class: "review-icon", style: "width: 20px; height: 20px; display: inline; margin-left: 10px;", title: status)
+
+        status_translation = I18n.t("page_version.show.#{status}", default: status)
+
+        if status == "transcribed" || status == "incomplete" || status == "review" || status == "blank"
+            image_tag(
+                "icons/#{status}-icon.svg",
+                alt: "#{status_translation} Icon",
+                class: "#{status}-icon",
+                style: "width: 20px; height: 20px; display: inline; margin-left: 10px; margin-right: 10px;",
+                title: status_translation
+            )
         else
             image_tag("icons/custom-icon.svg", alt: "Custom Icon", class: "custom-icon", style: "width: 20px; height: 20px; display: inline; margin: 10px;", title: status)
         end
