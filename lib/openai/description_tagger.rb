12690196f82f6b53372387e3a24a4201c21a19d9
Ben W. Brumfield <benwbrum@gmail.com>
2023-10-11 11:47:13 -0500

    Refine AI process for tagging collections for #3051

--- lib/openai/description_tagger.rb
+++ lib/openai/description_tagger.rb
@@ -1,21 +1,48 @@
 module DescriptionTagger
   def self.tag_description_by_subject(description, tags, title="")
     client = OpenAI::Client.new
+    prompt = prompt_by_subject(description, tags, title)
     response = client.completions(
       parameters: {
         model: "gpt-3.5-turbo-instruct",
-        prompt: prompt_by_subject(description, tags, title),
+        prompt: prompt,
         max_tokens: 100,
         n: 1,
-        temperature: 0.7,
+        temperature: 0.0,
         top_p: 1.0,
         frequency_penalty: 0.0,
         presence_penalty: 0.0
       }
     )
+    print prompt
+    print "\n"
+    if response['choices'].nil?
+      pp response
+      return []
+    end
+
     raw_text = response['choices'].first['text']
+
+    response_tags = []
     print raw_text
-    response_tags = raw_text.split(', ').map{|e| e.strip}
+    print "\n\n\n"
+    unless raw_text.match(/NOT ENOUGH INFORMATION/)
+      begin
+        response_tags = JSON.parse(raw_text)
+        if response_tags.is_a? String
+          response_tags = response_tags.split(',').map{|e| e.gsub('[','').gsub(']','').gsub('"','').gsub(/\b'/,'').gsub(/'\b/,'').strip}
+        end
+
+      rescue JSON::ParserError => e
+        print "WARNING: response could not be parsed as JSON\nRESPONSE: #{raw_text}\n"
+        raw_text.gsub!('POSSIBLE_TAGS:', '')
+        raw_text.gsub!('RESPONSE:', '')
+        raw_text.gsub!('Tags:', '')
+        response_tags = raw_text.split(',').map{|e| e.gsub('[','').gsub(']','').gsub('"','').gsub(/\b'/,'').gsub(/'\b/,'').strip}
+      end
+    end
+    pp response_tags
+    response_tags
   end
 
   def tag_description_by_date(description, tags, title)
