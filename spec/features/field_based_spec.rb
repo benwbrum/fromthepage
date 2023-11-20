94a0251945752d64958a948a9f9591f7416a6fec
symmetrically <sadanmasroor87@gmail.com>
2023-11-20 19:28:34 +0500

    fixed failing rspec

--- spec/features/field_based_spec.rb
+++ spec/features/field_based_spec.rb
@@ -130,7 +130,7 @@ describe "collection settings js tasks", :order => :defined do
     visit collection_export_path(@collection.owner, @collection)
     expect(page).to have_content("Export Individual Works")
     page.find('tr', text: work.title).find('.btnCsvTblExport').click
-    expect(page.response_headers['Content-Type']).to eq 'application/csv'
+    expect(page.response_headers['Content-Type']).to eq 'text/csv'
   end
 
 
