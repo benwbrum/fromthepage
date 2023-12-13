516f989301a7d836e8b5e2db7976cf3299e38faf
Ben W. Brumfield <benwbrum@gmail.com>
2023-12-13 15:33:14 -0600

    Remove obscured button problem

--- spec/features/editor_actions_spec.rb
+++ spec/features/editor_actions_spec.rb
@@ -313,10 +313,7 @@ describe "editor actions" , :order => :defined do
       #because of the note, page.source_text should not have changed
       expect(new_text).to eq text
       #save the note
-      print "\n\n\nPage contents before saving a note:\n"
-      print page.text
-      print "\n\n\n"
-#      find('#blankPageButton').click
+      find('#blankPageButton').click
       find('#save_note_button').click
       expect(test_page.notes.count).not_to be nil
     end
