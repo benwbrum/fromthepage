e61892a1466812f41b15a5689ac9ef39b89635a1
WillNigel23 <willnigelcdejesus@gmail.com>
2024-06-26 02:59:37 +0800

    Clean orphaned page article link records

--- app/models/page.rb
+++ app/models/page.rb
@@ -56,9 +56,8 @@ class Page < ApplicationRecord
   acts_as_list :scope => :work
   belongs_to :last_editor, :class_name => 'User', :foreign_key => 'last_editor_user_id', optional: true
 
-
-  has_many :page_article_links, :dependent => :destroy
-  has_many :articles, :through => :page_article_links
+  has_many :page_article_links, dependent: :destroy
+  has_many :articles, through: :page_article_links
   has_many :page_versions, -> { order 'page_version DESC' }, :dependent => :destroy
 
   belongs_to :current_version, :class_name => 'PageVersion', :foreign_key => 'page_version_id', optional: true
@@ -529,7 +528,7 @@ class Page < ApplicationRecord
     if self.page_article_links.present?
       self.clear_article_graphs
       # clear out the existing links to this page
-      PageArticleLink.where("page_id = #{self.id} and text_type = '#{text_type}'").delete_all
+      PageArticleLink.where("page_id = #{self.id} and text_type = '#{text_type}'").destroy_all
     end
   end
 
