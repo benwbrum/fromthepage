d8cbf2f4728a2b5dec3630a18f30871251b9413b
Will Nigel De Jesus <70936977+WillNigel23@users.noreply.github.com>
2024-07-19 00:48:06 +0800

    Clean orphaned page article link records (#4175)

---
+++ db/migrate/20240625185153_clean_orphan_page_link_records.rb
@@ -0,0 +1,9 @@
+class CleanOrphanPageLinkRecords < ActiveRecord::Migration[6.1]
+
+  def up
+    page_article_links = PageArticleLink.where(article_id: nil).or(PageArticleLink.where(page_id: nil))
+
+    page_article_links.in_batches(of: 1000).destroy_all
+  end
+
+end
