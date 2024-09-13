29dfc2c38c813d7725665c6c719c7cc396aacd47
worleydl <danworley@gmail.com>
2024-09-13 16:16:43 -0400

    Export method

--- lib/elastic_util.rb
+++ lib/elastic_util.rb
@@ -1,6 +1,6 @@
 module ElasticUtil
 
-  def get_client(enable_log = false)
+  def self.get_client(enable_log = false)
     return Elasticsearch::Client.new(
       log: enable_log,
       cloud_id: ELASTIC_CLOUD_ID,
