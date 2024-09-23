85608b69760efcbb5a79f14b33d64fe0891d9f35
worleydl <danworley@gmail.com>
2024-09-23 16:44:53 -0400

    Multithreaded indexing

--- lib/elastic_util.rb
+++ lib/elastic_util.rb
@@ -23,6 +23,11 @@ module ElasticUtil
   def self.reindex(model, index_name)
     client = self.get_client()
 
+    # Allow simulatenous indexing requests
+    pool_size= 2
+    pool = WorkerPool.new(pool_size)
+    pool.start
+
     model.find_in_batches(batch_size: 1000) do |batch|
       bulk_body = []
       batch.each do |item|
@@ -31,10 +36,63 @@ module ElasticUtil
         )
       end
 
-      client.bulk(body: bulk_body, refresh: false)
+      # Don't overload pending tasks
+      while pool.pending > pool_size * 2
+        sleep(1)
+      end
+
+      # Execute requests in new thread, it takes more time to prep the the next batch
+      pool.schedule do
+        begin
+          client.bulk(body: bulk_body, refresh: false)
+        rescue => e
+          puts "Error with threaded bulk request"
+        end
+      end
     end
 
+    # Finish remaining tasks and shutdown
+    pool.shutdown()
+
     client.indices.refresh(index: index_name)
+  end
+
+  # Helper threading pool to execute indexing requests in parallel
+  class WorkerPool
+    def initialize(size)
+      @size = size
+      @queue = Queue.new
+      @threads = []
+      @shutdown = false
+    end
+
+    def start
+      @size.times do
+        @threads << Thread.new do
+          until @shutdown && @queue.size == 0
+            task = @queue.pop(true) rescue nil
+            task&.call
+
+            if @queue.size == 0
+              sleep(1)
+            end
+          end
+        end
+      end
+    end
+
+    def pending
+      return @queue.size
+    end
+
+    def schedule(&task)
+      @queue << task
+    end
+
+    def shutdown
+      @shutdown = true
+      @threads.each(&:join)
+    end
   end
 
 end
