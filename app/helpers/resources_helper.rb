00bebe0b4952e9b802147f907b90351bd9746d20
WillNigel23 <willnigelcdejesus@gmail.com>
2025-01-03 06:26:45 +0800

    Fix category options ordering

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -7,4 +7,20 @@ module ResourcesHelper
       info: '#icon-warning-sign'
     }
   end
+
+  def categories_options(categories)
+    options = []
+    categories.walk_tree do |c, level|
+      options << [
+        c.title,
+        c.id,
+        {
+          aria: { label: I18n.t('category.options') },
+          data: { level: level }
+        }
+      ]
+    end
+
+    options
+  end
 end
