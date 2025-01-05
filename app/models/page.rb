5563fa7ac93f976b6bf26f46c0f0f97536c2a47b
worleydl <danworley@gmail.com>
2025-01-05 15:45:38 -0500

    Prep page schema for filters

--- app/models/page.rb
+++ app/models/page.rb
@@ -132,8 +132,10 @@ class Page < ApplicationRecord
     return {
       _id: self.id,
       collection_id: self.collection&.id,
+      docset_id: self.work&.document_sets&.pluck(:id),
+      owner_user_id: self.collection&.owner_user_id,
       work_id: self.work&.id,
-      is_public: !self.collection&.restricted || self.work.document_sets.where(:is_public => true).exists?,
+      is_public: !self.collection&.restricted || self.work&.document_sets.where(:is_public => true).exists?,
       search_text: self.search_text,
       content_english: self.source_text # TODO: Hook up language pipeline
     }
