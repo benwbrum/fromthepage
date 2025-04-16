788d77c9ce4225adea3d266a9858b4f819f05a5b
WillNigel23 <willnigelcdejesus@gmail.com>
2025-04-17 06:57:19 +0800

    4615 - PDF deletion nested in underline

--- lib/pandoc/risky_soul_filter.lua
+++ lib/pandoc/risky_soul_filter.lua
@@ -1,17 +1,8 @@
-local SINGLE_QUOTE_PLACEHOLDER = "__SINGLEQUOTE__"
-local BACKTICK_PLACEHOLDER = "__BACKTICK__"
-
 function Span(el)
   -- Escape special characters like backticks and single quotes
   local content = pandoc.utils.stringify(el.content)
-  if content then
-    content = content:gsub(SINGLE_QUOTE_PLACEHOLDER, "`")
-    content = content:gsub(BACKTICK_PLACEHOLDER, "`")
-  end
 
-  if el.classes:includes("textulst") then
-    return pandoc.RawInline("latex", "\\textulst{" .. pandoc.utils.stringify(content) .. "}")
-  elseif el.classes:includes("textul") then
-    return pandoc.RawInline("latex", "\\underline{" .. pandoc.utils.stringify(content) .. "}")
+  if el.classes:includes("ulst") then
+    return pandoc.RawInline("latex", "\\ulst{" .. pandoc.utils.stringify(content) .. "}")
   end
 end
