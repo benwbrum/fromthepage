bdeb9bd4b8ffd6f9ec4e525ab319c333819b832e
WillNigel23 <willnigelcdejesus@gmail.com>
2025-04-30 02:42:01 +0800

    4634 - Fix broken URI parsing

--- app/models/page.rb
+++ app/models/page.rb
@@ -719,8 +719,8 @@ class Page < ApplicationRecord
     elsif self.ia_leaf
       self.ia_leaf.facsimile_url
     else
-      encoded_path = URI::DEFAULT_PARSER.escape(self.canonical_facsimile_url)
-      uri = URI.parse(encoded_path)
+      encoded_url = Addressable::URI.parse(self.canonical_facsimile_url).normalize.to_s
+      uri = URI.parse(encoded_url)
       # if we are in test, we will be http://localhost:3000 and need to separate out the port from the host
       raw_host = Rails.application.config.action_mailer.default_url_options[:host]
       host = raw_host.split(":")[0]
