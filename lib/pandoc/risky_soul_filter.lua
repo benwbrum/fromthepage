cdef8ae829f4a2b57958503101c424d4a31a2c21
WillNigel23 <willnigelcdejesus@gmail.com>
2025-05-07 00:50:58 +0800

    Decrease size of notes

--- lib/pandoc/risky_soul_filter.lua
+++ lib/pandoc/risky_soul_filter.lua
@@ -1,8 +1,9 @@
 function Span(el)
-  -- Escape special characters like backticks and single quotes
   local content = pandoc.utils.stringify(el.content)
 
   if el.classes:includes("ulst") then
     return pandoc.RawInline("latex", "\\ulst{" .. pandoc.utils.stringify(content) .. "}")
+  elseif el.classes:includes("smalltext") then
+    return pandoc.RawInline("latex", "\\smalltext{" .. pandoc.utils.stringify(el.content) .. "}")
   end
 end
