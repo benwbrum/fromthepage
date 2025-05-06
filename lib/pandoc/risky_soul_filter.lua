050971a43a211bf0eda9c3610dddb65fe51337a8
WillNigel23 <willnigelcdejesus@gmail.com>
2025-05-07 01:39:26 +0800

    Add block smalltext effect

--- lib/pandoc/risky_soul_filter.lua
+++ lib/pandoc/risky_soul_filter.lua
@@ -1,9 +1,19 @@
+-- Custom underline-strike effect
 function Span(el)
   local content = pandoc.utils.stringify(el.content)
 
   if el.classes:includes("ulst") then
     return pandoc.RawInline("latex", "\\ulst{" .. pandoc.utils.stringify(content) .. "}")
-  elseif el.classes:includes("smalltext") then
-    return pandoc.RawInline("latex", "\\smalltext{" .. pandoc.utils.stringify(el.content) .. "}")
+  end
+end
+
+-- Custom small font text effect
+function Div(el)
+  if el.classes:includes("smalltext") then
+    return {
+      pandoc.RawBlock("latex", "\\begin{smalltext}"),
+      el,
+      pandoc.RawBlock("latex", "\\end{smalltext}")
+    }
   end
 end
