949a25034754987590a9a79d64af4970faf1e8e9
Will Nigel De Jesus <70936977+WillNigel23@users.noreply.github.com>
2025-05-28 02:49:39 +0800

    4644 - Collection data export task (#4645)
    * 4464 - Collection data export task
    
    * 4644 - Fix typo in model
    
    * 4644 - Add assets to export process
    
    * 4644 - Fix specs
    
    * 4644 - Add import task

---
+++ spec/interactors/database/export/dump_builder_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe Database::Export::DumpBuilder do
+  let(:dump_key) { 'dump_tester' }
+  let!(:user) do
+    create(
+      :unique_user,
+      :owner,
+      login: "#{dump_key}_login",
+      email: "#{dump_key}@sample.com"
+    )
+  end
+  let!(:collection) { create(:collection, :with_picture, title: "Collection #{dump_key}", owner_user_id: user.id) }
+  let!(:work) { create(:work, collection: collection, title: "Work #{dump_key}", owner_user_id: user.id) }
+  let!(:work_statistic) { create(:work_statistic, work: work) }
+  let!(:page) { create(:page, work: work) }
+  let(:path) { "/tmp/dumps/#{dump_key}" }
+
+  let(:result) do
+    described_class.new(collection_slugs: [collection.slug], path: path).call
+  end
+
+  before do
+    dir = Rails.root.join(path)
+
+    FileUtils.rm_rf(dir)
+    FileUtils.mkdir_p(dir)
+  end
+
+  around do |example|
+    ActiveRecord::Base.transaction do
+      example.run
+
+      raise ActiveRecord::Rollback
+    end
+  end
+
+  it 'creates dumps' do
+    expect(result.success?).to be_truthy
+    expect(Dir.children(path).count { |f| File.file?(File.join(path, f)) }).to eq(32)
+  end
+end
