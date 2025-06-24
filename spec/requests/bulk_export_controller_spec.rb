86d89aa2208bb9642b1d8baef4b0b83734bcef6c
Sara Brumfield <saracarl@gmail.com>
2025-06-24 16:08:53 -0500

    Restrict bulk export index and show to admin

---
+++ spec/requests/bulk_export_controller_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe BulkExportController do
+  let!(:admin) { create(:admin) }
+  let!(:bulk_export) { create(:bulk_export, user_id: admin.id) }
+
+  describe '#index' do
+    let(:action_path) { bulk_export_index_path }
+    let(:subject) { get action_path }
+
+    it 'redirects when not logged in' do
+      subject
+      expect(response).to have_http_status(:redirect)
+      expect(response).to redirect_to(dashboard_path)
+    end
+
+    it 'renders when logged in as admin' do
+      login_as admin
+      subject
+      expect(response).to have_http_status(:ok)
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe '#show' do
+    let(:action_path) { bulk_export_show_path(bulk_export_id: bulk_export.id) }
+    let(:subject) { get action_path }
+
+    it 'redirects when not logged in' do
+      subject
+      expect(response).to have_http_status(:redirect)
+      expect(response).to redirect_to(dashboard_path)
+    end
+
+    it 'renders when logged in as admin' do
+      login_as admin
+      subject
+      expect(response).to have_http_status(:ok)
+      expect(response).to render_template(:show)
+    end
+  end
+end
