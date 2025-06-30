b47668615f8de9460023754952025862222e0b46
WillNigel23 <willnigelcdejesus@gmail.com>
2025-07-01 02:43:39 +0800

    WIP

--- app/jobs/work/update_statistic_job.rb
+++ app/jobs/work/update_statistic_job.rb
@@ -1,5 +1,10 @@
 class Work::UpdateStatisticJob < ApplicationJob
-
   queue_as :default
 
+  def perform(work_id:)
+    work = Work.find(work_id)
+
+    work.work_statistic = WorkStatistic.new unless work.work_statistic
+    work.work_statistic.recalculate
+  end
 end
