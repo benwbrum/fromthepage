d90be84d79071a49c1b17f97479940ee4e598078
Ben W. Brumfield <benwbrum@gmail.com>
2025-09-12 14:50:33 -0500

    Update for rubocop

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{fromthepage@45.79.222.38}
-role :db, %w{fromthepage@45.79.222.38}
-#role :web, %w{deploy@example.com}
+role :app, %w[fromthepage@45.79.222.38]
+role :db, %w[fromthepage@45.79.222.38]
+# role :web, %w{deploy@example.com}
 
 
 # Extended Server Syntax
@@ -15,8 +15,8 @@ role :db, %w{fromthepage@45.79.222.38}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-#server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
-server '45.79.222.38', user: 'fromthepage', roles: %{app}
+# server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server '45.79.222.38', user: 'fromthepage', roles: %(app)
 
 
 # Custom SSH Options
