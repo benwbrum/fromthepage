893360911894b984b386295b4375fc1bc322572b
WillNigel23 <willnigelcdejesus@gmail.com>
2025-09-23 00:36:27 +0800

    4914 - CDM import does not reindex properly

--- app/controllers/sc_collections_controller.rb
+++ app/controllers/sc_collections_controller.rb
@@ -257,6 +257,15 @@ class ScCollectionsController < ApplicationController
       ocr_text = ocr ? 'and OCR text ' : ''
       flash[:notice] = t('.metadata_is_being_imported', ocr_text: ocr_text)
     end
+
+    # TODO: This patches CDM imports. It would be good to refactor it into interactor to make testing easier
+    Elasticsearch::Collection::SyncJob.perform_later(
+      user_id: current_user.id,
+      collection_id: @collection.id,
+      type: @collection.is_a?(DocumentSet) ? :document_set : :collection,
+      skip_collection: false
+    )
+
     if @collection.text_entry?
       redirect_to collection_read_work_path(@collection.owner, @collection, work)
     else
--- app/jobs/elasticsearch/collection/sync_job.rb
+++ app/jobs/elasticsearch/collection/sync_job.rb
@@ -1,9 +1,10 @@
 class Elasticsearch::Collection::SyncJob < ApplicationJob
   queue_as :default
 
-  def perform(user_id:, collection_id:, type: :collection)
+  def perform(user_id:, collection_id:, type: :collection, skip_collection: :true)
     collection = type == :collection ? Collection.find(collection_id) : DocumentSet.find(collection_id)
 
+    CollectionsIndex.import collection unless skip_collection
     WorksIndex.import collection.works
     PagesIndex.import collection.pages
   end
