02268debf1df57c2c464e4429dc06833c8b70aab
WillNigel23 <willnigelcdejesus@gmail.com>
2025-10-07 07:37:06 +0800

    4958 - Default SolidQueue disabled for main branch

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -93,7 +93,4 @@ Rails.application.configure do
   # Add a custom hostname for uploads
   # config.upload_host = 'uploads'
   # config.hosts << "#{config.upload_host}.localhost"
-
-  config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.logger = ActiveSupport::Logger.new(Rails.root.join('log/queue.log'))
 end
