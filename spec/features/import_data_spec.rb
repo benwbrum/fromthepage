4e84f5f349fb9e895671de0ff6b560433a4813a3
WillNigel23 <willnigelcdejesus@gmail.com>
2025-10-22 04:44:18 +0800

    4984 - Wrap whole test in VCR block

--- spec/features/import_data_spec.rb
+++ spec/features/import_data_spec.rb
@@ -18,56 +18,56 @@ describe 'import data' do
     let(:bad_item_url)  { 'https://hrc.contentdm.oclc.org/digital/collection/p15878coll90/id/41/rec/3' }
 
     it "browses a single record", js: true do
-      owner = User.first
-      login_as(owner, scope: :user)
-      visit dashboard_owner_path
-      page.find('.tabs').click_link("Start A Project")
-      page.find(:css, '#import-contentdm').click
       VCR.use_cassette('cdm/midpoint-shelwater-item', record: :none) do
+        owner = User.first
+        login_as(owner, scope: :user)
+        visit dashboard_owner_path
+        page.find('.tabs').click_link("Start A Project")
+        page.find(:css, '#import-contentdm').click
         page.fill_in 'cdm_url', with: item_url
         page.find('#cdm_import').click
+        expect(page).to have_content('Manifest: Letter with envelope from Virginia Shewalter', wait: 30)
       end
-      expect(page).to have_content('Manifest: Letter with envelope from Virginia Shewalter', wait: 30)
     end
 
     it 'browses records from a collection', js: true do
-      owner = User.first
-      login_as(owner, scope: :user)
-      visit dashboard_owner_path
-      page.find('.tabs').click_link('Start A Project')
-      page.find(:css, "#import-contentdm").click
       VCR.use_cassette('cdm/midpoint-shelwater-collection', record: :none) do
+        owner = User.first
+        login_as(owner, scope: :user)
+        visit dashboard_owner_path
+        page.find('.tabs').click_link('Start A Project')
+        page.find(:css, "#import-contentdm").click
         page.fill_in 'cdm_url', with: collection_url
         page.find('#cdm_import').click
+        expect(page).to have_content('Collection: The Virginia Shewalter Letters Collection', wait: 30)
       end
-      expect(page).to have_content('Collection: The Virginia Shewalter Letters Collection', wait: 30)
     end
 
     it 'browses collections from a repository', js: true do
-      owner = User.first
-      login_as(owner, scope: :user)
-      visit dashboard_owner_path
-      page.find('.tabs').click_link("Start A Project")
-      page.find(:css, '#import-contentdm').click
       VCR.use_cassette('cdm/midpoint-repository', record: :none) do
+        owner = User.first
+        login_as(owner, scope: :user)
+        visit dashboard_owner_path
+        page.find('.tabs').click_link("Start A Project")
+        page.find(:css, '#import-contentdm').click
         page.fill_in 'cdm_url', with: repository_url
         page.find('#cdm_import').click
+        expect(page).to have_content("Collections:", wait: 10)
       end
-      expect(page).to have_content("Collections:", wait: 10)
     end
 
     it 'Gives an error for a well-formed Cdm URL with a bad/empty IIIF manifest', js: true do
-      owner = User.first
-      login_as(owner, scope: :user)
-      visit dashboard_owner_path
-      page.find('.tabs').click_link('Start A Project')
-      page.find(:css, '#import-contentdm').click
       VCR.use_cassette('cdm/bad_iiif_manifest', record: :none) do
+        owner = User.first
+        login_as(owner, scope: :user)
+        visit dashboard_owner_path
+        page.find('.tabs').click_link('Start A Project')
+        page.find(:css, '#import-contentdm').click
         page.fill_in 'cdm_url', with: bad_item_url
         page.find('#cdm_import').click
+        flash_message = "No IIIF manifest exists for CONTENTdm item #{bad_item_url}"
+        expect(page).to have_content(flash_message, wait: 30)
       end
-      flash_message = "No IIIF manifest exists for CONTENTdm item #{bad_item_url}"
-      expect(page).to have_content(flash_message, wait: 30)
     end
   end
 end
