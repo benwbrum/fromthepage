a9930f528131f4fa5a289dbd0928729b4cfc21ef
Will Nigel De Jesus <70936977+WillNigel23@users.noreply.github.com>
2025-10-28 21:40:57 +0800

    5009 - Bulk CDM import es index (#5019)

--- app/jobs/elasticsearch/collection/sync_job.rb
+++ app/jobs/elasticsearch/collection/sync_job.rb
@@ -2,9 +2,19 @@ class Elasticsearch::Collection::SyncJob < ApplicationJob
   queue_as :default
 
   def perform(user_id:, collection_id:, type: :collection, skip_collection: :true)
-    collection = type == :collection ? Collection.find(collection_id) : DocumentSet.find(collection_id)
+    if type == :collection
+      collection = Collection.find(collection_id)
 
-    CollectionsIndex.import collection unless skip_collection
+      CollectionsIndex.import collection unless skip_collection
+    else
+      collection = DocumentSet.find(collection_id)
+
+      if !skip_collection
+        CollectionsIndex.import collection.collection
+        DocumentSetsIndex.import collection
+      end
+    end
+
     WorksIndex.import collection.works
     PagesIndex.import collection.pages
   end
