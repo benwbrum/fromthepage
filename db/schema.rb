0bc4f8a7a346708a1d5c3bf0788e0a4a346dc198
Ben W. Brumfield <benwbrum@gmail.com>
2026-01-29 11:05:23 -0600

    Change schema version to 2025_12_10_202813
    Updated schema version to reflect latest migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_29_102417) do
+ActiveRecord::Schema[7.2].define(version: 2025_12_10_202813) do
   create_table "active_storage_attachments", charset: "utf8", collation: "utf8_general_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
