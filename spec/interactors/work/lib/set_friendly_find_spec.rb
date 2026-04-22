864987aecd389078fe6ec0befeca6ba0b6ccfa93
Ben W. Brumfield <benwbrum@gmail.com>
2026-04-22 13:53:49 -0500

    Fix AI problems for #5438

--- spec/interactors/work/lib/set_friendly_find_spec.rb
+++ spec/interactors/work/lib/set_friendly_find_spec.rb
@@ -1,21 +1,29 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe Work::Lib::SetFriendlyFind do
   describe '.perform' do
-    let(:owner) { create(:owner) }
+    let!(:owner) { create(:unique_user, :owner) }
     let(:first_collection) { create(:collection, owner_user_id: owner.id) }
     let(:second_collection) { create(:collection, owner_user_id: owner.id) }
 
     let!(:first_work) { create(:work, collection: first_collection, owner: owner, slug: '123-work') }
     let!(:second_work) { create(:work, collection: second_collection, owner: owner, slug: "#{first_work.id}-text") }
 
+
     it 'resolves the work in the referenced collection when slug starts with a number' do
       found_work = described_class.perform(
-        id: second_work.slug,
-        collection_reference: second_collection
+        id: second_work.slug
       )
 
       expect(found_work).to eq(second_work)
+    end
+
+    it 'resolves the work by id' do
+      found_work = described_class.perform(
+        id: first_work.id
+      )
+
+      expect(found_work).to eq(first_work)
     end
   end
 end
