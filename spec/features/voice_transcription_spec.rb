5867b1c68e667823b2c6180c7257e47d246632dc
Ben W. Brumfield <benwbrum@gmail.com>
2026-05-10 20:58:39 -0500

    Refactor voice transcription feature spec to use isolated factory-backed test data

--- spec/features/voice_transcription_spec.rb
+++ spec/features/voice_transcription_spec.rb
@@ -1,90 +1,84 @@
 require 'spec_helper'
 
-describe "voice transcription", order: :defined do
-  before :all do
-    @owner = User.find_by(login: OWNER)
-    @user = User.find_by(login: USER)
-    @collection = Collection.second
-    @work = @collection.works.first
-    @page = @work.pages.first
-    @article = @collection.articles.first
+describe 'voice transcription' do
+  let!(:owner) { create(:unique_user, :owner) }
+  let!(:collection) { create(:collection, owner_user_id: owner.id, voice_recognition: voice_recognition, works: []) }
+  let!(:work) { create(:work, collection: collection, owner_user_id: owner.id, supports_translation: true) }
+  let!(:page) { create(:page, work: work) }
+  let!(:article) { create(:article, collection: collection, pages: [page]) }
+
+  let(:voice_recognition) { false }
+
+  before do
+    login_as(owner, scope: :user)
   end
 
-  before :each do
-    login_as(@owner, scope: :user)
-  end
+  def visit_transcribe_page
+    visit collection_transcribe_page_path(collection.owner, collection, work, page)
 
-  it "checks for microphones (not enabled)" do
-    # first set the work to translation
-    @work.supports_translation = true
-    @work.save
-    # transcribe
-    visit collection_transcribe_page_path(@collection.owner, @collection, @work, @page)
     if page.has_content?('Facsimile')
       page.find('.tabs').click_link('Transcribe')
     end
-    # transcription div
+  end
+
+  it 'does not show microphones when voice transcription is disabled' do
+    visit_transcribe_page
+
     expect(page).not_to have_selector('.voice-recognition')
     expect(page).not_to have_selector('#start_img')
-    # note
     expect(page).not_to have_selector('.voice-info')
     expect(page).not_to have_selector('#start_img_note')
-    # translate
+
     page.find('.tabs').click_link('Translate')
     expect(page).not_to have_selector('.voice-recognition')
     expect(page).not_to have_selector('#start_img')
-    # article
-    visit collection_article_edit_path(@collection.owner, @collection, @article)
+
+    visit collection_article_edit_path(collection.owner, collection, article)
     expect(page).not_to have_selector('.article-editarea')
     expect(page).not_to have_selector('#start_img')
   end
 
-  it "turns on voice transcription", js: true do
-    expect(@collection.voice_recognition).to be false
-    visit edit_collection_path(@collection.owner, @collection)
-    page.find('.side-tabs').click_link('Look & Feel')
-    # expect(page).not_to have_selector('#lang_opts')
-    page.check 'collection_voice_recognition'
-    # expect(page).to have_selector('#lang_opts')
-    sleep(2)
-    expect(Collection.second.voice_recognition).to be true
-  end
+  context 'when voice transcription is enabled' do
+    let(:voice_recognition) { true }
 
-it "checks for microphones (enabled)" do
-    # first set the work to translation
-    @work.supports_translation = true
-    @work.save
-    # transcribe
-    visit collection_transcribe_page_path(@collection.owner, @collection, @work, @page)
-    if page.has_content?('Facsimile')
-      page.find('.tabs').click_link('Transcribe')
+    it 'shows microphones on transcription, translation, and article pages' do
+      visit_transcribe_page
+
+      expect(page).to have_selector('.voice-recognition')
+      expect(page).to have_selector('#start_img')
+      expect(page).to have_selector('#start_img_note')
+
+      page.find('.tabs').click_link('Translate')
+      expect(page).to have_selector('.voice-recognition')
+      expect(page).to have_selector('#start_img')
+
+      visit collection_article_edit_path(collection.owner, collection, article)
+      expect(page).to have_selector('.article-editarea')
+      expect(page).to have_selector('#start_img')
     end
-    # transcription div
-    expect(page).to have_selector('.voice-recognition')
-    expect(page).to have_selector('#start_img')
-    # note
-    expect(page).to have_selector('#start_img_note')
-    # translate
-    page.find('.tabs').click_link('Translate')
-    expect(page).to have_selector('.voice-recognition')
-    expect(page).to have_selector('#start_img')
-    # article
-    visit collection_article_edit_path(@collection.owner, @collection, @article)
-    expect(page).to have_selector('.article-editarea')
-    expect(page).to have_selector('#start_img')
   end
 
-  it "turns off voice transcription", js: true do
-    @collection = Collection.second
-    expect(@collection.voice_recognition).to be true
-    visit edit_collection_path(@collection.owner, @collection)
+  it 'turns on voice transcription from collection settings', js: true do
+    expect(collection.voice_recognition).to be false
+
+    visit edit_collection_path(collection.owner, collection)
     page.find('.side-tabs').click_link('Look & Feel')
-    page.uncheck 'collection_voice_recognition'
-    # expect(page).not_to have_selector('#lang_opts')
-    # turn off work translation
-    @work.supports_translation = false
-    @work.save
-    sleep(2)
-    expect(Collection.second.voice_recognition).to be false
+    page.check 'collection_voice_recognition'
+
+    expect { collection.reload.voice_recognition }.to become(true)
+  end
+
+  context 'when voice transcription starts enabled' do
+    let(:voice_recognition) { true }
+
+    it 'turns off voice transcription from collection settings', js: true do
+      expect(collection.voice_recognition).to be true
+
+      visit edit_collection_path(collection.owner, collection)
+      page.find('.side-tabs').click_link('Look & Feel')
+      page.uncheck 'collection_voice_recognition'
+
+      expect { collection.reload.voice_recognition }.to become(false)
+    end
   end
 end
