5f4e8194e14f8cea877e3b4111a1691db8244bc2
Ben W. Brumfield <benwbrum@gmail.com>
2026-05-30 11:44:37 -0500

    Refactor user_deletion_spec to use factories and isolated setup
    Replace before :all with before :each, replace User.create/Collection.first/
    Article.first lookups with factory-created records (unique_user, collection,
    work, page, article). Admin is now also factory-created. Each example gets
    a fresh set of records and is no longer dependent on globally seeded test
    data from earlier specs.
    
    Also add explicit expectations to the "does not break" examples so they
    assert something meaningful rather than just checking the page loads
    without raising.

--- spec/features/user_deletion_spec.rb
+++ spec/features/user_deletion_spec.rb
@@ -1,64 +1,66 @@
 require 'spec_helper'
 
-describe "User deletion" do
-  before :all do
-    # create a user
-    # do a lot of things as that user
-    # transcribe a page
-    # edit an article
-    # delete the user
-    @user = User.create(login: 'foo', password: 'barbarbar', password_confirmation: 'barbarbar', email: "foo@example.com")
-    @collection = Collection.first
-    @work = @collection.works.first
-    @page1 = @work.pages.first
+describe 'User deletion' do
+  let!(:owner) { create(:unique_user, :owner) }
+  let!(:admin) { create(:unique_user, :admin) }
+  let!(:collection) { create(:collection, owner_user_id: owner.id, works: []) }
+  let!(:work) { create(:work, collection: collection, owner_user_id: owner.id) }
+  let!(:work_page) { create(:page, work: work) }
+  let!(:article) { create(:article, collection: collection) }
 
-    # do a llot of things as that user
-    login_as(@user, scope: :user)
-    visit collection_read_work_path(@work.collection.owner, @work.collection, @work)
+  # A user who will perform actions and then be deleted
+  let!(:doomed_user) { create(:unique_user) }
 
-    page.find('.work-page_title', text: @page1.title).click_link(@page1.title)
-    # leave a note
-    fill_in 'note_body', with: "Test private note"
+  before :each do
+    # Leave a note as the doomed user
+    login_as(doomed_user, scope: :user)
+    visit collection_read_work_path(work.collection.owner, work.collection, work)
+    page.find('.work-page_title', text: work_page.title).click_link(work_page.title)
+    fill_in 'note_body', with: 'Test private note'
     find('#save_note_button').click
 
-    # transcribe a page
-    visit "/display/display_page?page_id=#{@page1.id}"
-    page.find('.tabs').click_link("Transcribe")
-    fill_in_editor_field "[[Places|Texas]]"
+    # Transcribe the page as the doomed user
+    visit "/display/display_page?page_id=#{work_page.id}"
+    page.find('.tabs').click_link('Transcribe')
+    fill_in_editor_field '[[Places|Texas]]'
     find('#save_button_top').click
 
-    @article = Article.first
-    visit "/article/show?article_id=#{@article.id}"
-    click_link("Settings")
-    page.fill_in 'article_source_text', with: "This is more text about my article."
+    # Edit the article as the doomed user
+    visit "/article/show?article_id=#{article.id}"
+    click_link('Settings')
+    page.fill_in 'article_source_text', with: 'This is more text about my article.'
     click_button('Save Changes')
 
-    @admin = User.where(admin: true).first
-    login_as(@admin, scope: :user)
-    visit url_for(action: 'delete_user', controller: 'admin', user_id: @user.id)
+    # Delete the user as admin
+    login_as(admin, scope: :user)
+    visit url_for(action: 'delete_user', controller: 'admin', user_id: doomed_user.id)
   end
 
-  it "does not break collection home" do
-     visit collection_path(@collection.owner, @collection)
+  it 'does not break collection home' do
+    visit collection_path(collection.owner, collection)
+    expect(page).to have_selector('h1', text: collection.title)
   end
 
-  it "does not break deed list" do
+  it 'does not break deed list' do
     visit url_for(action: 'list', controller: 'deed')
+    expect(page.status_code).to eq(200)
   end
 
-  it "does not break page versions" do
-    @admin = User.where(admin: true).first
-    login_as(@admin, scope: :user)
-    visit "/display/display_page?page_id=#{@page1.id}"
-    click_link("Versions")
+  it 'does not break page versions' do
+    login_as(admin, scope: :user)
+    visit "/display/display_page?page_id=#{work_page.id}"
+    click_link('Versions')
+    expect(page.status_code).to eq(200)
   end
 
-  it "does not break page notes" do
-    visit "/display/display_page?page_id=#{@page1.id}"
+  it 'does not break page notes' do
+    visit "/display/display_page?page_id=#{work_page.id}"
+    expect(page.status_code).to eq(200)
   end
 
-  it "does not break article versions" do
-    visit "/article/show?article_id=#{@article.id}"
-    click_link("Versions")
+  it 'does not break article versions' do
+    visit "/article/show?article_id=#{article.id}"
+    click_link('Versions')
+    expect(page.status_code).to eq(200)
   end
 end
