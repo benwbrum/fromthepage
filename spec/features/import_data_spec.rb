24256526dad614a7fe2bb0b2adf2ee011d5707c3
Ben W. Brumfield <benwbrum@gmail.com>
2026-06-12 21:32:50 -0500

    Isolate import data feature spec

--- spec/features/import_data_spec.rb
+++ spec/features/import_data_spec.rb
@@ -1,73 +1,62 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe 'import data' do
-  let(:owner) { create(:owner) }
-
-  before :each do
+  before do
     DatabaseCleaner.start
+    login_as(owner, scope: :user)
   end
 
-  after :each do
+  after do
     DatabaseCleaner.clean
   end
 
-  context 'CONTENTdm' do
-    let(:item_url)      { 'https://cdm16488.contentdm.oclc.org/digital/collection/MPD01/id/2' }
+  let(:owner) { create(:unique_user, :owner, account_type: 'Small Organization') }
+
+  context 'with CONTENTdm' do
+    let(:item_url) { 'https://cdm16488.contentdm.oclc.org/digital/collection/MPD01/id/2' }
     let(:collection_url) { 'https://cdm16488.contentdm.oclc.org/digital/collection/MPD01' }
     let(:repository_url) { 'https://cdm16488.contentdm.oclc.org/' }
-    let(:bad_item_url)  { 'https://hrc.contentdm.oclc.org/digital/collection/p15878coll90/id/41/rec/3' }
+    let(:bad_item_url) { 'https://hrc.contentdm.oclc.org/digital/collection/p15878coll90/id/41/rec/3' }
 
-    it "browses a single record" do
+    it 'browses a single record' do
       VCR.use_cassette('cdm/midpoint-shelwater-item', record: :none) do
-        owner = User.first
-        login_as(owner, scope: :user)
-        visit dashboard_owner_path
-        page.find('.tabs').click_link("Start A Project")
-        find('#cdm_url', visible: false)
-          .set(item_url)
-        find('#cdm_import', visible: false).click
+        browse_contentdm(item_url)
+
         expect(page).to have_content('Manifest: Letter with envelope from Virginia Shewalter', wait: 30)
       end
     end
 
     it 'browses records from a collection' do
       VCR.use_cassette('cdm/midpoint-shelwater-collection', record: :none, allow_playback_repeats: false) do
-        owner = User.first
-        login_as(owner, scope: :user)
-        visit dashboard_owner_path
-        page.find('.tabs').click_link('Start A Project')
-        find('#cdm_url', visible: false)
-          .set(collection_url)
-        find('#cdm_import', visible: false).click
+        browse_contentdm(collection_url)
+
         expect(page).to have_content('Collection: The Virginia Shewalter Letters Collection', wait: 30)
       end
     end
 
     it 'browses collections from a repository' do
       VCR.use_cassette('cdm/midpoint-repository', record: :none) do
-        owner = User.first
-        login_as(owner, scope: :user)
-        visit dashboard_owner_path
-        page.find('.tabs').click_link("Start A Project")
-        find('#cdm_url', visible: false)
-          .set(repository_url)
-        find('#cdm_import', visible: false).click
-        expect(page).to have_content("Collections:", wait: 10)
+        browse_contentdm(repository_url)
+
+        expect(page).to have_content('Collections:', wait: 10)
       end
     end
 
-    it 'Gives an error for a well-formed Cdm URL with a bad/empty IIIF manifest' do
+    it 'gives an error for a well-formed CONTENTdm URL with an empty IIIF manifest' do
       VCR.use_cassette('cdm/bad_iiif_manifest', record: :none) do
-        owner = User.first
-        login_as(owner, scope: :user)
-        visit dashboard_owner_path
-        page.find('.tabs').click_link('Start A Project')
-        find('#cdm_url', visible: false)
-          .set(bad_item_url)
-        find('#cdm_import', visible: false).click
-        flash_message = "No IIIF manifest exists for CONTENTdm item #{bad_item_url}"
-        expect(page).to have_content(flash_message, wait: 30)
+        browse_contentdm(bad_item_url)
+
+        expect(page).to have_content("No IIIF manifest exists for CONTENTdm item #{bad_item_url}", wait: 30)
       end
     end
+  end
+
+  def browse_contentdm(url)
+    visit dashboard_owner_path
+    page.find('.tabs').click_link('Start A Project')
+    find('#cdm_url', visible: false).set(url)
+    find('#cdm_import', visible: false).click
   end
 end
