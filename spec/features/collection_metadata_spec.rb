b96b5959de141e6b8269f99cbadf6af650e0b8d1
Ben W. Brumfield <benwbrum@gmail.com>
2026-06-16 08:25:37 -0500

    Isolate collection metadata feature spec

--- spec/features/collection_metadata_spec.rb
+++ spec/features/collection_metadata_spec.rb
@@ -1,28 +1,65 @@
 require 'spec_helper'
 
-describe "collection metadata", order: :defined do
+RSpec.describe 'collection metadata', order: :defined do
   include ActiveJob::TestHelper
 
-  before :each do
-    @owner = User.where(login: 'wakanda').first
-    @user = User.where(login: 'margaret').first
+  let(:collection_title) { "ladi-#{SecureRandom.hex(4)}" }
+  let(:metadata_keys) { %w[filename field_identifier_local] }
+
+  before do |example|
+    DatabaseCleaner.start unless example.metadata[:js]
+    @owner = create(:unique_user, :owner, account_type: 'Small Organization')
+    @user = create(:unique_user)
   end
 
-  it "creates a collection as owner" do
+  after do |example|
+    clear_enqueued_jobs
+    clear_performed_jobs
+    Current.user = nil
+    logout
+
+    if example.metadata[:js]
+      Collection.where(owner_user_id: @owner.id).find_each(&:destroy!) if @owner&.persisted?
+      [@user, @owner].compact.each { |user| user.destroy! if user.persisted? }
+    else
+      DatabaseCleaner.clean
+    end
+  end
+
+  def create_metadata_collection(title: collection_title, owner: @owner)
+    collection = create(:collection, owner_user_id: owner.id, title: title, works: [])
+    metadata_keys.each do |key|
+      metadata_coverage = create(:metadata_coverage, collection: collection, key: key, count: 3)
+      create(:facet_config, metadata_coverage: metadata_coverage)
+    end
+    collection
+  end
+
+  def create_importable_collection(title: collection_title, owner: @owner)
+    collection = create(:collection, owner_user_id: owner.id, title: title, works: [])
+    %w[eaacone_c1955-01 eaacone_c1975-01 eaacone_c1977-01].each do |identifier|
+      create(:work, owner: owner, owner_user_id: owner.id, collection: collection, title: identifier, identifier: identifier)
+    end
+    collection
+  end
+
+  it 'creates a collection as owner' do
     login_as @owner
     visit dashboard_owner_path
     page.find('a', text: 'Create a Collection').click
-    fill_in 'collection_title', with: 'ladi'
+    fill_in 'collection_title', with: collection_title
     click_button('Create Collection')
-    expect(page).to have_content("ladi")
+    expect(page).to have_content(collection_title)
   end
 
-  it "uploads works from a zip file", js: true do
+  it 'uploads works from a zip file', js: true do
+    create(:collection, owner_user_id: @owner.id, title: collection_title, works: [])
+
     login_as @owner
     visit dashboard_owner_path
-    page.find('.tabs').click_link("Start A Project")
-    page.find(:css, "#document-upload").click
-    select2_select(id: 'document_upload_collection_id', value: 'ladi')
+    page.find('.tabs').click_link('Start A Project')
+    page.find(:css, '#document-upload').click
+    select2_select(id: 'document_upload_collection_id', value: collection_title)
 
     attach_file(
       'document_upload_file',
@@ -32,19 +69,20 @@ describe "collection metadata", order: :defined do
     sleep 2
     click_button('Upload File')
 
-    expect(page).to have_content("Document has been uploaded")
+    expect(page).to have_content('Document has been uploaded')
     title = find('h1').text
-    expect(title).to eq "ladi"
+    expect(title).to eq collection_title
     wait_for_upload_processing
     sleep(10)
   end
 
-  it "uploads metadata for the imported works", js: true do
+  it 'uploads metadata for the imported works', js: true do
+    c = create_importable_collection
+
     login_as @owner
-    c = Collection.where(title: "ladi").first
     visit edit_collection_path(@owner, c)
     page.find('.side-tabs').click_link('Look & Feel')
-    expect(page).to have_content("Allow users to browse works within this collection via metadata.")
+    expect(page).to have_content('Allow users to browse works within this collection via metadata.')
     visit collection_metadata_upload_path(c)
     expect(page).to have_content('To update metadata for several works within this collection')
 
@@ -59,20 +97,20 @@ describe "collection metadata", order: :defined do
     perform_enqueued_jobs
   end
 
-  it "increments occurrences as works are re-imported", js: true do
+  it 'increments occurrences as works are re-imported', js: true do
+    c = create_importable_collection
+    filename = create(:metadata_coverage, collection: c, key: 'filename', count: 3)
+    create(:facet_config, metadata_coverage: filename)
+
     login_as @owner
-    c = Collection.find_by(title: 'ladi')
-    filename = c.metadata_coverages.where(key: 'filename').first
     expect(filename.count).to eq 3
 
-    # reupload the same work here.
     visit edit_collection_path(@owner, c)
     page.find('.side-tabs').click_link('Look & Feel')
-    expect(page).to have_content("Allow users to browse works within this collection via metadata.")
+    expect(page).to have_content('Allow users to browse works within this collection via metadata.')
     visit collection_metadata_upload_path(c)
-    expect(page).to have_content("To update metadata for several works within this collection")
+    expect(page).to have_content('To update metadata for several works within this collection')
 
-    # workaround
     script = "$('#metadata_file').css({opacity: 100, display: 'block', position: 'relative', left: ''});"
     page.execute_script(script)
 
@@ -90,105 +128,108 @@ describe "collection metadata", order: :defined do
     expect(filename.count).to eq 3
   end
 
-  it "enables facets", js: true do
+  it 'enables facets', js: true do
+    c = create_metadata_collection
+
     login_as @owner
-    c = Collection.where(title: "ladi").first
     visit edit_collection_path(@owner, c)
     page.find('.side-tabs').click_link('Look & Feel')
-    page.check("Enable metadata facets")
+    page.check('Enable metadata facets')
     expect(page).to have_content('Collection has been updated')
     page.click_link('Edit Facets')
-    expect(page).to have_content("Metadata Facets")
-    expect(page).to have_content("Configure metadata facets by reviewing the metadata in your collection and labelling fields to be displayed to transcribers.")
-    expect(page).to have_content("filename")
-    expect(page).to have_content("field_identifier_local")
+    expect(page).to have_content('Metadata Facets')
+    expect(page).to have_content('Configure metadata facets by reviewing the metadata in your collection and labelling fields to be displayed to transcribers.')
+    expect(page).to have_content('filename')
+    expect(page).to have_content('field_identifier_local')
   end
 
-  it "allows saving additional metadata" do
+  it 'allows saving additional metadata' do
+    c = create_metadata_collection
+
     login_as @owner
-    c = Collection.where(title: "ladi").first
     visit edit_collection_path(@owner, c)
     page.find('.side-tabs').click_link('Look & Feel')
-    click_link "Edit Facets"
-    expect(page).to have_content("Metadata Facets")
-    expect(page).to have_content("filename")
+    click_link 'Edit Facets'
+    expect(page).to have_content('Metadata Facets')
+    expect(page).to have_content('filename')
     fill_in 'metadata_filename_label', with: 'Filename'
     fill_in 'metadata_filename_order', with: 9
     click_button 'Save Metadata'
-    expect(page).to have_content("Collection facets updated successfully")
-    expect(find_field('metadata_filename_label').value).to eq "Filename"
-    expect(find_field('metadata_filename_order').value).to eq "0"
+    expect(page).to have_content('Collection facets updated successfully')
+    expect(find_field('metadata_filename_label').value).to eq 'Filename'
+    expect(find_field('metadata_filename_order').value).to eq '0'
   end
 
-  it "allows a numeric value from 0 to 9 for text type" do
+  it 'allows a numeric value from 0 to 9 for text type' do
+    c = create_metadata_collection
+
     login_as @owner
-    c = Collection.where(title: "ladi").first
     visit edit_collection_path(@owner, c)
     page.find('.side-tabs').click_link('Look & Feel')
-    click_link "Edit Facets"
-    expect(page).to have_content("Metadata Facets")
-    expect(page).to have_content("filename")
+    click_link 'Edit Facets'
+    expect(page).to have_content('Metadata Facets')
+    expect(page).to have_content('filename')
     fill_in 'metadata_filename_label', with: 'Filename'
     fill_in 'metadata_filename_order', with: 25
     click_button 'Save Metadata'
-    expect(page).to have_content("Order is not included in the list")
+    expect(page).to have_content('Order is not included in the list')
   end
 
-  it "allows a numeric value from 0 to 2 for date type" do
+  it 'allows a numeric value from 0 to 2 for date type' do
+    c = create_metadata_collection
+
     login_as @owner
-    c = Collection.where(title: "ladi").first
     visit edit_collection_path(@owner, c)
     page.find('.side-tabs').click_link('Look & Feel')
-    click_link "Edit Facets"
-    expect(page).to have_content("Metadata Facets")
-    expect(page).to have_content("filename")
+    click_link 'Edit Facets'
+    expect(page).to have_content('Metadata Facets')
+    expect(page).to have_content('filename')
     fill_in 'metadata_filename_label', with: 'Filename'
-    select("date", from: 'metadata_filename_input_type')
+    select('date', from: 'metadata_filename_input_type')
     fill_in 'metadata_filename_order', with: 3
     click_button 'Save Metadata'
-    expect(page).to have_content("Order is not included in the list")
+    expect(page).to have_content('Order is not included in the list')
   end
 
   it "can't enter an order as a string" do
+    c = create_metadata_collection
+
     login_as @owner
-    c = Collection.where(title: "ladi").first
     visit edit_collection_path(@owner, c)
     page.find('.side-tabs').click_link('Look & Feel')
-    click_link "Edit Facets"
-    expect(page).to have_content("Metadata Facets")
-    expect(page).to have_content("filename")
+    click_link 'Edit Facets'
+    expect(page).to have_content('Metadata Facets')
+    expect(page).to have_content('filename')
     fill_in 'metadata_filename_label', with: 'Filename'
     fill_in 'metadata_filename_order', with: 'foo'
     click_button 'Save Metadata'
-    expect(page).to have_content("Order is not a number")
+    expect(page).to have_content('Order is not a number')
   end
 
-  it "should not be available/visible for the Individual Researcher plan", js: true do
+  it 'should not be available/visible for the Individual Researcher plan', js: true do
+    c = create_metadata_collection
+
     logout
-    old_account_type=@owner.account_type
-    @owner.account_type='Individual Researcher'
-    @owner.save
+    @owner.update!(account_type: 'Individual Researcher')
     login_as @owner
-    c = Collection.where(title: "ladi").first
     visit edit_collection_path(@owner, c)
     page.find('.side-tabs').click_link('Look & Feel')
-    expect(page).to have_field("Enable metadata facets", disabled: true)
-    expect(page.find_link("Edit Facets")).to match_css('[disabled]')
-    expect(page).to have_content("Not available for researcher accounts.")
-    @owner.account_type=old_account_type
-    @owner.save
+    expect(page).to have_field('Enable metadata facets', disabled: true)
+    expect(page.find_link('Edit Facets')).to match_css('[disabled]')
+    expect(page).to have_content('Not available for researcher accounts.')
   end
 
-  it "deletes a collection" do
+  it 'deletes a collection' do
+    c = create_metadata_collection(title: collection_title)
+
     logout
     login_as @owner
-    c = Collection.where(title: "ladi").first
     visit edit_collection_path(@owner, c)
     page.find('.side-tabs').click_link('Danger Zone')
-    expect(page).to have_content("ladi")
-    expect(page).to have_content("Please use caution")
-    click_link "Delete Collection"
-    expect(page).not_to have_content("ladi")
+    expect(page).to have_content(collection_title)
+    expect(page).to have_content('Please use caution')
+    click_link 'Delete Collection'
+    expect(page).not_to have_content(collection_title)
     expect(c.metadata_coverages).to be_empty
   end
 end
