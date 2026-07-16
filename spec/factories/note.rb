88e49420e7fa692c23229bedc544b833f8940a64
Will Nigel De Jesus <70936977+WillNigel23@users.noreply.github.com>
2026-07-16 22:56:58 +0800

    Parallel tests (#5646)

--- spec/factories/note.rb
+++ spec/factories/note.rb
@@ -2,5 +2,10 @@ FactoryBot.define do
   factory :note do
     sequence(:title) { |n| "Note Title #{n}" }
     sequence(:body) { |n| "Note Title #{n}" }
+
+    association :user
+    association :collection
+    association :work
+    association :page
   end
 end
